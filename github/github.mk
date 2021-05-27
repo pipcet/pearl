89b01455e8c8277c086dea157bef037f0b43f49b
Pip Cet <pipcet@gmail.com>
2021-05-27 20:25:14 +0000

    github/github.mk: forgot that rule

--- github/github.mk
+++ github/github.mk
@@ -24,10 +24,15 @@ build/%{release}: build/%
 	$(MKDIR) build/release
 	$(CP) $< build/release
 
+build/github-releases{list}: | build/github-releases/
+	curl -sSL https://api.github.com/repos/$$GITHUB_REPOSITORY/releases?per_page=100 | jq '.[] | [(.).tag_name,(.).id] | .[]' | while read tag; do read id; echo $$id > build/github-releases/$$tag; done
+	curl -sSL https://api.github.com/repos/$$GITHUB_REPOSITORY/releases/tags/latest | jq '.[.tag_name,.id] | .[]' | while read tag; do read id; echo $$id > build/github-releases/$$tag; done
+	ls -l build/github-releases/
+
 %{release}: | github/release/
-	$(MAKE) github/release/list!
+	$(MAKE) build/github-releases{list}
 	for name in $$(cd build/release; ls *); do for id in $$(jq ".[] | if .name == \"$$name\" then .id else 0 end" < github/assets/$*.json); do [ $$id != "0" ] && curl -sSL -XDELETE -H "Authorization: token $$GITHUB_TOKEN" "https://api.github.com/repos/$$GITHUB_REPOSITORY/releases/assets/$$id"; echo; done; done
-	(for name in build/release/*; do bname=$$(basename "$$name"); curl -sSL -XPOST -H "Authorization: token $$GITHUB_TOKEN" --header "Content-Type: application/octet-stream" "https://uploads.github.com/repos/$$GITHUB_REPOSITORY/releases/$$(cat github/release/\"$*\")/assets?name=$$bname" --upload-file $$name; echo; done)
+	(for name in build/release/*; do bname=$$(basename "$$name"); curl -sSL -XPOST -H "Authorization: token $$GITHUB_TOKEN" --header "Content-Type: application/octet-stream" "https://uploads.github.com/repos/$$GITHUB_REPOSITORY/releases/$$(cat build/github-releases/\"$*\")/assets?name=$$bname" --upload-file $$name; echo; done)
 
 {release}:
 	this_release_date="$$(date --iso)"; \
