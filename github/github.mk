e789055a30489ce7051ed1bc098912bb9766ec31
Pip Cet <pipcet@gmail.com>
2021-05-27 19:55:10 +0000

    github/github.mk: minimal support for pushing an artifact

--- github/github.mk
+++ github/github.mk
@@ -1,2 +1,16 @@
 # GitHub integration
 
+.github-init:
+	bash github/artifact-init
+	touch $@
+
+build/artifact-timestamp:
+	touch $@
+	sleep 1
+
+build/artifacts/up/pearl.macho: build/pearl.macho build/artifact-timestamp | build/artifacts/up/
+	$(MKDIR) $(dir $@)
+	$(CP) $< $@
+
+build/artifacts{push}:
+	(cd build/artifacts/up; for file in *; do if [ "$$file" -nt ../../artifact-timestamp ]; then name=$$(basename "$$file"); (cd $(PWD); bash github/ul-artifact "$$name" "build/artifacts/up/$$name"); fi; done)
