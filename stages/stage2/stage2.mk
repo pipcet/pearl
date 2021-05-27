f528336bb9e97ea2b70383b421d5299bf2be6eb9
Pip Cet <pipcet@gmail.com>
2021-05-27 21:55:08 +0000

    stages/stage2/stage2.mk: fix typo

--- stages/stage2/stage2.mk
+++ stages/stage2/stage2.mk
@@ -1,6 +1,6 @@
 build/stages/stage2/stage2.cpiospec: \
-	build/stages/stage1/initfs/boot/linux.image \
-	build/stages/stage1/initfs/boot/linux.dtb
+	build/stages/stage2/initfs/boot/linux.image \
+	build/stages/stage2/initfs/boot/linux.dtb
 
 build/stages/stage2/initfs/boot/linux.image: \
 	build/stages/linux/linux.image
