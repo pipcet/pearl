e6304e6821303a4449879cd714a385c51785fcc7
Pip Cet <pipcet@gmail.com>
2021-05-27 21:49:39 +0000

    stages/stage2/stage2.mk: include linux stage in stage2

--- stages/stage2/stage2.mk
+++ stages/stage2/stage2.mk
@@ -1,1 +1,14 @@
-#
+build/stages/stage2/stage2.cpiospec: \
+	build/stages/stage1/initfs/boot/linux.image \
+	build/stages/stage1/initfs/boot/linux.dtb
+
+build/stages/stage2/initfs/boot/linux.image: \
+	build/stages/linux/linux.image
+	$(MKDIR) $(dir $@)
+	$(CP) $< $@
+
+build/stages/stage2/initfs/boot/linux.dtb: \
+	build/stages/linux/linux.dtb
+	$(MKDIR) $(dir $@)
+	$(CP) $< $@
+
