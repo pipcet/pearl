da98f31b84a844cbc040367155b7566deb7f1505
Pip Cet <pipcet@gmail.com>
2021-05-27 19:37:08 +0000

    macho-tools/image-to-macho.c: minor fix

--- macho-tools/image-to-macho.c
+++ macho-tools/image-to-macho.c
@@ -203,7 +203,7 @@ int main(int argc, char **argv)
   SNIPPET(save_boot_args);
   //SNIPPET(disable_timers);
   //SNIPPET(x8r8g8b8);
-  SNIPPET(perform_alignment_4);
+  SNIPPET(perform_alignment);
   SNIPPET(enable_all_clocks);
   //SNIPPET(bring_up_phys_2);
   SNIPPET(restore_boot_args);
