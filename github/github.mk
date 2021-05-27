b5cc34f0ce1e6607b62122c0be938b0cbdf36425
Pip Cet <pipcet@gmail.com>
2021-05-27 20:17:26 +0000

    github/github.mk: release rules

--- github/github.mk
+++ github/github.mk
@@ -19,8 +19,18 @@ build/%{artifact}: build/%
 	$(MKDIR) build/artifacts/up
 	$(CP) $< build/artifacts/up
 	$(MAKE) build/artifacts{push}
+
+build/%{release}: build/%
+	$(MKDIR) build/release
+	$(CP) $< build/release
+
+%{release}: | github/release/
+	$(MAKE) github/release/list!
+	for name in $$(cd build/release; ls *); do for id in $$(jq ".[] | if .name == \"$$name\" then .id else 0 end" < github/assets/$*.json); do [ $$id != "0" ] && curl -sSL -XDELETE -H "Authorization: token $$GITHUB_TOKEN" "https://api.github.com/repos/$$GITHUB_REPOSITORY/releases/assets/$$id"; echo; done; done
+	(for name in build/release/*; do bname=$$(basename "$$name"); curl -sSL -XPOST -H "Authorization: token $$GITHUB_TOKEN" --header "Content-Type: application/octet-stream" "https://uploads.github.com/repos/$$GITHUB_REPOSITORY/releases/$$(cat github/release/\"$*\")/assets?name=$$bname" --upload-file $$name; echo; done)
+
 {release}:
 	this_release_date="$$(date --iso)"; \
 	node ./github/release.js $$this_release_date $$this_release_date > github/release.json; \
 	curl -sSL -XPOST -H "Authorization: token $$GITHUB_TOKEN" "https://api.github.com/repos/$$GITHUB_REPOSITORY/releases" --data '@github/release.json'; \
-	$(MAKE) ship/$$this_release_date!
+	$(MAKE) $$this_release_date{release}
