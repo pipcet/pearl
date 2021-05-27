6beb84a78a5a1601f3711b341c06d6a35936fae7
Pip Cet <pipcet@gmail.com>
2021-05-27 20:59:44 +0000

    dt/dt.mk: add rules for adtdump, adtp

--- dt/dt.mk
+++ dt/dt.mk
@@ -1,3 +1,9 @@
 build/dt/bin/dt: dt/dt
 	$(MKDIR) $(dir $@)
 	$(CP) $< $@
+
+build/dt/bin/adtdump: dt/adtdump.c
+	$(CROSS_COMPILE)gcc -Os -static -o $@ $<
+
+build/dt/bin/adtp: dt/adtp.cc
+	$(CROSS_COMPILE)g++ -Os -static -o $@ $<
