ba3c86be96db0514ba5befb3641172338e0eb852
Pip Cet <pipcet@gmail.com>
2021-05-27 21:45:15 +0000

    stages/stage1/stage1.mk: include stage2 image in stage1

--- stages/stage1/stage1.mk
+++ stages/stage1/stage1.mk
@@ -1,3 +1,13 @@
 build/stages/stage1/stage1.cpiospec: \
 	build/stages/stage1/initfs/boot/stage2.image \
 	build/stages/stage1/initfs/boot/stage2.dtb
+
+build/stages/stage1/initfs/boot/stage2.image: \
+	build/stages/stage2/stage2.image
+	$(MKDIR) $(dir $@)
+	$(CP) $< $@
+
+build/stages/stage1/initfs/boot/stage2.dtb: \
+	build/stages/stage2/stage2.dtb
+	$(MKDIR) $(dir $@)
+	$(CP) $< $@
