4beb3bb9ec31b710ec644b83c578c93852b9a69b
Pip Cet <pipcet@gmail.com>
2021-05-28 12:27:31 +0000

    dt/dt.mk: leave dt.tar uncompressed

--- dt/dt.mk
+++ dt/dt.mk
@@ -8,5 +8,5 @@ build/dt/bin/adtdump: dt/adtdump.c
 build/dt/bin/adtp: dt/adtp.cc
 	$(CROSS_COMPILE)g++ -Os -static -o $@ $<
 
-build/dt.tar.gz: build/dt/bin/dt build/dt/bin/adtdump build/dt/bin/adtp
-	(cd build/dt; tar cz bin/dt bin/adtdump bin/adtp) > $@
+build/dt.tar: build/dt/bin/dt build/dt/bin/adtdump build/dt/bin/adtp
+	(cd build/dt; tar c bin/dt bin/adtdump bin/adtp) > $@
