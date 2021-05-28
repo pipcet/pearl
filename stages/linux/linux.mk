420883d9df644760c624b4531910efb29957243c
Pip Cet <pipcet@gmail.com>
2021-05-28 11:28:54 +0000

    stages/linux/linux.mk: no longer need our own cpiospec

--- stages/linux/linux.mk
+++
@@ -1,2 +0,0 @@
-build/stages/linux/linux.initfs: build/stages/linux/linux.cpiospec build/stages/linux/linux.image
-	(cd build/linux/linux; $(PWD)/submodule/linux/usr/gen_initramfs.sh -o $(PWD)/$@ ../../../$<)
