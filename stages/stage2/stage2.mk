27ecf66d9604da65e4968cdabe87feace3c1a834
Pip Cet <pipcet@gmail.com>
2021-05-28 11:32:43 +0000

    stages/stage2/stage2.mk: simplify

--- stages/stage2/stage2.mk
+++ stages/stage2/stage2.mk
@@ -1,13 +1,3 @@
-build/stages/stage2/initfs/boot/linux.image: \
-	build/stages/linux/linux.image
-	$(MKDIR) $(dir $@)
-	$(CP) $< $@
-
-build/stages/stage2/initfs/boot/linux.dtb: \
-	build/stages/linux/linux.dtb
-	$(MKDIR) $(dir $@)
-	$(CP) $< $@
-
 build/initfs/common.tar: \
 	build/initfs/common/linux.image \
 	build/initfs/common/linux.dtb
