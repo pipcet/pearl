623cde6a939d9adb9b68b27b93fc9379993d040a
Pip Cet <pipcet@gmail.com>
2021-05-28 21:39:29 +0000

    dtc/dtc.mk: fix typo

--- dtc/dtc.mk
+++ dtc/dtc.mk
@@ -3,5 +3,5 @@ DTC ?= dtc
 build/%.dtb.h: build/%.dtb
 	(echo "{";  cat $< | od -tx4 --width=4 -Anone -v | sed -e 's/ \(.*\)/\t0x\1,/'; echo "};") > $@
 
-build/%.dtb.dts: build/%.dts
+build/%.dts.dtb: build/%.dts
 	$(DTC) -Idts -Odtb $< > $@.tmp && mv $@.tmp $@
