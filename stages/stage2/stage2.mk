9c4021b4c498fcef54431be0503e5d416c42da66
Pip Cet <pipcet@gmail.com>
2021-05-28 18:15:05 +0000

    stages/stage2/stage2.mk: add bin/stage2 as init for stage2

--- stages/stage2/stage2.mk
+++ stages/stage2/stage2.mk
@@ -1,3 +1,9 @@
 build/initfs/common.tar: \
 	build/initfs/common/linux.image \
-	build/initfs/common/linux.dtb
+	build/initfs/common/linux.dtb \
+	build/initfs/common/bin/stage2
+
+build/initfs/common/bin/stage2: stages/stage2/init
+	$(MKDIR) $(dir $@)
+	$(CP) $< $@
+	chmod ug+x $@
