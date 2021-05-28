b9d13cc2a98a0c99e88cd33199fcc361ee09aef3
Pip Cet <pipcet@gmail.com>
2021-05-28 09:02:12 +0000

    stages/stage2/stage2.mk: include linux.initfs

--- stages/stage2/stage2.mk
+++ stages/stage2/stage2.mk
@@ -1,5 +1,6 @@
 build/stages/stage2/stage2.cpiospec: \
 	build/stages/stage2/initfs/boot/linux.image \
+	build/stages/stage2/initfs/boot/linux.initfs \
 	build/stages/stage2/initfs/boot/linux.dtb
 
 build/stages/stage2/initfs/boot/linux.image: \
@@ -9,6 +10,11 @@ build/stages/stage2/initfs/boot/linux.image: \
 
 build/stages/stage2/initfs/boot/linux.dtb: \
 	build/stages/linux/linux.dtb
+	$(MKDIR) $(dir $@)
+	$(CP) $< $@
+
+build/stages/stage2/initfs/boot/linux.initfs: \
+	build/stages/linux/linux.initfs
 	$(MKDIR) $(dir $@)
 	$(CP) $< $@
 
