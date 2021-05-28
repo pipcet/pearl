95b28421ecb281aabdc49dbbeba07792d4aea043
Pip Cet <pipcet@gmail.com>
2021-05-28 09:01:59 +0000

    stages/linux/linux.mk: build linux.initfs

--- stages/linux/linux.mk
+++ stages/linux/linux.mk
@@ -1,1 +1,2 @@
-#
+build/stages/linux/linux.initfs: build/stages/linux/linux.cpiospec build/stages/linux/linux.image
+	(cd build/linux/linux; $(PWD)/submodule/linux/usr/gen_initramfs.sh -o $(PWD)/$@ ../../../$<)
