9e7bda277a51c79e92150cfa42f3aab84408a8e7
Pip Cet <pipcet@gmail.com>
2021-05-28 18:50:50 +0000

    stages/stages.mk: move per-stage stuff here

--- stages/stages.mk
+++ stages/stages.mk
@@ -1,3 +1,11 @@
+define perstage
+build/stages/$(stage)/$(stage).image: | build/stages/$(stage)/
+
+build/initfs/common/boot/$(stage).dtb: build/stages/$(stage)/$(stage).dtb
+	$$(MKDIR) $$(dir $$@)
+	$$(CP) $$< $$@
+endef
+
 # Linux stages
 include stages/stage1/stage1.mk
 include stages/stage2/stage2.mk
@@ -5,3 +13,16 @@ include stages/linux/linux.mk
 
 # m1n1 stage
 include stages/m1n1/m1n1.mk
+
+# macos stage
+include stages/macos/macos.mk
+
+# parasite stage
+include stages/parasite/parasite.mk
+
+# debian stage
+include stages/debian/debian.mk
+
+$(foreach stage,$(stages),$(eval $(perstage)))
+
+$(foreach stage,$(linux-stages),$(eval $(linux-perstage)))
