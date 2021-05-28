f451eef09d2b635766fe9297436bb95740a888a8
Pip Cet <pipcet@gmail.com>
2021-05-28 21:30:49 +0000

    stages/stage2/stage2.mk: simplify

--- stages/stage2/stage2.mk
+++ stages/stage2/stage2.mk
@@ -1,10 +1,2 @@
 stages += stage2
 linux-stages += stage2
-
-build/initfs/common.tar: \
-	build/initfs/common/bin/stage2
-
-build/initfs/common/bin/stage2: stages/stage2/init
-	$(MKDIR) $(dir $@)
-	$(CP) $< $@
-	chmod ug+x $@
