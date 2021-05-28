64617d9e104c28d51b8cb9a765ec91e2c56b3d2f
Pip Cet <pipcet@gmail.com>
2021-05-28 12:29:06 +0000

    stages/stage1/stage1.mk: common cpio stuff

--- stages/stage1/stage1.mk
+++ stages/stage1/stage1.mk
@@ -18,3 +18,10 @@ build/stages/stage1/stage1.cpio: \
 	build/initfs/common.cpio \
 	build/initfs/common.tar
 	(cd build/linux/linux; $(PWD)/submodule/linux/usr/gen_initramfs.sh -o $(PWD)/$@ ../../../$<)
+
+build/stages/stage1/stage1.image: \
+	build/initfs/complete.cpio
+
+build/initfs/common.tar: \
+	build/initfs/common/stage2.image \
+	build/initfs/common/stage2.dtb
