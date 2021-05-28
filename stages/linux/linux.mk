7e4a3f843a260600b99a62f4e41120c3c26e911c
Pip Cet <pipcet@gmail.com>
2021-05-28 21:30:05 +0000

    stages/linux/linux.mk: rm old code

--- stages/linux/linux.mk
+++ stages/linux/linux.mk
@@ -1,11 +1,2 @@
 stages += linux
 linux-stages += linux
-
-build/initfs/common.tar: \
-	build/initfs/common/boot/linux.image \
-	build/initfs/common/boot/linux.dtb
-
-build/initfs/common/bin/linux: stages/linux/init
-	$(MKDIR) $(dir $@)
-	$(CP) $< $@
-	chmod ug+x $@
