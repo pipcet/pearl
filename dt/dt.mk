0fad4850729737aa8b331446f0d4c48e6c601a7e
Pip Cet <pipcet@gmail.com>
2021-05-29 19:37:37 +0000

    dt/dt.mk: add rule

--- dt/dt.mk
+++ dt/dt.mk
@@ -10,3 +10,6 @@ build/dt/bin/adtp: dt/adtp.cc
 
 build/dt.tar: build/dt/bin/dt build/dt/bin/adtdump build/dt/bin/adtp
 	(cd build/dt; tar c bin/dt bin/adtdump bin/adtp) > $@
+
+build/%.dts.dtp: build/%.dts build/dt/bin/dt
+	build/dt/bin/dt dts-to-dtp $< $@
