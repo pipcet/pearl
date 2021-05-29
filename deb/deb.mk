c11b54ea8e1f9a8ff6a97138864180ba40f815fc
Pip Cet <pipcet@gmail.com>
2021-05-29 10:51:45 +0000

    deb/deb.mk: piece together things from the Debian repo

--- deb/deb.mk
+++ deb/deb.mk
@@ -1,47 +1,24 @@
 # XXX we need a better way to find the precise latest sid versions of debs
 # (without installing all of Debian)
 
-build/deb/libc.deb: | build/deb/
-	wget -O $@ http://http.us.debian.org/debian/pool/main/g/glibc/libc6_2.31-12_arm64.deb
+build/deb/Packages: | build/deb/
+	curl http://http.us.debian.org/debian/dists/sid/main/binary-arm64/Packages.xz | xzcat > $@
+	curl http://http.us.debian.org/debian/dists/sid/main/binary-all/Packages.xz | xzcat >> $@
 
-build/deb/libcrypt.deb: | build/deb/
-	wget -O $@ http://http.us.debian.org/debian/pool/main/libx/libxcrypt/libcrypt1_4.4.18-4_arm64.deb
-
-build/deb/perl.deb: | build/deb/
-	wget -O $@ http://http.us.debian.org/debian/pool/main/p/perl/perl_5.32.1-4_arm64.deb
-
-build/deb/perl-modules.deb: | build/deb/
-	wget -O $@ http://http.us.debian.org/debian/pool/main/p/perl/perl-modules-5.32_5.32.1-4_all.deb
-
-build/deb/perl-base.deb: | build/deb/
-	wget -O $@ http://http.us.debian.org/debian/pool/main/p/perl/perl-base_5.32.1-4_arm64.deb
-
-build/deb/libfile-slurp-perl.deb: | build/deb/
-	wget -O $@ http://http.us.debian.org/debian/pool/main/libf/libfile-slurp-perl/libfile-slurp-perl_9999.32-1_all.deb
-
-build/deb/libipc-run-perl.deb: | build/deb/
-	wget -O $@ http://http.us.debian.org/debian/pool/main/libi/libipc-run-perl/libipc-run-perl_20200505.0-1_all.deb
-
-build/deb/device-tree-compiler.deb: | build/deb/
-	wget -O $@ http://http.us.debian.org/debian/pool/main/d/device-tree-compiler/device-tree-compiler_1.6.0-1_arm64.deb
-
-build/deb/libfdt1.deb: | build/deb/
-	wget -O $@ http://http.us.debian.org/debian/pool/main/d/device-tree-compiler/libfdt1_1.6.0-1_arm64.deb
-
-build/deb/libyaml.deb: | build/deb/
-	wget -O $@ http://http.us.debian.org/debian/pool/main/liby/libyaml/libyaml-0-2_0.2.2-1_arm64.deb
+build/deb/%.deb: build/deb/Packages deb/deb.pl | build/deb/
+	wget -O $@ http://http.us.debian.org/debian/$(shell perl deb/deb.pl "$*" < $<)
 
 build/deb.tar: \
-	build/deb/libc.deb \
-	build/deb/libcrypt.deb \
+	build/deb/libc6.deb \
+	build/deb/libcrypt1.deb \
 	build/deb/perl.deb \
 	build/deb/perl-base.deb \
-	build/deb/perl-modules.deb \
+	build/deb/perl-modules-5.32.deb \
 	build/deb/libfile-slurp-perl.deb \
 	build/deb/libipc-run-perl.deb \
 	build/deb/device-tree-compiler.deb \
 	build/deb/libfdt1.deb \
-	build/deb/libyaml.deb
+	build/deb/libyaml-0-2.deb
 	$(MKDIR) build/deb-tmp
 	for file in $^; do dpkg -x $$file build/deb-tmp; done
 	(cd build/deb-tmp; tar c .) > $@
