8e7aeeced00f483ac6248ad2a3d473ee9c6d5aaf
Pip Cet <pipcet@gmail.com>
2021-05-29 17:10:55 +0000

    deb/deb.mk: more debs

--- deb/deb.mk
+++ deb/deb.mk
@@ -8,17 +8,17 @@ build/deb/Packages: | build/deb/
 build/deb/%.deb: build/deb/Packages deb/deb.pl | build/deb/
 	curl http://http.us.debian.org/debian/$(shell perl deb/deb.pl "$*" < $<) > $@
 
+libc-debs = libc6 libcrypt1
+perl-debs = perl perl-base perl-modules-5.32 libfile-slurp-perl libipc-run-perl
+dtc-debs = device-tree-compiler libfdt1 libyaml-0-2
+lvm-debs = lvm2 dmsetup dmeventd cryptsetup cryptsetup-bin libblkid1 libcryptsetup12 libpopt0 libuuid1
+
 build/deb.tar: \
-	build/deb/libc6.deb \
-	build/deb/libcrypt1.deb \
-	build/deb/perl.deb \
-	build/deb/perl-base.deb \
-	build/deb/perl-modules-5.32.deb \
-	build/deb/libfile-slurp-perl.deb \
-	build/deb/libipc-run-perl.deb \
-	build/deb/device-tree-compiler.deb \
-	build/deb/libfdt1.deb \
-	build/deb/libyaml-0-2.deb
+	$(libc-debs:%=build/deb/%.deb) \
+	$(perl-debs:%=build/deb/%.deb) \
+	$(dtc-debs:%=build/deb/%.deb) \
+	$(lvm-debs:%=build/deb/%.deb)
+	rm -rf build/deb-tmp
 	$(MKDIR) build/deb-tmp
 	for file in $^; do dpkg -x $$file build/deb-tmp; done
 	(cd build/deb-tmp; tar c .) > $@
