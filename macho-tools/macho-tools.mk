b9d089f42c0628313f845998d451fd4e907ede82
Pip Cet <pipcet@gmail.com>
2021-05-30 20:41:49 +0000

    macho-tools/macho-tools.mk: rules for macho-to-image

--- macho-tools/macho-tools.mk
+++ macho-tools/macho-tools.mk
@@ -9,5 +9,12 @@ build/host/image-to-macho: build/macho-tools/restore-boot-args..h
 build/host/image-to-macho: build/macho-tools/nop..h
 build/host/image-to-macho: build/macho-tools/bring-up-phys..h
 
+build/host/macho-to-image: macho-tools/macho-to-image.c | build/host/
+	gcc -Os -Ibuild/macho-tools -o $@ $<
+
+build/host/macho-to-image: build/macho-tools/macho-boot..h
+build/host/macho-to-image: build/macho-tools/image-header..h
+build/host/macho-to-image: build/macho-tools/disable-timers..h
+
 build/%.image.macho: build/%.image build/host/image-to-macho
 	build/host/image-to-macho $< $@
