104d0b554b5073a6c5b279c0fc104cb387e23f97
Pip Cet <pipcet@gmail.com>
2021-05-30 09:08:06 +0000

    linux/linux.mk: adjust for images

--- linux/linux.mk
+++ linux/linux.mk
@@ -37,6 +37,45 @@ build/initfs/common/boot/$(stage).image: build/stages/$(stage)/$(stage).image
 	$$(CP) $$< $$@
 endef
 
+define linux-perimage
+build/images/$(image)/linux.config: images/$(image)/linux.config
+	$$(MKDIR) $$(dir $$@)
+	$$(CP) $$< $$@
+
+linux/$(image){oldconfig}: build/images/$(image)/linux.config
+	$$(MKDIR) build/linux/$(image)
+	$$(CP) $$< build/linux/$(image)/.config
+	$$(MAKE) -C submodule/linux ARCH=arm64 CROSS_COMPILE=$$(CROSS_COMPILE) O=$(PWD)/build/linux/$(image) oldconfig
+	diff -u $$< build/linux/$(image)/.config || true
+	$$(CP) $$< $$<.old
+	$$(CP) build/linux/$(image)/.config $$<
+
+linux/$(image){menuconfig}:
+	$$(MKDIR) build/linux/$(image)
+	$$(CP) images/$(image)/linux.config build/linux/$(image)/.config
+	$$(MAKE) -C submodule/linux ARCH=arm64 CROSS_COMPILE=$$(CROSS_COMPILE) O=$(PWD)/build/linux/$(image) menuconfig
+	diff -u images/$(image)/linux.config build/linux/$(image)/.config || true
+	$$(CP) build/linux/$(image)/.config images/$(image)/linux.config
+
+
+build/images/$(image)/$(image).image: build/images/$(image)/linux.config
+	$$(MKDIR) build/linux/$(image)
+	$$(CP) $$< build/linux/$(image)/.config
+	$$(MAKE) -C submodule/linux ARCH=arm64 CROSS_COMPILE=$$(CROSS_COMPILE) O=$(PWD)/build/linux/$(image) oldconfig
+	diff -u $$< build/linux/$(image)/.config || true
+	$$(MAKE) -C build/linux/$(image) ARCH=arm64 CROSS_COMPILE=$$(CROSS_COMPILE) Image dtbs
+	$$(CP) build/linux/$(image)/arch/arm64/boot/Image $$@
+
+build/images/$(image)/$(image).dtb: build/images/$(image)/$(image).image
+	$$(CP) build/linux/$(image)/arch/arm64/boot/dts/apple/apple-m1-j293.dtb $$@
+
+build/images/$(image)/$(image).image: stamp/linux
+
+build/initfs/common/boot/$(image).image: build/images/$(image)/$(image).image
+	$$(MKDIR) $$(dir $$@)
+	$$(CP) $$< $$@
+endef
+
 build/initfs/common/dt.tar: build/dt.tar
 	$(MKDIR) $(dir $@)
 	$(CP) $< $@
