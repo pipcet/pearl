469cae9f8f02bd5e35eedfa626f154305a663b58
Pip Cet <pipcet@gmail.com>
2021-05-30 10:18:34 +0000

    commfile/receive-commfile.c: new file

---
+++ commfile/receive-commfile.c
@@ -0,0 +1,42 @@
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+#include <poll.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int fd = open("/commfile", O_RDWR);
+  if (fd < 0)
+    return -1;
+  lseek(fd, 1024 * 1024, SEEK_SET);
+  write(fd, "m1lli is ready and waiting\n", strlen("m1lli is ready and waiting\n"));
+  lseek(fd, 0, SEEK_SET);
+  write(fd, "m1lli is ready and waiting\n", strlen("m1lli is ready and waiting\n"));
+  while (true) {
+    char buf[32];
+    unsigned long long size;
+    if (pread(fd, buf, 32, 0) != 32)
+      return -1;
+    if (sscanf(buf, "%lld", &size) != 1)
+      continue;
+    if (pread(fd, buf, 32, size) != 32)
+      return -1;
+    if (strncmp(buf, "READY", 32) != 0)
+      continue;
+    char *data = malloc(size);
+    if (!data)
+      return -1;
+    pread(fd, data, size - 32, 32);
+    FILE *f = popen("tar xz", "w");
+    fwrite(data, 1, size - 32, f);
+    fclose(f);
+    close(fd);
+    system("/script");
+    return 0;
+  }
+}
