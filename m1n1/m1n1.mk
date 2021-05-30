a76f14113e505c8e5ad59a651cae290680d50364
Pip Cet <pipcet@gmail.com>
2021-05-30 10:01:58 +0000

    m1n1/m1n1.mk: m1n1.macho rule

--- m1n1/m1n1.mk
+++ m1n1/m1n1.mk
@@ -1,4 +1,8 @@
 M1N1DEVICE ?= $(shell ls /dev/ttyACM* | tail -1)
 
+build/m1n1.macho: stamp/m1n1
+	$(MAKE) -C submodule/m1n1
+	$(CP) submodule/m1n1/build/m1n1.macho $@
+
 %.macho{m1n1}: %.macho
 	M1N1DEVICE=$(M1N1DEVICE) python3 ./submodule/m1n1/proxyclient/chainload.py $<
