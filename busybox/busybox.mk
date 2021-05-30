1ed0db34911b66998fdaabfb34da061875062874
Pip Cet <pipcet@gmail.com>
2021-05-30 11:04:50 +0000

    busybox/busybox.mk: new rule for oldconfig

--- busybox/busybox.mk
+++ busybox/busybox.mk
@@ -1,4 +1,6 @@
-build/busybox/busybox: busybox/busybox.config stamp/busybox | build/busybox/
+busybox/busybox{oldconfig}: busybox/busybox.config stamp/busybox | build/busybox/
 	$(CP) $< build/busybox/.config
 	$(MAKE) -C submodule/busybox O=$(PWD)/build/busybox oldconfig
-	$(MAKE) -C submodule/busybox O=$(PWD)/build/busybox
+	$(CP) build/busybox/.config $<
+
+build/busybox/busybox: busybox/busybox.config stamp/busybox | build/busybox/
