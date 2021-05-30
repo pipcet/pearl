76f04906bc732f4e9d9f566abb9a000e59f43027
Pip Cet <pipcet@gmail.com>
2021-05-30 10:19:41 +0000

    packs/packs.mk: include receive-commfile

--- packs/packs.mk
+++ packs/packs.mk
@@ -55,24 +55,30 @@ build/packs/$(pack)/deb.tar: build/deb.tar
 	$$(MKDIR) $$(dir $$@)
 	$$(CP) $$< $$@
 
-build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/kexec
+build/packs/$(pack)/bin/receive-commfile: build/commfile/receive-commfile
+	$$(MKDIR) $$(dir $$@)
+	$$(CP) $$< $$@
+
+build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/adtdump
+build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/adtp
 build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/busybox
-build/packs/$(pack).cpiospec: build/packs/$(pack)/init
+build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/dt
+build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/gadget
+build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/kexec
+build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/linux
+build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/receive-commfile
 build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/stage1
 build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/stage2
-build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/linux
-build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/gadget
-build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/dt
-build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/adtp
-build/packs/$(pack).cpiospec: build/packs/$(pack)/bin/adtdump
-build/packs/$(pack).cpiospec: build/packs/$(pack)/boot/stage2.image
-build/packs/$(pack).cpiospec: build/packs/$(pack)/boot/stage2.dtb
-build/packs/$(pack).cpiospec: build/packs/$(pack)/boot/linux.image
 build/packs/$(pack).cpiospec: build/packs/$(pack)/boot/linux.dtb
+build/packs/$(pack).cpiospec: build/packs/$(pack)/boot/linux.image
+build/packs/$(pack).cpiospec: build/packs/$(pack)/boot/stage2.dtb
+build/packs/$(pack).cpiospec: build/packs/$(pack)/boot/stage2.image
 build/packs/$(pack).cpiospec: build/packs/$(pack)/deb.tar
+build/packs/$(pack).cpiospec: build/packs/$(pack)/init
 
 build/packs/$(pack).cpio: build/packs/$(pack).cpiospec build/stages/linux/linux.image
 	(cd build/linux/linux; $$(PWD)/submodule/linux/usr/gen_initramfs.sh -o $$(PWD)/$$@ ../../../$$<)
+
 endef
 
 $(foreach pack,$(packs),$(eval $(perpack)))
