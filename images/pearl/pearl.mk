6a34b3c854ac87d233bdd464b46caf11a5d6d24f
Pip Cet <pipcet@gmail.com>
2021-05-30 09:06:32 +0000

    images/pearl/pearl.mk: new file

---
+++ images/pearl/pearl.mk
@@ -0,0 +1,11 @@
+images += pearl
+linux-images += pearl
+
+build/images/pearl/pearl.image: | build/images/pearl/
+build/images/pearl/pearl.image: build/packs/pearl.cpio
+
+build/images/pearl/pearl.image: build/images/pearl/pearl.dts.dtb.h
+
+build/images/pearl/pearl.dts: images/pearl/pearl.dts
+	$(MKDIR) $(dir $@)
+	$(CP) $< $@
