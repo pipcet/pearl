d823e3287e746435d671d5ba32d120842f45b6fa
Pip Cet <pipcet@gmail.com>
2021-05-30 13:18:41 +0000

    deb/deb.mk: chase down way too many dependencies

--- deb/deb.mk
+++ deb/deb.mk
@@ -11,7 +11,42 @@ build/deb/%.deb: build/deb/Packages deb/deb.pl | build/deb/
 libc-debs = libc6 libcrypt1
 perl-debs = perl perl-base perl-modules-5.32 libfile-slurp-perl libipc-run-perl
 dtc-debs = device-tree-compiler libfdt1 libyaml-0-2
-lvm-debs = lvm2 dmsetup dmeventd cryptsetup cryptsetup-bin libblkid1 libcryptsetup12 libpopt0 libuuid1 libdevmapper1.02.1 libdevmapper-event1.02.1 libedit2 libudev1
+lvm-debs = \
+	lvm2 \
+	cryptsetup \
+	cryptsetup-bin \
+	dmeventd \
+	dmsetup \
+	libaio1 \
+	libargon2-1 \
+	libblkid1 \
+	libbsd0 \
+	libcryptsetup12 \
+	libdevmapper-event1.02.1 \
+	libdevmapper1.02.1 \
+	libedit2 \
+	libgcc-s1 \
+	libgcrypt20 \
+	libgpg-error0 \
+	libjson-c5 \
+	liblz4-1 \
+	liblzma5 \
+	libmd0 \
+	libpcre2 \
+	libpcre2-8 \
+	libpcre2-8-0 \
+	libpcre2-16 \
+	libpcre2-16-0 \
+	libpcre2-32 \
+	libpcre2-32-0 \
+	libpopt0 \
+	libselinux1 \
+	libssl1.1 \
+	libsystemd0 \
+	libtinfo6 \
+	libudev1 \
+	libuuid1 \
+	libzstd1
 
 build/deb.tar: \
 	$(libc-debs:%=build/deb/%.deb) \
