82f7003487044cb9be510b362d815027ae76f976
Pip Cet <pipcet@gmail.com>
2021-05-30 10:40:46 +0000

    commfile/commfile.mk: commfile rules

--- commfile/commfile.mk
+++ commfile/commfile.mk
@@ -5,3 +5,11 @@ build/commfile/send-commfile: commfile/send-commfile
 build/commfile/receive-commfile: commfile/receive-commfile.c
 	$(MKDIR) $(dir $@)
 	$(CROSS_COMPILE)gcc -static -Os -o $@ $<
+
+%.image.commfile: %.image
+	$(MKDIR) $@.d
+	$(CP) $< $@.d
+	tar -C $@.d -c . > $@
+
+%.commfile{send}: %.commfile
+	$(SUDO) commfile/send-commfile $<
