4efc6fb079dbd22daea1b5c2ef63af4ea639d669
Pip Cet <pipcet@gmail.com>
2021-05-30 12:07:28 +0000

    busybox/busybox.mk: oops, restore build code

--- busybox/busybox.mk
+++ busybox/busybox.mk
@@ -4,3 +4,5 @@ busybox/busybox{oldconfig}: busybox/busybox.config stamp/busybox | build/busybox
 	$(CP) build/busybox/.config $<
 
 build/busybox/busybox: busybox/busybox.config stamp/busybox | build/busybox/
+	$(CP) $< build/busybox/.config
+	$(MAKE) -C submodule/busybox O=$(PWD)/build/busybox
