c33308a3c78a3c8501be5ae300e9d95ebf0eb859
Pip Cet <pipcet@gmail.com>
2021-05-31 08:21:09 +0000

    macho-tools/macho-boot.c: update

--- macho-tools/macho-boot.c
+++ macho-tools/macho-boot.c
@@ -1,23 +1,9 @@
 #include "snippet.h"
 
-void boot_macho_init(unsigned long long *, unsigned long)
-  __attribute__((section(".text")));
-volatile register void *top_of_mem __asm__("x11");
-
-START_SNIPPET {
-} END_SNIPPET
-
 #include <stddef.h>
 #include <stdint.h>
 typedef unsigned long u64;
 typedef unsigned u32;
-
-static inline void *memalign(size_t align, size_t size);
-static inline void *memset(void *p, int c, size_t size);
-
-#define NULL ((void *)0)
-
-#define PRELUDE_SIZE 256 * 1024
 
 #define MACHO_COMMAND_UNIX_THREAD 0x05
 #define MACHO_COMMAND_SEGMENT_64  0x19
@@ -49,22 +35,51 @@ struct macho_command {
   } u;
 } __attribute__((packed));
 
-void boot_macho_init(unsigned long long *arg, unsigned long original_base)
+#define FOR_EACH_COMMAND(command)					\
+  for(command = (void *)(header + 1),					\
+	last_command = ((void *)command) + header->cmdsize;		\
+      command < last_command;						\
+      command = ((void*)command) + command->size)
+
+static inline void memcpy(void *p, void *q, size_t len)
 {
-  original_base -= 0x2000;
-  unsigned long ptr = *(long *)arg;
-  unsigned long flags;
-  top_of_mem = (void *)0xb00000000;
-  void * volatile start = ((void *)arg) - 0x48 + 256 * 1024;
-  struct macho_header *header = start;
-  struct macho_command *command = ((void *)header) + 32;
-  void *last_command = ((void *)command) + header->cmdsize;
-  u64 pc = 0;
+  unsigned __int128 *p128 = p;
+  unsigned __int128 *q128 = q;
+
+  len += 15;
+  len &= -16L;
+  while (len) {
+    *p128++ = *q128++;
+    len -= 16;
+  }
+}
+
+static inline void memset(void *p, int c_ignored, size_t len)
+{
+  unsigned __int128 *p128 = p;
+
+  len += 15;
+  len &= -16L;
+  while (len) {
+    *p128++ = 0;
+    len -= 16;
+  }
+}
+
+void boot_macho(unsigned long, void *, void *)
+  __attribute__((noreturn));
+
+extern void macho_entry(unsigned long, unsigned long) __attribute__((noreturn));
+
+void boot_macho(unsigned long bootargs, void *macho, void *base)
+{
   u64 vmin = (u64)-1, vmax = 0;
-  u64 vmtotalsize = 0;
-  int count = 0;
-  while ((void *)command < last_command) {
-    switch (command->type) {
+  u64 fmin = (u64)-1, fmax = 0;
+  u64 pc = 0;
+  struct macho_header *header = (struct macho_header *)macho;
+  struct macho_command *command, *last_command;
+  FOR_EACH_COMMAND(command) {
+    switch(command->type) {
     case MACHO_COMMAND_UNIX_THREAD:
       pc = command->u.unix_thread.pc;
       break;
@@ -76,74 +91,47 @@ void boot_macho_init(unsigned long long *arg, unsigned long original_base)
 	vmin = vmaddr;
       if (vmax <= vmaddr + vmsize)
 	vmax = vmaddr + vmsize;
+
+      u64 faddr = command->u.segment_64.fileoff;
+      u64 fsize = command->u.segment_64.filesize;
+
+      if (fmin >= faddr)
+	fmin = faddr;
+      if (fmax <= faddr + fsize)
+	fmax = faddr + fsize;
     }
     }
-    command = ((void *)command) + command->size;
   }
-  vmtotalsize = vmax - vmin;
-  vmtotalsize = 0;
-  vmtotalsize += 16384;
-  vmtotalsize &= -16384L;
-  memset(top_of_mem, (count^=0xff), 10240 * 1600);
-  void *dest = memalign(1, vmtotalsize);
-  dest = top_of_mem;
-  for (size_t count = 0; count < vmtotalsize; count++)
-    ((char*)dest)[count] = 0;
-  void *virtpc = NULL;
-  command = ((void *)header) + 32;
-  while ((void *)command < last_command) {
+  vmin -= fmin;
+  fmin = 0;
+  u64 vlen = vmax - vmin;
+  u64 flen = fmax - fmin;
+  void *safebase = base + vlen;
+  if (safebase != macho) {
+    memcpy(safebase, macho, flen);
+    typeof(&boot_macho) new_boot_macho;
+    new_boot_macho = boot_macho;
+    new_boot_macho = ((void *)new_boot_macho) + (safebase - macho);
+    boot_macho(bootargs, safebase, base);
+  }
+  typeof (&macho_entry) actual_pc;
+  actual_pc = (void *)pc - (void *)vmin + base;
+  u64 phys_pc = (u64)-1;
+  FOR_EACH_COMMAND(command) {
     switch (command->type) {
     case MACHO_COMMAND_SEGMENT_64: {
       u64 vmaddr = command->u.segment_64.vmaddr;
       u64 vmsize = command->u.segment_64.vmsize;
-      u64 fileoff = command->u.segment_64.fileoff;
-      u64 filesize = command->u.segment_64.filesize;
-      u64 pcoff = pc - vmaddr;
+      u64 faddr = command->u.segment_64.fileoff;
+      u64 fsize = command->u.segment_64.filesize;
 
-      for (size_t count = 0; count < vmsize; count++)
-	///((char *)0xbdf438000)[(vmaddr - vmin + count) % (2560*1600*4)] =
-	  ((char*)dest)[vmaddr - vmin + count] =
-	  ((char *)start)[fileoff + count];
-      if (pcoff < vmsize) {
-	if (pcoff < filesize) {
-	  virtpc = dest + vmaddr - vmin + pcoff;
-	  //virtpc = start + fileoff + pcoff;
-	}
-      }
+      memset((void *)base + vmaddr - vmin, 0, vmsize);
+      memcpy((void *)base + vmaddr - vmin, safebase + faddr, fsize);
     }
     }
-    command = ((void *)command) + command->size;
   }
-  if (virtpc == NULL)
-    return;
+
   asm volatile("isb");
-  ((void (*)(unsigned long, unsigned long))virtpc)(ptr, original_base);
+
+  actual_pc(bootargs, bootargs);
 }
-
-static inline void *memset(void *p, int c, size_t size)
-{
-  char *p2 = p;
-  while (size--) *p2++ = c;
-  return p;
-}
-
-static inline void *memalign(size_t align, size_t size)
-{
-  while (((size_t)top_of_mem) & (align - 1))
-    top_of_mem += align - ((unsigned long)top_of_mem & (align - 1));
-
-  void *ret = top_of_mem;
-  top_of_mem += size;
-  return ret;
-}
-
-unsigned int bswap(unsigned int x)
-{
-  return __builtin_bswap32(x);
-}
-
-unsigned long bswap64(unsigned long x)
-{
-  return __builtin_bswap64(x);
-}
-
