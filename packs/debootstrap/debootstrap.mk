cac55a010bc48c36c9ea726bc97a48b9376e9823
Pip Cet <pipcet@gmail.com>
2021-05-31 00:37:47 +0000

    packs/debootstrap/debootstrap.mk: include blobs

--- packs/debootstrap/debootstrap.mk
+++ packs/debootstrap/debootstrap.mk
@@ -4,7 +4,12 @@ common-debootstrap = devdir.cpio kexec.tar
 stages-debootstrap = stage2 linux
 
 build/packs/debootstrap.cpiospec: build/packs/debootstrap/debootstrap.tar
+build/packs/debootstrap.cpiospec: build/packs/debootstrap/blobs.tar
 
 build/packs/debootstrap/debootstrap.tar: build/debootstrap/stage15.tar
+	$(MKDIR) $(dir $@)
+	$(CP) $< $@
+
+build/packs/debootstrap/blobs.tar: build/blobs.tar
 	$(MKDIR) $(dir $@)
 	$(CP) $< $@
