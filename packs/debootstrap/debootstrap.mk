a990b93c62981adbe7b9c1aab77d574d7b13a260
Pip Cet <pipcet@gmail.com>
2021-05-31 11:48:59 +0000

    packs/debootstrap/debootstrap.mk: clean up

--- packs/debootstrap/debootstrap.mk
+++ packs/debootstrap/debootstrap.mk
@@ -6,10 +6,5 @@ stages-debootstrap = stage2 linux
 build/packs/debootstrap.cpiospec: build/packs/debootstrap/debootstrap.tar
 build/packs/debootstrap.cpiospec: build/packs/debootstrap/blobs.tar
 
-build/packs/debootstrap/debootstrap.tar: build/debootstrap/stage15.tar
-	$(MKDIR) $(dir $@)
-	$(CP) $< $@
-
-build/packs/debootstrap/blobs.tar: build/blobs.tar
-	$(MKDIR) $(dir $@)
-	$(CP) $< $@
+build/packs/debootstrap/debootstrap.tar: build/debootstrap/stage15.tar; $$(COPY)
+build/packs/debootstrap/blobs.tar: build/blobs.tar ; $$(COPY)
