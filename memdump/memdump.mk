c870e73732e913ecdac8875971385c100a792636
Pip Cet <pipcet@gmail.com>
2021-06-01 20:54:21 +0000

    memdump/memdump.mk: more make magic

--- memdump/memdump.mk
+++ memdump/memdump.mk
@@ -1,3 +1,6 @@
 build/memdump/macho-to-memdump: memdump/macho-to-memdump ; $(COPY)
 build/memdump/memdump-to-image: memdump/memdump-to-image.c | build/memdump/
-	$(CROSS_COMPILE)gcc -static -Os -Ibuild/macho-tools -o $@ $<
+	$(CROSS_COMPILE)gcc -static -Os -Ibuild/memdump -o $@ $<
+
+build/memdump/memdump-to-image: build/memdump/boot..h
+build/memdump/memdump-to-image: build/memdump/image..h
