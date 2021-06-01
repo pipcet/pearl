9913fe17e3c4c54b62342ea3d00170d4d35c0331
Pip Cet <pipcet@gmail.com>
2021-06-01 20:47:43 +0000

    memdump/memdump.mk: minor fix
    need to install this on the target

--- memdump/memdump.mk
+++ memdump/memdump.mk
@@ -1,3 +1,3 @@
-build/host/macho-to-memdump: memdump/macho-to-memdump ; $(COPY)
+build/memdump/macho-to-memdump: memdump/macho-to-memdump ; $(COPY)
 build/memdump/memdump-to-image: memdump/memdump-to-image.c | build/memdump/
 	gcc -Os -Ibuild/macho-tools -o $@ $<
