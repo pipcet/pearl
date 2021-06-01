c3fd36822e8aa1a5ba2227e4b38342ebc9f56626
Pip Cet <pipcet@gmail.com>
2021-06-01 22:00:21 +0000

    memdump/memdump-to-image.c: fix things to the point where m1n1 loads

--- memdump/memdump-to-image.c
+++ memdump/memdump-to-image.c
@@ -22,12 +22,24 @@ static
 #include "image..h"
 ;
 
+extern void macho_entry(void *) __attribute__((noreturn));
+
+struct memdump_args {
+  unsigned long header[10];
+  void *current_base;
+  void *bootargs;
+  void *original_base;
+  unsigned long length_of_header;
+  unsigned long length_of_memdump;
+  typeof(&macho_entry) entry;
+};
+
 int main(int argc, char **argv)
 {
   const size_t prelude_size = PRELUDE_SIZE;
-  if (argc != 4) {
+  if (argc != 6) {
   error:
-    fprintf(stderr, "usage: %s <memdump image> <base address> <pc>\n",
+    fprintf(stderr, "usage: %s <memdump image> <Linux image> <bootargs> <base address> <pc>\n",
 	    argv[0]);
     exit(1);
   }
@@ -36,8 +48,9 @@ int main(int argc, char **argv)
   if (!f)
     goto error;
 
-  u64 base = strtoll(argv[2], NULL, 0);
-  u64 pc = strtoll(argv[3], NULL, 0);
+  u64 bootargs = strtoll(argv[3], NULL, 0);
+  u64 original_base = strtoll(argv[4], NULL, 0);
+  u64 pc = strtoll(argv[5], NULL, 0);
 
   fseek(f, 0, SEEK_END);
   size_t memdump_size = ftell(f);
@@ -56,6 +69,13 @@ int main(int argc, char **argv)
   //p += sizeof(disable_timers);
   memcpy(p, boot, sizeof(boot));
   p += sizeof(boot);
+
+  struct memdump_args *args = buf;
+  args->bootargs = (void *)bootargs;
+  args->original_base = (void *)original_base;
+  args->length_of_header = prelude_size;
+  args->length_of_memdump = memdump_size;
+  args->entry = (void*)pc;
 
   void *memdump = buf + prelude_size;
   assert(p <= memdump);
