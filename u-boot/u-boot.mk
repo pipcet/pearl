f28fc83ccedeea1b4e32db7069becec5b614937b
Pip Cet <pipcet@gmail.com>
2021-06-02 12:08:32 +0000

    u-boot/u-boot.mk: dependency for u-boot.dtb

--- u-boot/u-boot.mk
+++ u-boot/u-boot.mk
@@ -2,5 +2,7 @@ build/u-boot/u-boot.bin: stamp/u-boot | build/u-boot/
 	$(MAKE) -C submodule/u-boot O=$(PWD)/build/u-boot CROSS_COMPILE=$(CROSS_COMPILE) apple_m1_defconfig
 	$(MAKE) -C submodule/u-boot O=$(PWD)/build/u-boot CROSS_COMPILE=$(CROSS_COMPILE)
 
+build/u-boot/u-boot.dtb: build/u-boot/u-boot.bin
+
 build/u-boot.image.gz: build/u-boot/u-boot.bin
 	gzip < $< > $@
