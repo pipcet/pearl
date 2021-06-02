1c3536f06a235cf8b2a7640a4c03504d88fc02e1
Pip Cet <pipcet@gmail.com>
2021-06-02 07:56:57 +0000

    u-boot/u-boot.mk: minor fixes

--- u-boot/u-boot.mk
+++ u-boot/u-boot.mk
@@ -1,3 +1,6 @@
-build/u-boot/u-boot.img: stamp/u-boot | build/u-boot/
+build/u-boot/u-boot.bin: stamp/u-boot | build/u-boot/
 	$(MAKE) -C submodule/u-boot O=$(PWD)/build/u-boot CROSS_COMPILE=$(CROSS_COMPILE) apple_m1_defconfig
 	$(MAKE) -C submodule/u-boot O=$(PWD)/build/u-boot CROSS_COMPILE=$(CROSS_COMPILE)
+
+build/u-boot.image.gz: build/u-boot/u-boot.bin
+	gzip < $< > $@
