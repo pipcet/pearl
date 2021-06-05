aa0702fae8a576e6de759e04fc916c8f552840ff
Pip Cet <pipcet@gmail.com>
2021-06-05 20:37:30 +0000

    busybox/busybox.mk: support menuconfig

--- busybox/busybox.mk
+++ busybox/busybox.mk
@@ -1,3 +1,8 @@
+busybox/busybox{menuconfig}: busybox/busybox.config stamp/busybox | build/busybox/
+	$(CP) $< build/busybox/.config
+	$(MAKE) -C submodule/busybox O=$(PWD)/build/busybox menuconfig
+	$(CP) build/busybox/.config $<
+
 busybox/busybox{oldconfig}: busybox/busybox.config stamp/busybox | build/busybox/
 	$(CP) $< build/busybox/.config
 	$(MAKE) -C submodule/busybox O=$(PWD)/build/busybox oldconfig
