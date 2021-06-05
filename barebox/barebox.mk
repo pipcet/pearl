9b99fabad9e848d0366f85f104ca48fafdbe96ab
Pip Cet <pipcet@gmail.com>
2021-06-05 08:31:19 +0000

    barebox/barebox.mk: new file

---
+++ barebox/barebox.mk
@@ -0,0 +1,8 @@
+build/barebox/images/barebox-dt-2nd.img: stamp/barebox | build/barebox/
+	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox CROSS_COMPILE=$(CROSS_COMPILE) ARCH=arm64 menuconfig
+	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox CROSS_COMPILE=$(CROSS_COMPILE) ARCH=arm64
+
+build/barebox/barebox.dtb: build/barebox/barebox.bin
+
+build/barebox.image.gz: build/barebox/barebox.bin
+	gzip < $< > $@
