2cd6d76e97de2f5526f3e11d7a8430674f26ba70
Pip Cet <pipcet@gmail.com>
2021-06-05 08:32:33 +0000

    barebox/barebox.mk: minor fix

--- barebox/barebox.mk
+++ barebox/barebox.mk
@@ -1,5 +1,5 @@
 build/barebox/images/barebox-dt-2nd.img: stamp/barebox | build/barebox/
-	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox CROSS_COMPILE=$(CROSS_COMPILE) ARCH=arm64 menuconfig
+	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox CROSS_COMPILE=$(CROSS_COMPILE) ARCH=arm64 oldconfig
 	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox CROSS_COMPILE=$(CROSS_COMPILE) ARCH=arm64
 
 build/barebox/barebox.dtb: build/barebox/barebox.bin
