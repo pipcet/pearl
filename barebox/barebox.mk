634b1ad4cd8aaf6f4b968da5b0391fd88a8572d0
Pip Cet <pipcet@gmail.com>
2021-06-05 20:37:20 +0000

    barebox/barebox.mk: support menuconfig etc

--- barebox/barebox.mk
+++ barebox/barebox.mk
@@ -1,5 +1,15 @@
-build/barebox/images/barebox-dt-2nd.img: stamp/barebox | build/barebox/
-	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox CROSS_COMPILE=$(CROSS_COMPILE) ARCH=arm64 oldconfig
+barebox/barebox{menuconfig}: barebox/barebox.config stamp/barebox | build/barebox/
+	$(CP) $< build/barebox/.config
+	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) menuconfig
+	$(CP) build/barebox/.config $<
+
+barebox/barebox{oldconfig}: barebox/barebox.config stamp/barebox | build/barebox/
+	$(CP) $< build/barebox/.config
+	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox oldconfig ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE)
+	$(CP) build/barebox/.config $<
+
+build/barebox/images/start_dt_2nd.pblb: barebox/barebox.config stamp/barebox | build/barebox/
+	$(CP) $< build/barebox/.config
 	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox CROSS_COMPILE=$(CROSS_COMPILE) ARCH=arm64
 
 build/barebox/barebox.dtb: build/barebox/barebox.bin
