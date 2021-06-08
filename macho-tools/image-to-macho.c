8e605cdf591f6fffca55226a34d365cdf5619ee4
Pip Cet <pipcet@gmail.com>
2021-06-08 07:39:12 +0000

    macho-tools/image-to-macho.c: switch colors after alignment

--- macho-tools/image-to-macho.c
+++ macho-tools/image-to-macho.c
@@ -205,8 +205,8 @@ int main(int argc, char **argv)
 
   SNIPPET(save_boot_args);
   //SNIPPET(disable_timers);
-  SNIPPET(x8r8g8b8);
   SNIPPET(perform_alignment);
+  SNIPPET(x8r8g8b8);
   SNIPPET(enable_all_clocks);
   SNIPPET(bring_up_phys);
   SNIPPET(restore_boot_args);
