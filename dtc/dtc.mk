4ccc69872fa1de39f1abcd2e0ca15f9308f88d57
Pip Cet <pipcet@gmail.com>
2021-06-10 22:08:09 +0000

    dtc/dtc.mk: rule for .dts.h

--- dtc/dtc.mk
+++ dtc/dtc.mk
@@ -3,6 +3,9 @@ DTC ?= dtc
 build/%.dtb.h: build/%.dtb
 	(echo "{";  cat $< | od -tx4 --width=4 -Anone -v | sed -e 's/ \(.*\)/\t0x\1,/'; echo "};") > $@
 
+build/%.dts.h: build/%.dts dtc/dtc-relocs
+	$(CC) -E -x assembler-with-cpp -nostdinc $< | dtc/dtc-relocs > $@
+
 build/%.dts.dtb: build/%.dts
 	$(DTC) -Idts -Odtb $< > $@.tmp && mv $@.tmp $@
 
