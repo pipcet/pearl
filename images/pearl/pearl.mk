bd680db616d6b01e04775a31a246d8d85c542504
Pip Cet <pipcet@gmail.com>
2021-06-10 22:17:49 +0000

    images/pearl/pearl.mk: fix dependency

--- images/pearl/pearl.mk
+++ images/pearl/pearl.mk
@@ -4,6 +4,6 @@ linux-images += pearl
 build/images/pearl/pearl.image: | build/images/pearl/
 build/images/pearl/pearl.image: build/packs/pearl.cpio
 
-build/images/pearl/pearl.image: build/images/pearl/pearl.dts.dtb.h
+build/images/pearl/pearl.image: build/images/pearl/pearl.dts.h
 
 build/images/pearl/pearl.dts: images/pearl/pearl.dts ; $(COPY)
