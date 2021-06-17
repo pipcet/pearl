8f7384e1d9668512b2063904df532041efab7975
Pip Cet <pipcet@gmail.com>
2021-06-17 21:04:06 +0000

    userspace/glibc/glibc.mk: final glibc rules

--- userspace/glibc/glibc.mk
+++ userspace/glibc/glibc.mk
@@ -1,5 +1,23 @@
 # stage1 is built with the pre-installed cross compiler
 
+
+$(BUILD)/done/glibc/glibc/install: $(BUILD)/done/glibc/glibc/build
+	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/glibc/glibc/build DESTDIR=$(BUILD)/install install
+	@touch $@
+
+$(BUILD)/done/glibc/glibc/build: $(BUILD)/done/glibc/glibc/configure
+	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/glibc/glibc/build
+	@touch $@
+
+$(BUILD)/done/glibc/glibc/configure: $(BUILD)/done/glibc/glibc/copy | $(BUILD)/glibc/glibc/build/
+	(cd $(BUILD)/glibc/glibc/build; PATH="$(CROSS_PATH):$$PATH" ../source/configure --host=aarch64-linux-gnu --target=aarch64-linux-gnu --prefix=/ CFLAGS="$(CROSS_CFLAGS)")
+	@touch $@
+
+$(BUILD)/done/glibc/glibc/copy: | $(BUILD)/glibc/glibc/source/ $(BUILD)/done/glibc/glibc/
+	$(CP) -a userspace/glibc/glibc/* $(BUILD)/glibc/glibc/source/
+	@touch $@
+
+
 $(BUILD)/done/glibc/stage1/install: $(BUILD)/done/glibc/stage1/build
 	$(MAKE) -C $(BUILD)/glibc/stage1/build DESTDIR=$(BUILD)/install install
 	@touch $@
