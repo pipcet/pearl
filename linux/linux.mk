54824e323771ddd83e9ae5bbdf60dbf2c3650146
Pip Cet <pipcet@gmail.com>
2021-06-17 21:03:20 +0000

    linux/linux.mk: install headers in $(BUILD)/install

--- linux/linux.mk
+++ linux/linux.mk
@@ -1,5 +1,5 @@
 $(BUILD)/done/linux/headers/install: $(BUILD)/done/linux/headers/copy $(BUILD)/done/toolchain/gcc/stage1/install
-	$(MAKE) -C $(BUILD)/linux/headers/source ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) O=$(BUILD)/linux/headers/o INSTALL_HDR_PATH=$(BUILD)/linux/headers/headers headers_install
+	$(MAKE) -C $(BUILD)/linux/headers/source ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) O=$(BUILD)/linux/headers/o INSTALL_HDR_PATH=$(BUILD)/install headers_install
 	@touch $@
 
 $(BUILD)/done/linux/headers/copy: | $(BUILD)/done/linux/headers/ $(BUILD)/linux/headers/source/
