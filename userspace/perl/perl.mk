ac1284b246e030fda604f461ea2261f97b6308a6
Pip Cet <pipcet@gmail.com>
2021-06-17 22:58:39 +0000

    userspace/perl/perl.mk: perl rules

---
+++ userspace/perl/perl.mk
@@ -0,0 +1,15 @@
+$(BUILD)/done/perl/install: $(BUILD)/done/perl/build
+	$(NATIVE_CODE_ENV) $(MAKE) -C $(BUILD)/perl/build install
+	@touch $@
+
+$(BUILD)/done/perl/build: $(BUILD)/done/perl/configure
+	$(NATIVE_CODE_ENV) $(MAKE) -C $(BUILD)/perl/build
+	@touch $@
+
+$(BUILD)/done/perl/configure: $(BUILD)/done/perl/copy $(BUILD)/glibc/done/install $(BUILD)/gcc/done/install
+	(cd $(BUILD)/perl/build; $(NATIVE_CODE_ENV) sh ./Configure -der -Uversiononly -Uusemymalloc -Dtargetarch="aarch64-linux-gnu" -Dcc="aarch64-linux-gnu-gcc $(CORE_CFLAGS)" -Dccflags="$(CORE_CFLAGS)" -Doptimize="$(CORE_CFLAGS) -fno-strict-aliasing" -Dincpth='' -Dcccdlflags="-fPIC -Wl,--shared -shared" -Dlddlflags="-Wl,--shared -shared" -Uman1dir -Dusedevel -Dprefix="/" -Dinstallprefix="$(BUILD)/install" -Dsysroot="$(BUILD)/install")
+	@touch $@
+
+$(BUILD)/done/perl/copy: | $(BUILD)/perl/build/ $(BUILD)/done/perl/
+	cp -a userspace/perl/perl/* $(addprefix userspace/perl/perl/.,dir-locals.el editorconfig lgtm.yml metaconf-exclusions.txt travis.yml) $(BUILD)/perl/build/
+	@touch $@
