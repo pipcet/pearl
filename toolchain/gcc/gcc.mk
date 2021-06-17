07d77160fb723da32dfb07ded0be23501edd67e1
Pip Cet <pipcet@gmail.com>
2021-06-17 20:16:44 +0000

    require directory structure

--- toolchain/gcc/gcc.mk
+++ toolchain/gcc/gcc.mk
@@ -1,13 +1,20 @@
+$(BUILD)/done/toolchain/gcc/gcc/configure: $(BUILD)/done/toolchain/gcc/gcc/copy $(BUILD)/done/linux/headers/install $(BUILD)/done/glibc/install | $(BUILD)/toolchain/gcc/gcc/build/
+	(cd $(BUILD)/toolchain/gcc/gcc/build; ../source/configure --target=aarch64-linux-gnu --enable-languages=c --enable-shared --disable-bootstrap --prefix=/ --with-sysroot="$(BUILD)")
+
+$(BUILD)/done/toolchain/gcc/gcc/copy: | $(BUILD)/done/toolchain/gcc/gcc/ $(BUILD)/toolchain/gcc/gcc/source/
+	$(CP) -a toolchain/gcc/gcc/* $(BUILD)/toolchain/gcc/gcc/source/
+	touch $@
+
 $(BUILD)/done/toolchain/gcc/stage1/install: $(BUILD)/done/toolchain/gcc/stage1/build
 	$(MAKE) -C $(BUILD)/toolchain/gcc/stage1/build install
 	touch $@
 
 $(BUILD)/done/toolchain/gcc/stage1/build: $(BUILD)/done/toolchain/gcc/stage1/configure
-	$(MAKE) -C $(BUILD)/toolchain/gcc/stage1/build
+	$(MAKE) -C $(BUILD)/toolchain/gcc/stage1/build all-gcc
 	touch $@
 
-$(BUILD)/done/toolchain/gcc/stage1/configure: $(BUILD)/done/toolchain/gcc/stage1/copy | $(BUILD)/toolchain/gcc/stage1/build/
-	(cd $(BUILD)/toolchain/gcc/stage1/build; ../source/configure --target=aarch64-linux-gnu --enable-languages=c --disable-shared --disable-nls --disable-threads --disable-bootstrap --prefix=/ --with-sysroot="$(BUILD)/busybocs/install" --disable-libgcc --disable-libssp --disable-libquadmath --disable-libatomic --disable-libgomp)
+$(BUILD)/done/toolchain/gcc/stage1/configure: $(BUILD)/done/toolchain/gcc/stage1/copy $(BUILD)/done/install/mkdir | $(BUILD)/toolchain/gcc/stage1/build/
+	(cd $(BUILD)/toolchain/gcc/stage1/build; ../source/configure --target=aarch64-linux-gnu --enable-languages=c --disable-shared --disable-nls --disable-threads --disable-bootstrap --prefix=/ --with-sysroot="$(BUILD)/install" --disable-libgcc --disable-libssp --disable-libquadmath --disable-libatomic --disable-libgomp --without-headers)
 	touch $@
 
 $(BUILD)/done/toolchain/gcc/stage1/copy: | $(BUILD)/done/toolchain/gcc/stage1/ $(BUILD)/toolchain/gcc/stage1/source/
