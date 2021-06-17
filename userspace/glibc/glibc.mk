494c7eef030c42a9a843f6a916efbdb858dc8aaf
Pip Cet <pipcet@gmail.com>
2021-06-17 20:51:26 +0000

    userspace/glibc/glibc.mk: looks like we need a stage1 glibc as well...

--- userspace/glibc/glibc.mk
+++ userspace/glibc/glibc.mk
@@ -1,11 +1,15 @@
-$(BUILD)/done/glibc/glibc/build: $(BUILD)/done/glibc/glibc/configure
-	$(MAKE) -C $(BUILD)/glibc/glibc/build
+$(BUILD)/done/glibc/stage1/install: $(BUILD)/done/glibc/stage1/build
+	$(MAKE) -C $(BUILD)/glibc/stage1/build install
 	@touch $@
 
-$(BUILD)/done/glibc/glibc/configure: $(BUILD)/done/glibc/glibc/copy | $(BUILD)/glibc/glibc/build/
-	(cd $(BUILD)/glibc/glibc/build; ../source/configure --host=aarch64-linux-gnu --target=aarch64-linux-gnu --prefix=/ CFLAGS="$(CROSS_CFLAGS)")
+$(BUILD)/done/glibc/stage1/build: $(BUILD)/done/glibc/stage1/configure
+	$(MAKE) -C $(BUILD)/glibc/stage1/build
 	@touch $@
 
-$(BUILD)/done/glibc/glibc/copy: | $(BUILD)/glibc/glibc/source/ $(BUILD)/done/glibc/glibc/
-	$(CP) -a userspace/glibc/glibc/* $(BUILD)/glibc/glibc/source/
+$(BUILD)/done/glibc/stage1/configure: $(BUILD)/done/glibc/stage1/copy | $(BUILD)/glibc/stage1/build/
+	(cd $(BUILD)/glibc/stage1/build; ../source/configure --host=aarch64-linux-gnu --target=aarch64-linux-gnu --prefix=/ CFLAGS="$(CROSS_CFLAGS)")
+	@touch $@
+
+$(BUILD)/done/glibc/stage1/copy: | $(BUILD)/glibc/stage1/source/ $(BUILD)/done/glibc/stage1/
+	$(CP) -a userspace/glibc/glibc/* $(BUILD)/glibc/stage1/source/
 	@touch $@
