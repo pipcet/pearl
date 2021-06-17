c009a4141f3fe40d615f19c908db99180fe20304
Pip Cet <pipcet@gmail.com>
2021-06-17 20:32:32 +0000

    linux/linux.mk: rules for building linux headers

---
+++ linux/linux.mk
@@ -0,0 +1,7 @@
+$(BUILD)/done/linux/headers/install: $(BUILD)/done/linux/headers/copy $(BUILD)/done/toolchain/gcc/stage1/install
+	$(MAKE) -C $(BUILD)/linux/headers/source ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) O=$(BUILD)/linux/headers/o INSTALL_HDR_PATH=$(BUILD)/linux/headers/headers headers_install
+	@touch $@
+
+$(BUILD)/done/linux/headers/copy: | $(BUILD)/done/linux/headers/ $(BUILD)/linux/headers/source/
+	$(CP) -a linux/linux/* $(BUILD)/linux/headers/source/
+	@touch $@
