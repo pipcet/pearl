5f340ce776f240b9625e4a09dd415b57719ddba6
Pip Cet <pipcet@gmail.com>
2021-06-17 20:45:04 +0000

    userspace/glibc/glibc.mk: add glibc configure/build rules

--- userspace/glibc/glibc.mk
+++ userspace/glibc/glibc.mk
@@ -1,3 +1,11 @@
+$(BUILD)/done/glibc/glibc/build: $(BUILD)/done/glibc/glibc/configure
+	$(MAKE) -C $(BUILD)/glibc/glibc/build
+	@touch $@
+
+$(BUILD)/done/glibc/glibc/configure: $(BUILD)/done/glibc/glibc/copy | $(BUILD)/glibc/glibc/build/
+	(cd $(BUILD)/glibc/glibc/build; ../source/configure --host=aarch64-linux-gnu --target=aarch64-linux-gnu --prefix=/ CFLAGS="$(CROSS_CFLAGS)")
+	@touch $@
+
 $(BUILD)/done/glibc/glibc/copy: | $(BUILD)/glibc/glibc/source/ $(BUILD)/done/glibc/glibc/
 	$(CP) -a userspace/glibc/glibc/* $(BUILD)/glibc/glibc/source/
 	@touch $@
