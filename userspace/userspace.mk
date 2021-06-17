cd4ec9f25ae2a66a2afdaceb199b8fc127662c9a
Pip Cet <pipcet@gmail.com>
2021-06-17 22:25:44 +0000

    userspace/userspace.mk: include new files

--- userspace/userspace.mk
+++ userspace/userspace.mk
@@ -1,4 +1,7 @@
 include userspace/glibc/glibc.mk
 include userspace/busybox/busybox.mk
 include userspace/kexec-tools/kexec-tools.mk
+include userspace/lvm2/lvm2.mk
+include userspace/nvme-cli/nvme-cli.mk
+include userspace/ncurses/ncurses.mk
 include userspace/emacs/emacs.mk
