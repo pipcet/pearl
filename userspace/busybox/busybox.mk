fbba85b053285db61a47444cb9348a6cfc60e233
Pip Cet <pipcet@gmail.com>
2021-06-17 23:14:09 +0000

    userspace/busybox/busybox.mk: fix busybox recipes

--- userspace/busybox/busybox.mk
+++ userspace/busybox/busybox.mk
@@ -3,16 +3,16 @@ $(BUILD)/done/busybox/menuconfig: $(BUILD)/done/busybox/configure
 	$(CP) $(BUILD)/busybox/build/.config userspace/busybox/busybox.config
 
 $(BUILD)/done/busybox/install: $(BUILD)/done/busybox/build
-	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/busybox/build install
+	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/busybox/build CROSS_COMPILE=aarch64-linux-gnu- CFLAGS="$(CROSS_CFLAGS)" install
 	@touch $@
 
 $(BUILD)/done/busybox/build: $(BUILD)/done/busybox/configure
-	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/busybox/build
+	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/busybox/build CROSS_COMPILE=aarch64-linux-gnu- CFLAGS="$(CROSS_CFLAGS)"
 	@touch $@
 
 $(BUILD)/done/busybox/configure: userspace/busybox/busybox.config $(BUILD)/done/busybox/copy
 	$(CP) $< $(BUILD)/busybox/build/.config
-	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/busybox/build/ oldconfig
+	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/busybox/build/ CFLAGS="$(CROSS_CFLAGS)" oldconfig
 	@touch $@
 
 $(BUILD)/done/busybox/copy: | $(BUILD)/done/busybox/ $(BUILD)/busybox/build/
