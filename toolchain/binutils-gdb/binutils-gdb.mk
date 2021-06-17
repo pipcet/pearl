8be557a7c1d52fbffe4cc13f66528c81bff20f3d
Pip Cet <pipcet@gmail.com>
2021-06-17 21:41:31 +0000

    toolchain/binutils-gdb/binutils-gdb.mk: new file

---
+++ toolchain/binutils-gdb/binutils-gdb.mk
@@ -0,0 +1,15 @@
+$(BUILD)/done/binutils-gdb/install: $(BUILD)/done/binutils-gdb/build
+	$(MAKE) -C $(BUILD)/binutils-gdb/source DESTDIR=$(BUILD)/install install
+	@touch $@
+
+$(BUILD)/done/binutils-gdb/build: $(BUILD)/done/binutils-gdb/configure
+	$(MAKE) -C $(BUILD)/binutils-gdb/source
+	@touch $@
+
+$(BUILD)/done/binutils-gdb/configure: $(BUILD)/done/binutils-gdb/copy
+	(cd $(BUILD)/binutils-gdb/source/; ../source/configure --target=aarch64-linux-gnu --prefix=/)
+	@touch $@
+
+$(BUILD)/done/binutils-gdb/copy: | $(BUILD)/binutils-gdb/source/ $(BUILD)/done/binutils-gdb/
+	$(CP) -a toolchain/binutils-gdb/binutils-gdb/* $(BUILD)/binutils-gdb/source/
+	@touch $@
