555a4185f67919b1a06e5f0a41b90a4199b4f9d6
Pip Cet <pipcet@gmail.com>
2021-06-18 13:19:10 +0000

    userspace/ncurses/ncurses.mk: add paths

--- userspace/ncurses/ncurses.mk
+++ userspace/ncurses/ncurses.mk
@@ -1,13 +1,13 @@
 $(BUILD)/done/ncurses/install: $(BUILD)/done/ncurses/build
-	$(MAKE) -C $(BUILD)/ncurses/build install
+	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/ncurses/build install
 	@touch $@
 
 $(BUILD)/done/ncurses/build: $(BUILD)/done/ncurses/configure
-	$(MAKE) -C $(BUILD)/ncurses/build
+	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/ncurses/build
 	@touch $@
 
 $(BUILD)/done/ncurses/configure: $(BUILD)/done/ncurses/copy $(BUILD)/done/glibc/glibc/install $(BUILD)/done/gcc/gcc/install
-	(cd $(BUILD)/ncurses/build; ./configure --host=aarch64-linux-gnu --target=aarch64-linux-gnu --prefix=/ --with-install-prefix=$(BUILD)/install --disable-stripping CFLAGS="$(CROSS_CFLAGS)" CXXFLAGS="$(CROSS_CFLAGS)")
+	(cd $(BUILD)/ncurses/build; PATH="$(CROSS_PATH):$$PATH" ./configure --host=aarch64-linux-gnu --target=aarch64-linux-gnu --prefix=/ --with-install-prefix=$(BUILD)/install --disable-stripping CFLAGS="$(CROSS_CFLAGS)" CXXFLAGS="$(CROSS_CFLAGS)")
 	@touch $@
 
 $(BUILD)/done/ncurses/copy: | $(BUILD)/done/ncurses/ $(BUILD)/ncurses/build/
