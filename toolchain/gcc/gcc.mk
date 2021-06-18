020fa6c620e8312f13eee92068b90b569ade88c6
Pip Cet <pipcet@gmail.com>
2021-06-18 20:25:01 +0000

    maybe we can simplify this after all...

--- toolchain/gcc/gcc.mk
+++ toolchain/gcc/gcc.mk
@@ -14,21 +14,37 @@ $(BUILD)/gcc/done/gcc/copy: $(BUILD)/gcc/done/checkout | $(BUILD)/gcc/done/gcc/ 
 	$(CP) -a toolchain/gcc/gcc/* $(BUILD)/gcc/gcc/source/
 	@touch $@
 
-$(BUILD)/gcc/done/stage1/install: $(BUILD)/gcc/done/stage1/build
-	$(MAKE) -C $(BUILD)/gcc/stage1/build install
-	@touch $@
+# $(BUILD)/gcc/done/stage2/install: $(BUILD)/gcc/done/stage2/build
+# 	$(MAKE) -C $(BUILD)/gcc/stage2/build install
+# 	@touch $@
 
-$(BUILD)/gcc/done/stage1/build: $(BUILD)/gcc/done/stage1/configure
-	$(MAKE) -C $(BUILD)/gcc/stage1/build
-	@touch $@
+# $(BUILD)/gcc/done/stage2/build: $(BUILD)/gcc/done/stage2/configure
+# 	$(MAKE) -C $(BUILD)/gcc/stage2/build
+# 	@touch $@
 
-$(BUILD)/gcc/done/stage1/configure: $(BUILD)/gcc/done/stage1/copy $(BUILD)/install/done/mkdir $(BUILD)/binutils-gdb/done/install $(BUILD)/glibc/done/headers/install | $(BUILD)/gcc/stage1/build/
-	(cd $(BUILD)/gcc/stage1/build; ../source/configure --target=aarch64-linux-gnu --enable-languages=c --disable-libcc1 --disable-shared --disable-nls --disable-threads --disable-bootstrap --prefix="$(BUILD)/toolchain" --with-sysroot="$(BUILD)/install" --with-static-standard-libraries --disable-libssp --disable-libquadmath --disable-libatomic --disable-libgomp --without-headers --with-build-sysroot="$(BUILD)/install" --disable-c++tools)
-	@touch $@
+# $(BUILD)/gcc/done/stage2/configure: $(BUILD)/gcc/done/stage2/copy $(BUILD)/install/done/mkdir $(BUILD)/binutils-gdb/done/install $(BUILD)/glibc/done/headers/install | $(BUILD)/gcc/stage2/build/
+# 	(cd $(BUILD)/gcc/stage2/build; ../source/configure --target=aarch64-linux-gnu --enable-languages=c --disable-libcc1 --disable-shared --disable-nls --disable-threads --disable-bootstrap --prefix="$(BUILD)/toolchain" --with-sysroot="$(BUILD)/install" --with-static-standard-libraries --disable-libssp --disable-libquadmath --disable-libatomic --disable-libgomp --without-headers --with-build-sysroot="$(BUILD)/install" --disable-c++tools)
+# 	@touch $@
 
-$(BUILD)/gcc/done/stage1/copy: $(BUILD)/gcc/done/checkout | $(BUILD)/gcc/done/stage1/ $(BUILD)/gcc/stage1/source/
-	$(CP) -a toolchain/gcc/gcc/* $(BUILD:$(PWD)/%=%)/gcc/stage1/source/
-	@touch $@
+# $(BUILD)/gcc/done/stage2/copy: $(BUILD)/gcc/done/checkout | $(BUILD)/gcc/done/stage2/ $(BUILD)/gcc/stage2/source/
+# 	$(CP) -a toolchain/gcc/gcc/* $(BUILD:$(PWD)/%=%)/gcc/stage2/source/
+# 	@touch $@
+
+# $(BUILD)/gcc/done/stage1/install: $(BUILD)/gcc/done/stage1/build
+# 	$(MAKE) -C $(BUILD)/gcc/stage1/build install
+# 	@touch $@
+
+# $(BUILD)/gcc/done/stage1/build: $(BUILD)/gcc/done/stage1/configure
+# 	$(MAKE) -C $(BUILD)/gcc/stage1/build
+# 	@touch $@
+
+# $(BUILD)/gcc/done/stage1/configure: $(BUILD)/gcc/done/stage1/copy $(BUILD)/install/done/mkdir $(BUILD)/binutils-gdb/done/install | $(BUILD)/gcc/stage1/build/
+# 	(cd $(BUILD)/gcc/stage1/build; ../source/configure --target=aarch64-linux-gnu --enable-languages=c --disable-libcc1 --disable-shared --disable-nls --disable-threads --disable-bootstrap --prefix="$(BUILD)/toolchain" --with-sysroot="$(BUILD)/install" --with-static-standard-libraries --disable-libssp --disable-libquadmath --disable-libatomic --disable-libgomp --without-headers --with-build-sysroot="$(BUILD)/install" --disable-c++tools --disable-libgcc)
+# 	@touch $@
+
+# $(BUILD)/gcc/done/stage1/copy: $(BUILD)/gcc/done/checkout | $(BUILD)/gcc/done/stage1/ $(BUILD)/gcc/stage1/source/
+# 	$(CP) -a toolchain/gcc/gcc/* $(BUILD:$(PWD)/%=%)/gcc/stage1/source/
+# 	@touch $@
 
 $(BUILD)/gcc/done/checkout: toolchain/gcc/gcc{checkout} | $(BUILD)/gcc/done/gcc/
 	@touch $@
