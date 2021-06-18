6ca8da77596cc2c66c577942c44f038c24e90e65
Pip Cet <pipcet@gmail.com>
2021-06-18 12:05:36 +0000

    toolchain/binutils-gdb/binutils-gdb.mk: make binutils install in build/toolchain

--- toolchain/binutils-gdb/binutils-gdb.mk
+++ toolchain/binutils-gdb/binutils-gdb.mk
@@ -1,5 +1,5 @@
 $(BUILD)/done/binutils-gdb/install: $(BUILD)/done/binutils-gdb/build
-	$(MAKE) -C $(BUILD)/binutils-gdb/source DESTDIR=$(BUILD)/install install
+	$(MAKE) -C $(BUILD)/binutils-gdb/source DESTDIR=$(BUILD)/toolchain install
 	@touch $@
 
 $(BUILD)/done/binutils-gdb/build: $(BUILD)/done/binutils-gdb/configure
@@ -7,7 +7,7 @@ $(BUILD)/done/binutils-gdb/build: $(BUILD)/done/binutils-gdb/configure
 	@touch $@
 
 $(BUILD)/done/binutils-gdb/configure: $(BUILD)/done/binutils-gdb/copy
-	(cd $(BUILD)/binutils-gdb/source/; ../source/configure --target=aarch64-linux-gnu --prefix=/)
+	(cd $(BUILD)/binutils-gdb/source/; ../source/configure --target=aarch64-linux-gnu --prefix=$(BUILD)/toolchain)
 	@touch $@
 
 $(BUILD)/done/binutils-gdb/copy: | $(BUILD)/binutils-gdb/source/ $(BUILD)/done/binutils-gdb/
