a6e4f6287fdca1ba9ba80f20a9bca2f504b08243
Pip Cet <pipcet@gmail.com>
2021-06-18 20:49:22 +0000

    toolchain/gcc/gcc.mk: now?

--- toolchain/gcc/gcc.mk
+++ toolchain/gcc/gcc.mk
@@ -6,8 +6,8 @@ $(BUILD)/gcc/done/gcc/build: $(BUILD)/gcc/done/gcc/configure
 	$(MAKE) -C $(BUILD)/gcc/gcc/build
 	@touch $@
 
-$(BUILD)/gcc/done/gcc/configure: $(BUILD)/gcc/done/gcc/copy $(BUILD)/linux/done/headers/install $(BUILD)/glibc/done/stage1/install | $(BUILD)/gcc/gcc/build/
-	(cd $(BUILD)/gcc/gcc/build; ../source/configure --target=aarch64-linux-gnu --enable-languages=c,lto --enable-shared --disable-bootstrap --prefix=/ --with-sysroot="$(BUILD)/install")
+$(BUILD)/gcc/done/gcc/configure: $(BUILD)/gcc/done/gcc/copy $(BUILD)/linux/done/headers/install $(BUILD)/glibc/done/headers/install | $(BUILD)/gcc/gcc/build/
+	(cd $(BUILD)/gcc/gcc/build; ../source/configure --target=aarch64-linux-gnu --enable-languages=c,lto --enable-shared --disable-bootstrap --prefix=/ --with-sysroot="$(BUILD)/install" --disable-libssp --disable-libquadmath --disable-libatomic --disable-libgomp --without-headers --disable-shared --with-static-standard-libraries --with-build-sysroot="$(BUILD)/install" --disable-c++tools)
 	@touch $@
 
 $(BUILD)/gcc/done/gcc/copy: $(BUILD)/gcc/done/checkout | $(BUILD)/gcc/done/gcc/ $(BUILD)/gcc/gcc/source/
