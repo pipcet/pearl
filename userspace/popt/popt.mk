91f178361ec8dfe0e4e19e5608193e9e0ed60267
Pip Cet <pipcet@gmail.com>
2021-06-19 08:39:20 +0000

    userspace/popt/popt.mk: make sure popt picks up aarch64-linux-gnu-gcc

--- userspace/popt/popt.mk
+++ userspace/popt/popt.mk
@@ -1,14 +1,14 @@
 $(BUILD)/popt/done/install: $(BUILD)/popt/done/build
-	$(MAKE) -C $(BUILD)/popt/build DESTDIR="$(BUILD)/pearl/install" install
+	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/popt/build DESTDIR="$(BUILD)/pearl/install" install
 	@touch $@
 
 $(BUILD)/popt/done/build: $(BUILD)/popt/done/configure
-	$(MAKE) -C $(BUILD)/popt/build
+	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/popt/build
 	@touch $@
 
 $(BUILD)/popt/done/configure: $(BUILD)/popt/done/copy
-	(cd $(BUILD)/popt/build; sh autogen.sh)
-	(cd $(BUILD)/popt/build; ./configure --target=aarch64-linux-gnu --host=aarch64-linux-gnu --prefix=/ CFLAGS="$(CROSS_CFLAGS)")
+	(cd $(BUILD)/popt/build; PATH="$(CROSS_PATH):$$PATH" sh autogen.sh)
+	(cd $(BUILD)/popt/build; PATH="$(CROSS_PATH):$$PATH" ./configure --target=aarch64-linux-gnu --host=aarch64-linux-gnu --prefix=/ CFLAGS="$(CROSS_CFLAGS)")
 	@touch $@
 
 $(BUILD)/popt/done/copy: $(BUILD)/popt/done/checkout | $(BUILD)/popt/done/ $(BUILD)/popt/build/
