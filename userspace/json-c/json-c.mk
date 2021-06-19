c6fb11547745e268f723a4304c44281e60502d89
Pip Cet <pipcet@gmail.com>
2021-06-19 08:32:21 +0000

    userspace/json-c/json-c.mk: another path adjustment

--- userspace/json-c/json-c.mk
+++ userspace/json-c/json-c.mk
@@ -7,7 +7,7 @@ $(BUILD)/json-c/done/build: $(BUILD)/json-c/done/configure
 	@touch $@
 
 $(BUILD)/json-c/done/configure: $(BUILD)/json-c/done/copy $(BUILD)/glibc/done/glibc/install $(BUILD)/gcc/done/gcc/install
-	(cd $(BUILD)/json-c/build; cmake -DCMAKE_LINKER=$(BUILD)/toolchain/bin/aarch64-linux-gnu-ld -DCMAKE_SHARED_LINKER=$(BUILD)/toolchain/bin/aarch64-linux-gnu-ld -DCMAKE_C_COMPILER=$(BUILD)/toolchain/bin/aarch64-linux-gnu-gcc -DCMAKE_C_FLAGS="-I$(BUILD)/pearl/install/include -L$(BUILD)/pearl/install/lib --sysroot=$(BUILD)/pearl/install" .)
+	(cd $(BUILD)/json-c/build; cmake -DCMAKE_LINKER=$(BUILD)/pearl/toolchain/bin/aarch64-linux-gnu-ld -DCMAKE_SHARED_LINKER=$(BUILD)/pearl/toolchain/bin/aarch64-linux-gnu-ld -DCMAKE_C_COMPILER=$(BUILD)/pearl/toolchain/bin/aarch64-linux-gnu-gcc -DCMAKE_C_FLAGS="-I$(BUILD)/pearl/install/include -L$(BUILD)/pearl/install/lib --sysroot=$(BUILD)/pearl/install" .)
 	@touch $@
 
 $(BUILD)/json-c/done/copy: $(BUILD)/json-c/done/checkout | $(BUILD)/json-c/done/ $(BUILD)/json-c/build/
