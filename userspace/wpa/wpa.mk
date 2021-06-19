330beb2af2c37a5e3b5e3aa5b0df487a14157f81
Pip Cet <pipcet@gmail.com>
2021-06-19 20:49:57 +0000

    userspace/wpa/wpa.mk: write

--- userspace/wpa/wpa.mk
+++ userspace/wpa/wpa.mk
@@ -1,4 +1,20 @@
 $(BUILD)/wpa_supplicant/done/install: $(BUILD)/wpa_supplicant/done/build
-$(BUILD)/wpa_supplicant/done/build: $(BUILD)/wpa_supplicant/done/configure
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/wpa_supplicant/build/wpa_supplicant $(WITH_CROSS_CC) install
 
-# userspace-modules += wpa-supplicant
+$(BUILD)/wpa_supplicant/done/build: $(BUILD)/wpa_supplicant/done/configure
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/wpa_supplicant/build/wpa_supplicant $(WITH_CROSS_CC)
+	@touch $@
+
+$(BUILD)/wpa_supplicant/done/configure: userspace/wpa/wpa_supplicant.config $(BUILD)/wpa_supplicant/done/copy
+	cp $< $(BUILD)/wpa_supplicant/build/wpa_supplicant/.config
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/wpa_supplicant/build/wpa_supplicant defconfig $(WITH_CROSS_CC)
+	@touch $@
+
+$(BUILD)/wpa_supplicant/done/copy: $(BUILD)/wpa_supplicant/done/checkout | $(BUILD)/wpa_supplicant/build/
+	cp -as $(PWD)/userspace/wpa/wpa/* $(BUILD)/wpa_supplicant/build/
+	@touch $@
+
+$(BUILD)/wpa_supplicant/done/checkout: userspace/wpa/wpa{checkout} | $(BUILD)/wpa_supplicant/done/
+	@touch $@
+
+userspace-modules += wpa-supplicant
