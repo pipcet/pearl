64b8974b11a2b136012940081c1640dfa2c30bd3
Pip Cet <pipcet@gmail.com>
2021-06-19 10:06:07 +0000

    toolchain/gcc/gcc.mk: use convenience macro

--- toolchain/gcc/gcc.mk
+++ toolchain/gcc/gcc.mk
@@ -1,16 +1,16 @@
 $(BUILD)/gcc/done/gcc/install: $(BUILD)/gcc/done/gcc/build
-	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/gcc/gcc/build DESTDIR="$(BUILD)/pearl/toolchain" install
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/gcc/gcc/build DESTDIR="$(BUILD)/pearl/toolchain" install
 	@touch $@
 
 $(BUILD)/gcc.tar: $(BUILD)/gcc/done/gcc/build
 	tar -C $(BUILD) -cf $@ gcc/gcc/build
 
 $(BUILD)/gcc/done/gcc/build: $(BUILD)/gcc/done/gcc/configure
-	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/gcc/gcc/build
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/gcc/gcc/build
 	@touch $@
 
 $(BUILD)/gcc/done/gcc/configure: $(BUILD)/gcc/done/gcc/copy $(BUILD)/linux/done/headers/install $(BUILD)/glibc/done/headers/install | $(BUILD)/gcc/gcc/build/
-	(cd $(BUILD)/gcc/gcc/build; PATH="$(CROSS_PATH):$$PATH" ../source/configure --target=aarch64-linux-gnu --enable-languages=c,lto --enable-shared --disable-bootstrap --prefix=/ --with-sysroot="$(BUILD)/pearl/install" --disable-libssp --disable-libquadmath --disable-libatomic --disable-libgomp --without-headers --disable-shared --with-static-standard-libraries --with-build-sysroot="$(BUILD)/pearl/install" --disable-c++tools)
+	(cd $(BUILD)/gcc/gcc/build; $(WITH_CROSS_PATH) ../source/configure --target=aarch64-linux-gnu --enable-languages=c,lto --enable-shared --disable-bootstrap --prefix=/ --with-sysroot="$(BUILD)/pearl/install" --disable-libssp --disable-libquadmath --disable-libatomic --disable-libgomp --without-headers --disable-shared --with-static-standard-libraries --with-build-sysroot="$(BUILD)/pearl/install" --disable-c++tools)
 	@touch $@
 
 $(BUILD)/gcc/done/gcc/copy: $(BUILD)/gcc/done/checkout | $(BUILD)/gcc/done/gcc/ $(BUILD)/gcc/gcc/source/
