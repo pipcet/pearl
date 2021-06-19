54e4df84cf021123046ccd6ef4ac151f5a7b1ca1
Pip Cet <pipcet@gmail.com>
2021-06-19 08:28:01 +0000

    more path adjustments

--- g/github/github.mk
+++ g/github/github.mk
@@ -13,7 +13,7 @@ build/artifacts/up/pearl.macho: build/pearl.macho build/artifact-timestamp | bui
 	$(CP) $< $@
 
 build/artifacts{push}: .github-init
-	(cd build/artifacts/up; for file in *; do name=$$(basename "$$file"); (cd $(PWD); bash github/ul-artifact "$$name" "build/artifacts/up/$$name"); done)
+	(cd build/artifacts/up; for file in *; do name=$$(basename "$$file"); (cd $(PWD); bash g/github/ul-artifact "$$name" "build/artifacts/up/$$name"); done)
 
 build/%{artifact}: build/% .github-init
 	$(MKDIR) build/artifacts/up
@@ -29,7 +29,7 @@ build/github-releases{list}: .github-init | build/github-releases/
 	curl -sSL https://api.github.com/repos/$$GITHUB_REPOSITORY/releases/tags/latest | jq '.[.tag_name,.id] | .[]' | while read tag; do read id; echo $$id > build/github-releases/$$tag; done
 	ls -l build/github-releases/
 
-%{release}: .github-init | github/release/
+%{release}: .github-init | g/github/release/
 	$(MAKE) build/github-releases{list}
 	for name in $$(cd build/release; ls *); do for id in $$(jq ".[] | if .name == \"$$name\" then .id else 0 end" < github/assets/$*.json); do [ $$id != "0" ] && curl -sSL -XDELETE -H "Authorization: token $$GITHUB_TOKEN" "https://api.github.com/repos/$$GITHUB_REPOSITORY/releases/assets/$$id"; echo; done; done
 	(for name in build/release/*; do bname=$$(basename "$$name"); curl -sSL -XPOST -H "Authorization: token $$GITHUB_TOKEN" --header "Content-Type: application/octet-stream" "https://uploads.github.com/repos/$$GITHUB_REPOSITORY/releases/$$(cat build/github-releases/\"$*\")/assets?name=$$bname" --upload-file $$name; echo; done)
