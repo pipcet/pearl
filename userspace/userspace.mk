1b8dabaa79b5bb1c6fa688c420156e467ac332c4
Pip Cet <pipcet@gmail.com>
2021-06-19 20:50:05 +0000

    userspace/userspace.mk: include wpa.mk

--- userspace/userspace.mk
+++ userspace/userspace.mk
@@ -17,6 +17,7 @@ include userspace/perl/perl.mk
 include userspace/popt/popt.mk
 include userspace/slurp/slurp.mk
 include userspace/util-linux/util-linux.mk
+include userspace/wpa/wpa.mk
 
 $(BUILD)/userspace/done/%: $(foreach module,$(userspace-modules),$(BUILD)/$(module)/done/%) | $(BUILD)/userspace/done/
 	@touch $@
