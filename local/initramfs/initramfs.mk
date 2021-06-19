94d270436e8baf9d4164585b5782d8a872ef34b9
Pip Cet <pipcet@gmail.com>
2021-06-19 16:29:33 +0000

    local/initramfs/initramfs.mk: keep paths consistent

--- local/initramfs/initramfs.mk
+++ local/initramfs/initramfs.mk
@@ -4,7 +4,7 @@ $(BUILD)/linux/pearl.cpio: $(BUILD)/initramfs/pearl.cpio ; $(COPY)
 $(BUILD)/initramfs/%: local/initramfs/% ; $(COPY)
 $(BUILD)/initramfs/pearl.cpiospec: local/initramfs/pearl.cpiospec
 	$(MKDIR) $(dir $@)
-	@(cat $<; $(foreach file,$(patsubst $(BUILD)/initramfs/pearl/%,/%,$(wordlist 2,$(words $^),$^)),echo dir $(dir $(patsubst %/,%,$(file))) 755 0 0; echo file $(file) $(BUILD)/initramfs/pearl/$(file) 755 0 0;) (cd $(BUILD)/pearl/install/; find -type f | while read file; do echo "file $$file $(BUILD)/pearl/install/$$file 755 0 0"; done)) | sort | uniq > $@
+	@(cat $<; $(foreach file,$(patsubst $(BUILD)/initramfs/pearl/%,/%,$(wordlist 2,$(words $^),$^)),echo dir $(dir $(patsubst %/,%,$(file))) 755 0 0; echo file $(patsubst ./%,%,$(file)) $(BUILD)/initramfs/pearl/$(file) 755 0 0;) (cd $(BUILD)/pearl/install/; find -type f | while read file; do echo "file $$file $(BUILD)/pearl/install/$$file 755 0 0"; done)) | sort | uniq > $@
 
 $(BUILD)/initramfs/pearl.cpio: $(BUILD)/initramfs/pearl.cpiospec $(BUILD)/linux/done/checkout | $(BUILD)/initramfs/
 	(cd $(BUILD)/linux/linux/build/; ./usr/gen_initramfs.sh -o $@ $<)
