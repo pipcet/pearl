d7a4349c4c722a7660565659c0f53d9986b1e3da
Pip Cet <pipcet@gmail.com>
2021-06-19 12:20:11 +0000

    userspace/slurp/slurp.mk: new file

---
+++ userspace/slurp/slurp.mk
@@ -0,0 +1,23 @@
+$(BUILD)/slurp/done/install: $(BUILD)/slurp/done/build
+	$(WITH_CROSS_PATH) $(WITH_QEMU) $(MAKE) -C $(BUILD)/slurp/build DESTDIR="$(BUILD)/pearl/install" install
+	@touch $@
+
+$(BUILD)/slurp/slurp.tar: $(BUILD)/slurp/done/build
+	tar -C $(BUILD)/slurp -cf $@ done build
+
+$(BUILD)/slurp/done/build: $(BUILD)/slurp/done/configure
+	$(WITH_CROSS_PATH) $(WITH_QEMU) $(MAKE) -C $(BUILD)/slurp/build
+	@touch $@
+
+$(BUILD)/slurp/done/configure: $(BUILD)/slurp/done/copy $(BUILD)/perl/done/install
+	(cd $(BUILD)/slurp/build; $(WITH_CROSS_PATH) $(WITH_QEMU) perl Makefile.PL)
+	@touch $@
+
+$(BUILD)/slurp/done/copy: $(BUILD)/slurp/done/checkout | $(BUILD)/slurp/done/ $(BUILD)/slurp/build/
+	cp -as $(PWD)/userspace/slurp/slurp/* $(BUILD)/slurp/build/
+	@touch $@
+
+$(BUILD)/slurp/done/checkout: userspace/slurp/slurp{checkout} | $(BUILD)/slurp/done/
+	@touch $@
+
+userspace-modules += slurp
