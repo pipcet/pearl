437eded0df8980c0ca4e4edcc4abec116d8634ce
Pip Cet <pipcet@gmail.com>
2021-06-20 14:38:48 +0000

    toolchain/toolchain.mk: actually build toolchain.tar

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -1,2 +1,5 @@
 include toolchain/binutils-gdb/binutils-gdb.mk
 include toolchain/gcc/gcc.mk
+
+$(BUILD)/userspace.tar: $(BUILD)/binutils-gdb/done/install $(BUILD)/gcc/done/gcc/install
+	tar -C $(BUILD) -cf $@ pearl/toolchain $(patsubst $(BUILD)/%,%,$(wildcard $(BUILD)/*/done))
