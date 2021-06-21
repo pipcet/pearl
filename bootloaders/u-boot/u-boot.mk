580d541196dc5d9e150645953d8f774b4b3e72c3
Pip Cet <pipcet@gmail.com>
2021-06-21 22:28:16 +0000

    bootloaders/u-boot/u-boot.mk: build u-boot again

--- bootloaders/u-boot/u-boot.mk
+++ bootloaders/u-boot/u-boot.mk
@@ -1,11 +1,27 @@
-build/u-boot/u-boot.bin: stamp/u-boot | build/u-boot/
-	$(MAKE) -C submodule/u-boot O=$(PWD)/build/u-boot CROSS_COMPILE=$(CROSS_COMPILE) apple_m1_defconfig
-	$(MAKE) -C submodule/u-boot O=$(PWD)/build/u-boot CROSS_COMPILE=$(CROSS_COMPILE)
+$(BUILD)/u-boot-plus-grub.image: $(BUILD)/u-boot.image.gz $(BUILD)/grub.efi
+	(gunzip < $<; cat $(BUILD)/grub.efi) > $@
 
-build/u-boot/u-boot.dtb: build/u-boot/u-boot.bin
+$(BUILD)/u-boot.dtb: $(BUILD)/u-boot/done/build
+	$(CP) $(BUILD)/u-boot/build/u-boot.dtb $@
 
-build/u-boot.image.gz: build/u-boot/u-boot.bin
-	gzip < $< > $@
+$(BUILD)/u-boot.image.gz: $(BUILD)/u-boot/done/build
+	gzip < $(BUILD)/u-boot/build/u-boot.bin > $@
 
-build/u-boot-plus-grub.image: build/u-boot/u-boot.bin build/grub/grub.efi
-	cat $^ > $@
+$(BUILD)/u-boot/done/build: $(BUILD)/u-boot/done/configure
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/u-boot/build ARCH=arm CROSS_COMPILE=$(CROSS_COMPILE)
+	@touch $@
+
+$(BUILD)/u-boot/done/configure: $(BUILD)/u-boot/done/copy
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/u-boot/build ARCH=arm CROSS_COMPILE=$(CROSS_COMPILE) apple_m1_defconfig
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/u-boot/build ARCH=arm CROSS_COMPILE=$(CROSS_COMPILE) oldconfig
+	@touch $@
+
+$(BUILD)/u-boot/done/copy: $(BUILD)/u-boot/done/checkout | $(BUILD)/u-boot/done/ $(BUILD)/u-boot/build/
+	$(CP) -aus $(PWD)/bootloaders/u-boot/u-boot/* $(BUILD)/u-boot/build/
+	@touch $@
+
+$(BUILD)/u-boot/done/checkout: | $(BUILD)/u-boot/done/
+	$(MAKE) bootloaders/u-boot/u-boot{checkout}
+	@touch $@
+
+$(call pearl-static,$(wildcard bootloaders/u-boot/pearl/bin/*),bootloaders/u-boot/pearl)
