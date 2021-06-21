b5041cb569922e8b040deeef334db6f83c5f4f02
Pip Cet <pipcet@gmail.com>
2021-06-21 06:35:33 +0000

    linux/linux.mk: fix path

--- linux/linux.mk
+++ linux/linux.mk
@@ -52,4 +52,4 @@ $(BUILD)/linux/done/headers/copy: $(BUILD)/linux/done/checkout | $(BUILD)/linux/
 $(BUILD)/linux/done/checkout: linux/linux{checkout} | $(BUILD)/linux/done/
 	@touch $@
 
-$(call pearl-static,$(wildcard $(PWD)/local/linux/pearl/bin/*),$(PWD)/local/linux/pearl)
+$(call pearl-static,$(wildcard $(PWD)/linux/pearl/bin/*),$(PWD)/linux/pearl)
