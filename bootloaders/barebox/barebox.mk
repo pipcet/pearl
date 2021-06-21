862a53bf0cc1ae846d2e757670fb6032e2eb0e8f
Pip Cet <pipcet@gmail.com>
2021-06-21 22:14:16 +0000

    bootloaders/barebox/barebox.mk: build barebox again

--- bootloaders/barebox/barebox.mk
+++ bootloaders/barebox/barebox.mk
@@ -1,18 +1,32 @@
-barebox/barebox{menuconfig}: barebox/barebox.config stamp/barebox | build/barebox/
-	$(CP) $< build/barebox/.config
-	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) menuconfig
+bootloaders/barebox/barebox{menuconfig}: bootloaders/barebox/barebox.config | $(BUILD)/barebox/
+	$(CP) $< $(BUILD)/barebox/.config
+	$(MAKE) -C submodule/barebox ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) menuconfig
 	$(CP) build/barebox/.config $<
 
 barebox/barebox{oldconfig}: barebox/barebox.config stamp/barebox | build/barebox/
 	$(CP) $< build/barebox/.config
-	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox oldconfig ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE)
+	$(MAKE) -C submodule/barebox oldconfig ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE)
 	$(CP) build/barebox/.config $<
 
-build/barebox/images/barebox-dt-2nd.img: barebox/barebox.config stamp/barebox | build/barebox/
-	$(CP) $< build/barebox/.config
-	$(MAKE) -C submodule/barebox O=$(PWD)/build/barebox CROSS_COMPILE=$(CROSS_COMPILE) ARCH=arm64
-
-build/barebox.image.gz: build/barebox/barebox.bin
+$(BUILD)/barebox.image.gz: $(BUILD)/barebox/done/build
 	gzip < $< > $@
+
+$(BUILD)/barebox/done/build: $(BUILD)/barebox/done/configure
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/barebox/build ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE)
+	@touch $@
+
+$(BUILD)/barebox/done/configure: bootloaders/barebox/barebox.config $(BUILD)/barebox/done/copy
+	$(CP) $< $(BUILD)/barebox/build/.config
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/barebox/build ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) olddefconfig
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/barebox/build ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) oldconfig
+	@touch $@
+
+$(BUILD)/barebox/done/copy: $(BUILD)/barebox/done/checkout | $(BUILD)/barebox/done/ $(BUILD)/barebox/build/
+	$(CP) -aus $(PWD)/bootloaders/barebox/barebox/* $(BUILD)/barebox/build/
+	@touch $@
+
+$(BUILD)/barebox/done/checkout: | $(BUILD)/barebox/done/
+	$(MAKE) bootloaders/barebox/barebox{checkout}
+	@touch $@
 
 $(call pearl-static,$(wildcard bootloaders/barebox/pearl/bin/*),bootloaders/barebox/pearl)
