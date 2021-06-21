64ecd681c6a0f161a2f25709d8fe0b7f3db12614
Pip Cet <pipcet@gmail.com>
2021-06-21 21:09:03 +0000

    bootloaders/m1n1/m1n1.mk: rm conflicting rule

--- bootloaders/m1n1/m1n1.mk
+++ bootloaders/m1n1/m1n1.mk
@@ -15,10 +15,6 @@ $(BUILD)/m1n1/done/checkout: | $(BUILD)/m1n1/done/
 	$(MAKE) bootloaders/m1n1/m1n1{checkout}
 	@touch $@
 
-build/m1n1.macho: stamp/m1n1
-	$(MAKE) -C submodule/m1n1
-	$(CP) submodule/m1n1/build/m1n1.macho $@
-
 %.macho{m1n1}: %.macho
 	M1N1DEVICE=$(M1N1DEVICE) python3 ./submodule/m1n1/proxyclient/chainload.py $<
 
