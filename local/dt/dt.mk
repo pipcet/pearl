acd77f28b0ccb2b2e81370b9bef265ac90573f28
Pip Cet <pipcet@gmail.com>
2021-06-21 09:57:10 +0000

    local/dt/dt.mk: exclude adtp for now

--- local/dt/dt.mk
+++ local/dt/dt.mk
@@ -10,10 +10,10 @@ $(BUILD)/dt/build/bin/macho-version: local/dt/macho-version.c | $(BUILD)/dt/buil
 $(BUILD)/dt/build/bin/adtp: local/dt/adtp.cc | $(BUILD)/dt/build/bin/
 	$(WITH_CROSS_PATH) $(CROSS_COMPILE)g++ -Os -static -o $@ $<
 
-$(BUILD)/dt.tar: $(BUILD)/dt/bin/dt $(BUILD)/dt/bin/adtdump $(BUILD)/dt/bin/adtp
-	(cd $(BUILD)/dt; tar c bin/dt bin/adtdump bin/adtp) > $@
+$(BUILD)/dt.tar: $(BUILD)/dt/bin/dt $(BUILD)/dt/bin/adtdump
+	(cd $(BUILD)/dt; tar c bin/dt bin/adtdump) > $@
 
 $(BUILD)/%.dts.dtp: $(BUILD)/%.dts $(BUILD)/dt/bin/dt
 	$(BUILD)/dt/bin/dt dts-to-dtp $< $@
 
-$(call pearl-static,$(BUILD)/dt/build/bin/dt $(BUILD)/dt/build/bin/adtdump $(BUILD)/dt/build/bin/macho-version $(BUILD)/dt/build/bin/adtp,$(BUILD)/dt/build)
+$(call pearl-static,$(BUILD)/dt/build/bin/dt $(BUILD)/dt/build/bin/adtdump $(BUILD)/dt/build/bin/macho-version,$(BUILD)/dt/build)
