9bb79dc59af0214c2643347adc99e9f101df7db5
Pip Cet <pipcet@gmail.com>
2021-06-22 14:41:19 +0000

    local/initramfs/build-cpiospec.pl: make things boot

--- local/initramfs/build-cpiospec.pl
+++ local/initramfs/build-cpiospec.pl
@@ -17,16 +17,18 @@ while ($didsomething) {
     for my $path (sort keys %paths) {
 	my $dir = $path;
 	$dir =~ s/\/[^\/]*$//;
-	next if exists $paths{$dir};
+	next if (exists $paths{$dir} or ($dir eq "/usr") or ($dir eq "/local"));
 	$paths{$dir} = "/";
 	$didsomething = 1;
     }
 }
 
 print "dir / 755 0 0\n";
+print "slink /usr / 755 0 0\n";
+print "slink /local / 755 0 0\n";
 
 for my $key (sort { $a cmp $b } keys %paths) {
-    next if $key eq "";
+    next if $key eq "" or $key eq "/usr" or $key eq "/local";
     if ($paths{$key} eq "/" or -d $paths{$key}) {
 	print "dir $key 755 0 0\n";
     } elsif (-l $paths{$key}) {
