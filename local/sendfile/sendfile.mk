341fc91584575d628fe1ad6f189bb0adbab5e03e
Pip Cet <pipcet@gmail.com>
2021-06-22 07:09:36 +0000

    local/sendfile/sendfile.mk: include receive-sendfile

--- local/sendfile/sendfile.mk
+++ local/sendfile/sendfile.mk
@@ -1,10 +1,10 @@
-build/host/sendfile/send-sendfile: sendfile/send-sendfile
+$(BUILD)/host/sendfile/send-sendfile: local/sendfile/send-sendfile
 	$(MKDIR) $(dir $@)
 	$(CP) $< $@
 
-build/pearl/bin/receive-sendfile: sendfile/receive-sendfile.c
+$(BUILD)/pearl/bin/receive-sendfile: local/sendfile/receive-sendfile.c
 	$(MKDIR) $(dir $@)
-	$(CROSS_COMPILE)gcc -static -Os -o $@ $<
+	$(WITH_CROSS_PATH) $(CROSS_COMPILE)gcc -static -Os -o $@ $<
 
 %.image.sendfile: %.image %.image.d/sendfile
 	$(MKDIR) $@.d
@@ -14,3 +14,5 @@ build/pearl/bin/receive-sendfile: sendfile/receive-sendfile.c
 
 %.sendfile{send}: %.sendfile
 	$(SUDO) local/sendfile/send-sendfile $<
+
+$(call pearl-static,$(BUILD)/pearl/bin/receive-sendfile,$(BUILD)/pearl/bin)
