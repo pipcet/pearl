32b7337fee6b6f93df281b1bd95cddc13a255497
Pip Cet <pipcet@gmail.com>
2021-06-23 12:45:44 +0000

    userspace/ncurses/ncurses.mk: install ncurses.pc

--- userspace/ncurses/ncurses.mk
+++ userspace/ncurses/ncurses.mk
@@ -7,7 +7,7 @@ $(BUILD)/ncurses/done/build: $(BUILD)/ncurses/done/configure
 	@touch $@
 
 $(BUILD)/ncurses/done/configure: $(BUILD)/ncurses/done/copy $(BUILD)/glibc/done/glibc/install $(BUILD)/gcc/done/gcc/install
-	(cd $(BUILD)/ncurses/build; $(WITH_CROSS_PATH) ./configure --host=aarch64-linux-gnu --target=aarch64-linux-gnu --prefix=/ --with-install-prefix=$(BUILD)/pearl/install --disable-stripping CFLAGS="$(CROSS_CFLAGS)" CXXFLAGS="$(CROSS_CFLAGS)" --without-cxx-binding)
+	(cd $(BUILD)/ncurses/build; $(WITH_CROSS_PATH) ./configure --host=aarch64-linux-gnu --target=aarch64-linux-gnu --prefix=/ --with-install-prefix=$(BUILD)/pearl/install --disable-stripping CFLAGS="$(CROSS_CFLAGS)" CXXFLAGS="$(CROSS_CFLAGS)" --without-cxx-binding --enable-pc-files=yes)
 	@touch $@
 
 $(BUILD)/ncurses/done/copy: $(BUILD)/ncurses/done/checkout | $(BUILD)/ncurses/done/ $(BUILD)/ncurses/build/
