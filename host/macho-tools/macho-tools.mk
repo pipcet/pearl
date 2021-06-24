549e96229eabc62038f15dd6e2fc7052ed99bb6b
Pip Cet <pipcet@gmail.com>
2021-06-24 12:57:13 +0000

    host/macho-tools/macho-tools.mk: fix paths

--- host/macho-tools/macho-tools.mk
+++ host/macho-tools/macho-tools.mk
@@ -1,4 +1,4 @@
-$(BUILD)/host/image-to-macho: macho-tools/image-to-macho.c | $(BUILD)/host/
+$(BUILD)/host/image-to-macho: host/macho-tools/image-to-macho.c | $(BUILD)/host/
 	gcc -Os -I$(BUILD)/macho-tools -o $@ $<
 
 $(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/perform-alignment..h
@@ -9,14 +9,14 @@ $(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/restore-boot-args..h
 $(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/nop..h
 $(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/bring-up-phys..h
 
-$(BUILD)/host/macho-to-image: macho-tools/macho-to-image.c | $(BUILD)/host/
+$(BUILD)/host/macho-to-image: host/macho-tools/macho-to-image.c | $(BUILD)/host/
 	gcc -Os -Ibuild/macho-tools -o $@ $<
 
 $(BUILD)/host/macho-to-image: $(BUILD)/macho-tools/macho-boot..h
 $(BUILD)/host/macho-to-image: $(BUILD)/macho-tools/image-header..h
 $(BUILD)/host/macho-to-image: $(BUILD)/macho-tools/disable-timers..h
 
-$(BUILD)/macho-image-fill: macho-tools/macho-image-fill
+$(BUILD)/macho-image-fill: host/macho-tools/macho-image-fill
 	$(MKDIR) $(dir $@)
 	$(CP) $< $@
 
