49f6d5f585b805468223eb4533af9973f4f7e98b
Pip Cet <pipcet@gmail.com>
2021-06-25 08:28:48 +0000

    host/snippet/snippet.mk: minor fix

--- host/snippet/snippet.mk
+++ host/snippet/snippet.mk
@@ -1,3 +1,6 @@
+$(BUILD)/%.c: %.c ; $(COPY)
+$(BUILD)/%.S: %.S ; $(COPY)
+
 %..h: %.c.S.elf.bin.s.h ; $(COPY)
 %..h: %.S.elf.bin.s.h ; $(COPY)
 %..bin: %.c.S.elf.bin ; $(COPY)
@@ -16,5 +19,5 @@ $(BUILD)/include/snippet.h: snippet/snippet.h | $(BUILD)/include/ ; $(COPY)
 %.bin.s: %.bin
 	$(CROSS_COMPILE)objdump -maarch64 --disassemble-zeroes -D -bbinary $< > $@
 
-%.s.h: $(BUILD)/%.s
+%.s.h: %.s
 	(NAME=$$(echo $(notdir $*) | sed -e 's/\..*//' -e 's/-/_/g'); echo "unsigned int $$NAME[] = {";  cat $< | tail -n +8 | sed -e 's/\t/ /g' | sed -e 's/^\(.*\):[ \t]*\([0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]\)[ \t]*\(.*\)$$/\t0x\2 \/\* \1: \3 \*\/,/g'; echo "};") > $@
