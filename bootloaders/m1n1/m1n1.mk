bfd6b79bdaacd58ab97b8c5eae7539dcdd214c44
Pip Cet <pipcet@gmail.com>
2021-06-26 21:11:17 +0000

    bootloaders/m1n1/m1n1.mk: build again

--- bootloaders/m1n1/m1n1.mk
+++ bootloaders/m1n1/m1n1.mk
@@ -3,6 +3,9 @@ M1N1DEVICE ?= $(shell ls /dev/ttyACM* | tail -2 | head -1)
 $(BUILD)/m1n1.macho: $(BUILD)/m1n1/done/build
 	$(CP) $(BUILD)/m1n1/build/build/m1n1.macho $@
 
+$(BUILD)/m1n1/done/install: $(BUILD)/m1n1/done/build
+	@touch $@
+
 $(BUILD)/m1n1/done/build: $(BUILD)/m1n1/done/copy
 	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/m1n1/build build/m1n1.macho
 	@touch $@
@@ -14,6 +17,11 @@ $(BUILD)/m1n1/done/copy: $(BUILD)/m1n1/done/checkout | $(BUILD)/m1n1/done/ $(BUI
 $(BUILD)/m1n1/done/checkout: | $(BUILD)/m1n1/done/
 	$(MAKE) bootloaders/m1n1/m1n1{checkout}
 	@touch $@
+
+$(BUILD)/initramfs/pearl.cpiospec: $(BUILD)/initramfs/pearl/boot/m1n1.macho
+$(BUILD)/initramfs/pearl/boot/m1n1.macho: $(BUILD)/m1n1.macho ; $(COPY)
+
+$(call pearl-static,bootloaders/m1n1/pearl/bin/m1n1,bootloaders/m1n1/pearl)
 
 %.macho{m1n1}: %.macho
 	M1N1DEVICE=$(M1N1DEVICE) python3 ./submodule/m1n1/proxyclient/chainload.py $<
