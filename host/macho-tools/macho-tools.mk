013947550eaffc23e397648378082c9b2253abb2
Pip Cet <pipcet@gmail.com>
2021-06-26 10:05:16 +0000

    host/macho-tools/macho-tools.mk: fix paths

--- host/macho-tools/macho-tools.mk
+++ host/macho-tools/macho-tools.mk
@@ -1,20 +1,20 @@
 $(BUILD)/host/image-to-macho: host/macho-tools/image-to-macho.c | $(BUILD)/host/
-	gcc -Os -I$(BUILD)/macho-tools -o $@ $<
+	gcc -Os -I$(BUILD)/host/macho-tools -o $@ $<
 
-$(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/perform-alignment..h
-$(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/x8r8g8b8..h
-$(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/enable-all-clocks..h
-$(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/save-boot-args..h
-$(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/restore-boot-args..h
-$(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/nop..h
-$(BUILD)/host/image-to-macho: $(BUILD)/macho-tools/bring-up-phys..h
+$(BUILD)/host/image-to-macho: $(BUILD)/host/macho-tools/perform-alignment..h
+$(BUILD)/host/image-to-macho: $(BUILD)/host/macho-tools/x8r8g8b8..h
+$(BUILD)/host/image-to-macho: $(BUILD)/host/macho-tools/enable-all-clocks..h
+$(BUILD)/host/image-to-macho: $(BUILD)/host/macho-tools/save-boot-args..h
+$(BUILD)/host/image-to-macho: $(BUILD)/host/macho-tools/restore-boot-args..h
+$(BUILD)/host/image-to-macho: $(BUILD)/host/macho-tools/nop..h
+$(BUILD)/host/image-to-macho: $(BUILD)/host/macho-tools/bring-up-phys..h
 
 $(BUILD)/host/macho-to-image: host/macho-tools/macho-to-image.c | $(BUILD)/host/
-	gcc -Os -Ibuild/macho-tools -o $@ $<
+	gcc -Os -I$(BUILD)/host/macho-tools -o $@ $<
 
-$(BUILD)/host/macho-to-image: $(BUILD)/macho-tools/macho-boot..h
-$(BUILD)/host/macho-to-image: $(BUILD)/macho-tools/image-header..h
-$(BUILD)/host/macho-to-image: $(BUILD)/macho-tools/disable-timers..h
+$(BUILD)/host/macho-to-image: $(BUILD)/host/macho-tools/macho-boot..h
+$(BUILD)/host/macho-to-image: $(BUILD)/host/macho-tools/image-header..h
+$(BUILD)/host/macho-to-image: $(BUILD)/host/macho-tools/disable-timers..h
 
 $(BUILD)/macho-image-fill: host/macho-tools/macho-image-fill
 	$(MKDIR) $(dir $@)
