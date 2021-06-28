6b98a86afb437f77842efc6f2c71d88129747b2c
Pip Cet <pipcet@gmail.com>
2021-06-28 10:35:57 +0000

    userspace/wpa/wpa.mk: add dep

--- userspace/wpa/wpa.mk
+++ userspace/wpa/wpa.mk
@@ -6,7 +6,7 @@ $(BUILD)/wpa_supplicant/done/build: $(BUILD)/wpa_supplicant/done/configure
 	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/wpa_supplicant/build/wpa_supplicant $(WITH_CROSS_CC) EXTRA_CFLAGS="$(CROSS_CFLAGS) -I$(BUILD)/pearl/install/include/libnl3" PKG_CONFIG=/bin/false
 	@touch $@
 
-$(BUILD)/wpa_supplicant/done/configure: userspace/wpa/wpa_supplicant.config $(BUILD)/wpa_supplicant/done/copy $(BUILD)/libnl/done/install
+$(BUILD)/wpa_supplicant/done/configure: userspace/wpa/wpa_supplicant.config $(BUILD)/wpa_supplicant/done/copy $(BUILD)/libnl/done/install $(BUILD)/openssl/done/install
 	cp $< $(BUILD)/wpa_supplicant/build/wpa_supplicant/.config
 	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/wpa_supplicant/build/wpa_supplicant $(WITH_CROSS_CC) EXTRA_CFLAGS="$(CROSS_CFLAGS) -I$(BUILD)/pearl/install/include/libnl3" PKG_CONFIG=/bin/false defconfig
 	@touch $@
