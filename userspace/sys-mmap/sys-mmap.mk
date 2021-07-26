61f8ebc85435a06ac99da48a9130850ec7b9ed41
Pip Cet <pipcet@gmail.com>
2021-07-26 14:21:36 +0000

    userspace/sys-mmap/sys-mmap.mk: use a wildcard for the perl version

--- userspace/sys-mmap/sys-mmap.mk
+++ userspace/sys-mmap/sys-mmap.mk
@@ -6,11 +6,11 @@ $(BUILD)/sys-mmap/sys-mmap.tar: $(BUILD)/sys-mmap/done/build
 	tar -C $(BUILD)/sys-mmap -cf $@ done build
 
 $(BUILD)/sys-mmap/done/build: $(BUILD)/sys-mmap/done/configure
-	$(WITH_CROSS_PATH) $(WITH_CROSS_CC) $(WITH_QEMU) $(MAKE) -C $(BUILD)/sys-mmap/build INSTALLPRIVLIB=$(PWD)/build/pearl/install/lib/perl5/5.35.2 INSTALLSITELIB=$(PWD)/build/pearl/install/lib/perl5/site_perl
+	$(WITH_CROSS_PATH) $(WITH_CROSS_CC) $(WITH_QEMU) $(MAKE) -C $(BUILD)/sys-mmap/build INSTALLPRIVLIB=$(PWD)/build/pearl/install/lib/perl5/5.35.* INSTALLSITELIB=$(PWD)/build/pearl/install/lib/perl5/site_perl
 	@touch $@
 
 $(BUILD)/sys-mmap/done/configure: $(BUILD)/sys-mmap/done/copy $(call deps,perl)
-	(cd $(BUILD)/sys-mmap/build; $(WITH_CROSS_PATH) $(WITH_CROSS_CC) $(WITH_QEMU) PERL_ARCHLIB=$(PWD)/build/pearl/install/lib/perl5/5.35.2/linux-gnu PERL_LIB=$(PWD)/build/pearl/install/lib/perl5/5.35.2/linux-gnu PERL_INCDEP=$(PWD)/build/pearl/install/lib/perl5/5.35.2/linux-gnu/CORE $(PWD)/build/pearl/install/bin/perl -I $(PWD)/build/pearl/install/lib/perl5/5.35.2/linux-gnu -I $(PWD)/build/pearl/install/lib/perl5/5.35.2/ Makefile.PL PERLPREFIX=$(PWD)/build/pearl/install/ PERL_ARCHLIB=$(PWD)/build/pearl/install/lib/perl5/5.35.2/linux-gnu PERL_INC=$(PWD)/build/pearl/install/lib/perl5/5.35.2/linux-gnu/CORE INC=$(PWD)/build/pearl/install/lib/perl5/5.35.2/linux-gnu/CORE PERL_INCDEP=$(PWD)/build/pearl/install/lib/perl5/5.35.2/linux-gnu/CORE PERL_LIB=$(PWD)/build/pearl/install/lib/perl5/5.35.2)
+	(cd $(BUILD)/sys-mmap/build; $(WITH_CROSS_PATH) $(WITH_CROSS_CC) $(WITH_QEMU) PERL_ARCHLIB=$(PWD)/build/pearl/install/lib/perl5/5.35.*/linux-gnu PERL_LIB=$(PWD)/build/pearl/install/lib/perl5/5.35.*/linux-gnu PERL_INCDEP=$(PWD)/build/pearl/install/lib/perl5/5.35.*/linux-gnu/CORE $(PWD)/build/pearl/install/bin/perl -I $(PWD)/build/pearl/install/lib/perl5/5.35.*/linux-gnu -I $(PWD)/build/pearl/install/lib/perl5/5.35.*/ Makefile.PL PERLPREFIX=$(PWD)/build/pearl/install/ PERL_ARCHLIB=$(PWD)/build/pearl/install/lib/perl5/5.35.*/linux-gnu PERL_INC=$(PWD)/build/pearl/install/lib/perl5/5.35.*/linux-gnu/CORE INC=$(PWD)/build/pearl/install/lib/perl5/5.35.*/linux-gnu/CORE PERL_INCDEP=$(PWD)/build/pearl/install/lib/perl5/5.35.*/linux-gnu/CORE PERL_LIB=$(PWD)/build/pearl/install/lib/perl5/5.35.*)
 	@touch $@
 
 $(BUILD)/sys-mmap/done/copy: $(BUILD)/sys-mmap/done/checkout | $(BUILD)/sys-mmap/done/ $(BUILD)/sys-mmap/build/
