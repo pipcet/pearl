26be88f80b2edeee7503403a123d8cbfb1baa349
Pip Cet <pipcet@gmail.com>
2021-07-26 08:13:45 +0000

    local/memdump/memdump.mk: fix paths

--- local/memdump/memdump.mk
+++ local/memdump/memdump.mk
@@ -1,8 +1,8 @@
-build/memdump/macho-to-memdump: local/memdump/macho-to-memdump ; $(COPY)
-build/memdump/memdump-to-image: local/memdump/memdump-to-image.c | build/memdump/
+build/memdump/bin/macho-to-memdump: local/memdump/macho-to-memdump | build/memdump/bin/ ; $(COPY)
+build/memdump/bin/memdump-to-image: local/memdump/memdump-to-image.c | build/memdump/bin/
 	$(WITH_CROSS_PATH) $(WITH_CROSS_CC) $(CROSS_CC) -static -Os -Ibuild/local/memdump -o $@ $<
 
-build/memdump/memdump-to-image: build/local/memdump/boot..h
-build/memdump/memdump-to-image: build/local/memdump/image..h
+build/memdump/bin/memdump-to-image: build/local/memdump/boot..h
+build/memdump/bin/memdump-to-image: build/local/memdump/image..h
 
-$(call pearl-static,build/memdump/memdump-to-image build/memdump/macho-to-memdump,$(BUILD)/memdump)
+$(call pearl-static,build/memdump/bin/memdump-to-image build/memdump/bin/macho-to-memdump,build/memdump)
