d1bcc266ad57a88ef00db906f0fc85c6947f141d
Pip Cet <pipcet@gmail.com>
2021-07-31 12:56:35 +0000

    debian: add workflow, add rules to build the initrd

--- debian/debootstrap/debootstrap.mk
+++ debian/debootstrap/debootstrap.mk
@@ -8,3 +8,12 @@ $(BUILD)/debian/debootstrap/stage15.tar: $(BUILD)/debian/debootstrap/stage1.tar
 	(cd $(BUILD)/debian/debootstrap/stage15/var/cache/apt/archives/; for a in *.deb; do sudo dpkg-deb -R $$a $$a.d; sudo dpkg-deb -b -Znone $$a.d; sudo mv $$a.d.deb $$a; sudo rm -rf $$a.d; done)
 	for a in $(BUILD)/debian/debootstrap/stage15/var/cache/apt/archives/*.deb; do sudo dpkg -x $$a $(BUILD)/debian/debootstrap/stage15; done
 	(cd $(BUILD)/debian/debootstrap/stage15; sudo tar c .) > $@
+
+$(BUILD)/debian.cpio: $(BUILD)/debian/debootstrap/stage15.tar
+	$(MKDIR) $(BUILD)/debian/cpio.d
+	(cd $(BUILD)/debian/cpio.d; sudo tar x) < $<
+	sudo ln -sf sbin/init $(BUILD)/debian/cpio.d/init
+	(cd $(BUILD)/debian/cpio.d; sudo find | sudo cpio -o -H newc) > $@
+
+$(BUILD)/debian.cpio.zst: $(BUILD)/debian.cpio
+	zstd -22 --ultra --long --verbose < $< > $@
