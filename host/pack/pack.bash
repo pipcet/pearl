c0ecb7b6c3af9d3acd522e7366af505b0ab20c3e
Pip Cet <pipcet@gmail.com>
2021-09-05 16:07:00 +0000

    host/pack/pack.bash: update

--- host/pack/pack.bash
+++ host/pack/pack.bash
@@ -1,10 +1,22 @@
 #!/bin/sh
-echo "#!/bin/bash"
+QUOTE='"'
+if [ "x$1" = "x--zsh" ]; then
+    shift;
+    echo "#!/bin/zsh"
+else
+    echo "#!/bin/bash"
+fi
 NONCE=
-for i in $(seq 1 8); do
+for i in 1 2 3 4 5 6 7 8; do
     NONCE=$NONCE$(printf "%04x" $RANDOM)
 done
-echo head -c $(wc -c < $1) '>' "/persist/$(basename $1)" '<<"'$NONCE'"'
-cat $1
-echo
-echo $NONCE
+DIR="$1"
+shift;
+for FILE; do
+    BASENAME="$(echo "$FILE" | sed -e 's/.*\///')"
+    echo mkdir -p "$DIR"
+    echo head -c $(wc -c < $FILE) '>' "$DIR/$BASENAME" '<<'"$QUOTE$NONCE$QUOTE"
+    cat $FILE
+    echo
+    echo $NONCE
+done
