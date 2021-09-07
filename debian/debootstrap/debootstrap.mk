a42f307bc64572ad93680678ed5669e23bec04c0
Pip Cet <pipcet@gmail.com>
2021-09-07 16:12:40 +0000

    debian/debootstrap/debootstrap.mk: d-i rules

--- debian/debootstrap/debootstrap.mk
+++ debian/debootstrap/debootstrap.mk
@@ -23,3 +23,44 @@ $(BUILD)/debian.cpio.zst: $(BUILD)/debian.cpio
 
 $(BUILD)/debian.cpio.xz: $(BUILD)/debian.cpio
 	xz --compress < $< > $@
+
+$(BUILD)/debian/di-debootstrap.cpio: | $(BUILD)/debian/
+	sudo rm -rf $(BUILD)/debian/di-debootstrap
+	sudo DEBOOTSTRAP_DIR=$(PWD)/debian/debootstrap/debootstrap ./debian/debootstrap/debootstrap/debootstrap --foreign --arch=arm64 --include=build-essential,git,linux-image-cloud-arm64,bash,kmod,dash,wget,busybox,busybox-static,net-tools,libpam-systemd,file,xsltproc,mtools,openssl,mokutil,libx11-data,libx11-6,sharutils,dpkg-dev sid $(BUILD)/debian/di-debootstrap http://deb.debian.org/debian
+	sudo chmod a+r -R $(BUILD)/debian/di-debootstrap/root
+	sudo chmod a+x $(BUILD)/debian/di-debootstrap/root
+	(cd $(BUILD)/debian/di-debootstrap/root; sudo git clone https://github.com/pipcet/debian-installer)
+	(cd $(BUILD)/debian/di-debootstrap/root/debian-installer; sudo mr checkout)
+	sudo rm -f $(BUILD)/debian/di-debootstrap/init
+	(echo '#!/bin/bash'; \
+	echo "export PATH"; \
+	echo "/debootstrap/debootstrap --second-stage"; \
+	echo "/bin/busybox mount -t proc proc proc"; \
+	echo "depmod -a"; \
+	echo "modprobe virtio"; \
+	echo "modprobe virtio_pci"; \
+	echo "modprobe virtio_net"; \
+	echo "dhclient -v eth0 &"; \
+	echo "echo deb-src https://deb.debian.org/debian sid main >> /etc/apt/sources.list"; \
+	echo "apt-get -y update"; \
+	echo "apt-get -y dist-ugprade"; \
+	echo "apt-get -y build-dep debian-installer anna"; \
+	echo "apt-get -y clean"; \
+	echo "(cd /root/debian-installer/packages/anna; ./debian/rules build)"; \
+	echo "(cd /root/debian-installer/packages/anna; ./debian/rules binary)"; \
+	echo "cp /root/debian-installer/packages/anna_*_arm64.udeb /root/debian-installer/installer/build/localudebs/"; \
+	echo "(cd /root/debian-installer/installer/build; make build_netboot-gtk)"; \
+	echo "uuencode 'netboot.tar.gz' < /root/debian-installer/installer/build/dest/netboot/gtk/netboot.tar.gz") | sudo tee $(BUILD)/debian/di-debootstrap/init
+	sudo chmod u+x $(BUILD)/debian/di-debootstrap/init
+	(cd $(BUILD)/debian/di-debootstrap; sudo find . | sudo cpio -H newc -o) > $@
+
+$(BUILD)/netboot.tar.gz.uuencoded: $(BUILD)/qemu-kernel $(BUILD)/debian/di-debootstrap.cpio
+	qemu-system-aarch64 -machine virt -cpu max -kernel $(BUILD)/qemu-kernel -m 6g -serial stdio -initrd ./build/debian/di-debootstrap.cpio -nic user,model=virtio -monitor none -smp 8 > $@
+
+$(BUILD)/netboot.tar.gz: $(BUILD)/netboot.tar.gz.uuencoded
+	uudecode -o $@ < $<
+
+$(BUILD)/netboot-initrd.cpio.gz: $(BUILD)/netboot.tar.gz
+	rm -rf $(BUILD)/netboot-tmp
+	tar -C $(BUILD)/netboot-tmp -xzvf $<
+	cp $(BUILD)/netboot-tmp/debian-installer/arm64/initrd.gz $@
