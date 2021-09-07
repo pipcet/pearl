935b69066e66846bd5c7a20d813eabd9ac40778b
Pip Cet <pipcet@gmail.com>
2021-09-07 16:07:29 +0000

    debian/deb/deb.mk: extract a qemu-suitable kernel from the right .deb

--- debian/deb/deb.mk
+++ debian/deb/deb.mk
@@ -122,4 +122,9 @@ $(BUILD)/debian/deb.tar: \
 	for file in $^; do if which dpkg > /dev/null 2>&1; then dpkg -x $$file $(BUILD)/deb-tmp; else ar -x $$file --output $(BUILD)/deb-tmp-ar && tar -C $(BUILD)/deb-tmp -axf $(BUILD)/deb-tmp-ar/data.tar.*; rm -rf $(BUILD)/deb-tmp-ar; fi; done
 	(tar -C $(BUILD)/deb-tmp -cf $@ .)
 
+$(BUILD)/qemu-kernel: build/debian/deb/linux-image-5.10.0-8-cloud-arm64.deb
+	mkdir $(BUILD)/kernel
+	dpkg --extract $< $(BUILD)/kernel
+	cp $(BUILD)/kernel/boot/vmlinuz* $@
+
 # $(call pearl-static,$(BUILD)/debian/deb.tar,$(BUILD))
