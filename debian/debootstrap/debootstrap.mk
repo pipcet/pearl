310a98a6b1cbce57701201538b76ee39aaa8c0e4
Pip Cet <pipcet@gmail.com>
2021-09-09 15:33:36 +0000

    debian/debootstrap/debootstrap.mk: let's see whether this works

--- debian/debootstrap/debootstrap.mk
+++ debian/debootstrap/debootstrap.mk
@@ -9,9 +9,14 @@ $(BUILD)/debian/debootstrap/stage15.tar: $(BUILD)/debian/debootstrap/stage1.tar
 	for a in $(BUILD)/debian/debootstrap/stage15/var/cache/apt/archives/*.deb; do sudo dpkg -x $$a $(BUILD)/debian/debootstrap/stage15; done
 	(cd $(BUILD)/debian/debootstrap/stage15; sudo tar c .) > $@
 
-$(BUILD)/debian.cpio: $(BUILD)/debian/debootstrap/stage15.tar
+$(BUILD)/debian/installer.cpio.gz: $(BUILD)/debian/
+	wget -O $@ https://github.com/pipcet/debian-installer/releases/latest/download/netboot-initrd.cpio.gz
+
+$(BUILD)/debian.cpio: $(BUILD)/debian/debootstrap/stage15.tar $(BUILD)/debian/installer.cpio.gz
 	$(MKDIR) $(BUILD)/debian/cpio.d
 	(cd $(BUILD)/debian/cpio.d; sudo tar x) < $<
+	cp $(BUILD)/debian/installer.cpio.gz $(BUILD)/debian/cpio.d
+	sudo chown root.root $(BUILD)/debian/cpio.d
 	sudo ln -sf sbin/init $(BUILD)/debian/cpio.d/init
 	(cd $(BUILD)/debian/cpio.d; sudo find | sudo cpio -o -H newc) > $@
 
