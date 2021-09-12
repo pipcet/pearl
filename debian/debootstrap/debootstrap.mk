d9651d5ac2fc6dac3d3d470ea222f11f94d9268c
Pip Cet <pipcet@gmail.com>
2021-09-12 07:36:19 +0000

    strip down the image a bit further

--- debian/debootstrap/debootstrap.mk
+++ debian/debootstrap/debootstrap.mk
@@ -6,7 +6,7 @@ $(BUILD)/debian/debootstrap/stage15.tar: $(BUILD)/debian/debootstrap/stage1.tar
 	$(MKDIR) $(BUILD)/debian/debootstrap/stage15
 	(cd $(BUILD)/debian/debootstrap/stage15; sudo tar x) < $<
 	(cd $(BUILD)/debian/debootstrap/stage15/var/cache/apt/archives/; for a in *.deb; do sudo dpkg-deb -R $$a $$a.d; sudo dpkg-deb -b -Znone $$a.d; sudo mv $$a.d.deb $$a; sudo rm -rf $$a.d; done)
-	for a in $(BUILD)/debian/debootstrap/stage15/var/cache/apt/archives/*.deb; do sudo dpkg -x $$a $(BUILD)/debian/debootstrap/stage15; done
+#	for a in $(BUILD)/debian/debootstrap/stage15/var/cache/apt/archives/*.deb; do sudo dpkg -x $$a $(BUILD)/debian/debootstrap/stage15; done
 	(cd $(BUILD)/debian/debootstrap/stage15; sudo tar c .) > $@
 
 $(BUILD)/debian/full-installer.cpio.gz: | $(BUILD)/debian/
@@ -30,6 +30,7 @@ $(BUILD)/debian/installer: debian/injected/bin/installer
 $(BUILD)/debian.cpio: $(BUILD)/debian/debootstrap/stage15.tar $(BUILD)/debian/installer.cpio $(BUILD)/debian/installer
 	$(MKDIR) $(BUILD)/debian/cpio.d
 	(cd $(BUILD)/debian/cpio.d; sudo tar x) < $<
+	sudo rm -rf $(BUILD)/debian/cpio.d/var/lib/apt/lists/*
 	sudo cp $(BUILD)/debian/installer.cpio $(BUILD)/debian/cpio.d
 	sudo cp $(BUILD)/debian/installer $(BUILD)/debian/cpio.d/bin
 	sudo chown root.root $(BUILD)/debian/cpio.d
