74b488c541ae5b3cc49fad31c2dd4ef58f86c328
Pip Cet <pipcet@gmail.com>
2022-07-06 13:22:30 +0000

    put sendfiles in a directory of their own

---
+++ sendfile/sendfile.mk
@@ -0,0 +1,38 @@
+kernels = linux stage2 pearl
+models = j274 j293 j313
+stages = root shell stage2
+
+define stagerule
+$(BUILD)/sendfile/linux-$(stage).sendfile: $(BUILD)/linux/linux.modules
+
+$(BUILD)/sendfile/linux-$(stage).sendfile: $(BUILD)/linux/linux.image $(foreach model,$(models),$(BUILD)/linux/linux-$(model).dtb) $(BUILD)/sendfile/linux-$(stage).sendfile.d/sendfile
+	$(CP) $$(filter-out $(BUILD)/sendfile/linux-$(stage).sendfile.d/sendfile,$$^) $$@.d
+	$(CP) $(BUILD)/sendfile/linux-$(stage).sendfile.d/sendfile $$@.d/script
+	tar -C $$@.d -c . | gzip -1 > $$@
+
+$(BUILD)/sendfile/linux-$(stage).sendfile.d/sendfile: $(BUILD)/linux/linux.image | $(BUILD)/sendfile/linux-$(stage).sendfile.d/
+	echo "#!/bin/sh" > $$@
+	echo "cp linux-\`cat /persist/model\`.dtb linux.dtb" >> $$@
+	echo "cp linux.dtb persist" >> $$@
+	echo "cp linux.dtb boot" >> $$@
+	echo "cp linux.modules persist" >> $$@
+	echo "cp linux.modules boot" >> $$@
+	echo "egrep -v persist < /file.list > /file.list.new" >> $$@
+	echo "mv /file.list.new /file.list" >> $$@
+	echo "echo final > /persist/stage" >> $$@
+	echo "echo $(stage) > /persist/substages" >> $$@
+	echo "find persist >> /file.list" >> $$@
+	echo "cat /file.list | cpio -H newc -o > /boot/linux.cpio" >> $$@
+	echo "dt dtb-to-dtp /boot/linux.dtb /boot/linux.dtp" >> $$@
+	echo "cat /boot/resmem.dtp >> /boot/linux.dtp" >> $$@
+	echo "cat /boot/bootargs.dtp >> /boot/linux.dtp" >> $$@
+	echo "cat /boot/tunables.dtp >> /boot/linux.dtp" >> $$@
+	echo "dt permallocs >> /boot/linux.dtp" >> $$@
+	echo "dt dtp-to-dtb /boot/linux.dtp /boot/linux.dtb" >> $$@
+	$(if $(filter $(stage),stage2),,echo "/bin/kexec --mem-min=\`dt mem-min\` -fix linux.image --dtb=/boot/linux.dtb --ramdisk=/boot/linux.cpio --command-line=\"clk_ignore_unused\"" >> $$@)
+	chmod u+x $$@
+
+
+endef
+
+$(eval $(foreach stage,$(stages),$(stagerule)))
