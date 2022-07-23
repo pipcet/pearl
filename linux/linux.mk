47993d390733f0fca5726c4d34727a05e605a574
Pip Cet <pipcet@gmail.com>
2022-07-23 21:41:08 +0000

    linux/linux.mk: minor fix

--- linux/linux.mk
+++ linux/linux.mk
@@ -112,7 +112,7 @@ $(call done,linux,%/build): $(call done,linux,%/configure)
 	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/linux/$*/build ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) dtbs
 	@touch $@
 
-$(call done,linux,%/configure): $(BUILD)/linux/%.config $(call done,linux,%/copy) $(call done,gcc,gcc/install)
+$(call done,linux,%/configure): $(BUILD)/linux/%.config $(call done,linux,%/copy) $(call done,toolchain/gcc,gcc/install)
 	$(CP) $< $(BUILD)/linux/$*/build/.config
 	PATH="$(CROSS_PATH):$$PATH" $(MAKE) -C $(BUILD)/linux/$*/build ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) olddefconfig
 	@touch $@
