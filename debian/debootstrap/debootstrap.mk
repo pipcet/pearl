f4df2cc773b99a9be55cc6ae7c1e539115aebee1
Pip Cet <pipcet@gmail.com>
2022-07-23 08:30:30 +0000

    debian/debootstrap/debootstrap.mk: guess we can't get away with it after all

--- debian/debootstrap/debootstrap.mk
+++ debian/debootstrap/debootstrap.mk
@@ -1,6 +1,5 @@
 $(BUILD)/debian/debootstrap/stage1.tar: | $(BUILD)/debian/debootstrap/
 	sudo DEBOOTSTRAP_DIR=$(PWD)/debian/debootstrap/debootstrap ./debian/debootstrap/debootstrap/debootstrap --foreign --arch=arm64 --include=dash,wget,busybox,busybox-static,network-manager,openssh-client,net-tools,libpam-systemd,cryptsetup,lvm2,memtool,nvme-cli,watchdog,minicom,device-tree-compiler,file,gpm,ssh,usbutils,pciutils,wpasupplicant,ntpdate sid $(BUILD)/debian/debootstrap/stage1 http://deb.debian.org/debian
-	sudo rm -f $(BUILD)/debian/debootstrap/stage1/var/lib/apt/lists/*Packages*
 	(cd $(BUILD)/debian/debootstrap/stage1; sudo tar c .) > $@
 
 $(BUILD)/debian/debootstrap/stage15.tar: $(BUILD)/debian/debootstrap/stage1.tar
