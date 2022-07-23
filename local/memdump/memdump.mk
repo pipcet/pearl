2305bb8706ea906fa26efb8406a2e31acfef2ed5
Pip Cet <pipcet@gmail.com>
2022-07-23 23:25:09 +0000

    local/memdump/memdump.mk: how did this ever work?

--- local/memdump/memdump.mk
+++ local/memdump/memdump.mk
@@ -2,6 +2,12 @@ $(BUILD)/memdump/bin/macho-to-memdump: local/memdump/macho-to-memdump | $(BUILD)
 $(BUILD)/memdump/bin/memdump-to-image: local/memdump/memdump-to-image.c $(call deps,glibc gcc) | $(BUILD)/memdump/bin/
 	$(WITH_CROSS_PATH) $(WITH_CROSS_CC) $(CROSS_CC) -static -Os -I$(BUILD)/local/memdump -o $@ $<
 
+$(BUILD)/local/memdump/boot.c: local/memdump/boot.c
+	$(COPY)
+
+$(BUILD)/local/memdump/image.S: local/memdump/image.S
+	$(COPY)
+
 $(BUILD)/memdump/bin/memdump-to-image: $(BUILD)/local/memdump/boot..h
 $(BUILD)/memdump/bin/memdump-to-image: $(BUILD)/local/memdump/image..h
 
