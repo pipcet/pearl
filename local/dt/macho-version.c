5570695f14e78a2c2921b0ceb02bdcff4339813a
Pip Cet <pipcet@gmail.com>
2022-07-23 07:46:18 +0000

    local/dt/macho-version.c: don't read non-macho eaders

--- local/dt/macho-version.c
+++ local/dt/macho-version.c
@@ -7,9 +7,15 @@
 
 int main(int argc, char **argv)
 {
-  char buf[8192];
+  char buf[8193];
   if (read(0, buf, 8192) != 8192)
     return 1;
+  if (buf[0] != 0xcf ||
+      buf[1] != 0xfa ||
+      buf[2] != 0xed ||
+      buf[3] != 0xfe)
+    return 2;
+  buf[8192] = 0;
   char *p = buf + 8191;
   while (p[-1])
     p--;
