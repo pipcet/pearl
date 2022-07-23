f0553704a22ecdba840c5ebff731a581de7046a4
Pip Cet <pipcet@gmail.com>
2022-07-23 20:41:45 +0000

    toolchain/gcc/gcc.mk: fix

--- toolchain/gcc/gcc.mk
+++ toolchain/gcc/gcc.mk
@@ -10,7 +10,7 @@ $(BUILD)/toolchain/gcc/done/gcc/build: $(BUILD)/toolchain/gcc/done/gcc/configure
 	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/toolchain/gcc/gcc/build
 	@touch $@
 
-$(BUILD)/toolchain/gcc/done/gcc/configure: $(BUILD)/toolchain/gcc/done/gcc/copy $(BUILD)/linux/done/headers/install $(BUILD)/glibc/done/headers/install $(BUILD)/binutils-gdb/done/install | $(BUILD)/toolchain/gcc/gcc/build/
+$(BUILD)/toolchain/gcc/done/gcc/configure: $(BUILD)/toolchain/gcc/done/gcc/copy $(BUILD)/linux/done/headers/install $(BUILD)/userspace/glibc/done/headers/install $(BUILD)/binutils-gdb/done/install | $(BUILD)/toolchain/gcc/gcc/build/
 	(cd $(BUILD)/toolchain/gcc/gcc/build; $(WITH_CROSS_PATH) ../source/configure --target=aarch64-linux-gnu --enable-languages=c,lto --disable-bootstrap --prefix=/ --with-sysroot="$(BUILD)/pearl/install" --disable-libssp --disable-libquadmath --disable-libatomic --disable-libgomp --without-headers --disable-shared --with-static-standard-libraries --with-build-sysroot="$(BUILD)/pearl/install" --disable-c++tools)
 	@touch $@
 
@@ -31,7 +31,7 @@ $(BUILD)/toolchain/gcc/done/libgcc/build: $(BUILD)/toolchain/gcc/done/libgcc/con
 	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/toolchain/gcc/libgcc/build
 	@touch $@
 
-$(BUILD)/toolchain/gcc/done/libgcc/configure: $(BUILD)/toolchain/gcc/done/libgcc/copy $(BUILD)/linux/done/headers/install $(BUILD)/glibc/done/headers/install $(BUILD)/binutils-gdb/done/install $(BUILD)/glibc/done/glibc/install | $(BUILD)/toolchain/gcc/libgcc/build/
+$(BUILD)/toolchain/gcc/done/libgcc/configure: $(BUILD)/toolchain/gcc/done/libgcc/copy $(BUILD)/linux/done/headers/install $(BUILD)/userspace/glibc/done/headers/install $(BUILD)/binutils-gdb/done/install $(BUILD)/userspace/glibc/done/glibc/install | $(BUILD)/toolchain/gcc/libgcc/build/
 	(cd $(BUILD)/toolchain/gcc/libgcc/build; $(WITH_CROSS_PATH) ../source/configure --target=aarch64-linux-gnu --enable-languages=c,c++,lto --enable-shared --disable-bootstrap --prefix=/ --with-sysroot="$(BUILD)/pearl/install" --disable-libssp --disable-libquadmath --disable-libatomic --disable-libgomp --without-headers --with-build-sysroot="$(BUILD)/pearl/install" --disable-c++tools)
 	@touch $@
 
