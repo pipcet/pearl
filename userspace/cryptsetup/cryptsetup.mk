bdf2bc7d2032bd15a5357cd8957d603b4e983fb4
Pip Cet <pipcet@gmail.com>
2022-07-24 12:17:52 +0000

    userspace/cryptsetup/cryptsetup.mk: disable asciidoctor, whatever that is

--- userspace/cryptsetup/cryptsetup.mk
+++ userspace/cryptsetup/cryptsetup.mk
@@ -8,7 +8,7 @@ $(call done,userspace/cryptsetup,build): $(call done,userspace/cryptsetup,config
 
 $(call done,userspace/cryptsetup,configure): $(call done,userspace/cryptsetup,copy) $(call done,userspace/libuuid,install) $(call done,userspace/json-c,install) $(call done,userspace/popt,install) $(call done,userspace/libblkid,install) $(call done,userspace/lvm2,install) $(call done,userspace/openssl,install) $(call done,userspace/glibc,glibc/install) $(call done,toolchain/gcc,gcc/install)
 	(cd $(BUILD)/userspace/cryptsetup/build; $(WITH_CROSS_PATH) sh autogen.sh)
-	(cd $(BUILD)/userspace/cryptsetup/build; $(WITH_CROSS_PATH) ./configure --target=aarch64-linux-gnu --host=aarch64-linux-gnu --enable-ssh-token=no --prefix=/ CFLAGS="$(CROSS_CFLAGS)" LDFLAGS="$(CROSS_CFLAGS) -Wl,-rpath-link -Wl,$(BUILD)/pearl/install/lib" JSON_C_CFLAGS="-I$(BUILD)/pearl/install/include -I$(BUILD)/pearl/install/include/json-c" JSON_C_LIBS="-ljson-c")
+	(cd $(BUILD)/userspace/cryptsetup/build; $(WITH_CROSS_PATH) ./configure --target=aarch64-linux-gnu --host=aarch64-linux-gnu --disable-asciidoc --enable-ssh-token=no --prefix=/ CFLAGS="$(CROSS_CFLAGS)" LDFLAGS="$(CROSS_CFLAGS) -Wl,-rpath-link -Wl,$(BUILD)/pearl/install/lib" JSON_C_CFLAGS="-I$(BUILD)/pearl/install/include -I$(BUILD)/pearl/install/include/json-c" JSON_C_LIBS="-ljson-c")
 	$(TIMESTAMP)
 
 $(call done,userspace/cryptsetup,copy): $(call done,userspace/cryptsetup,checkout) | $(call done,userspace/cryptsetup,) $(BUILD)/userspace/cryptsetup/build/
