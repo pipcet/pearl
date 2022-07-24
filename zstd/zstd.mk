a707a1a2eef74882f0829616fe61bdf98da7e4c6
Pip Cet <pipcet@gmail.com>
2022-07-24 19:02:36 +0000

    zstd/zstd.mk: fix incorrect compiler used for zstdlib

--- zstd/zstd.mk
+++ zstd/zstd.mk
@@ -1,12 +1,12 @@
 #	for a in zstd/zstdmain.c zstd/zstd/lib/decompress/*.c zstd/zstd/lib/common/*.c; do aarch64-linux-gnu-gcc -mgeneral-regs-only -static -DPAYLOAD_SIZE=$$(wc -c < zstd/payload) -Wl,--script=zstd/zstd.lds -O3 -Wall -fno-exceptions -fpie -mstrict-align -S -o $$a.s -ffreestanding -fno-builtin-functions -nostdlib -fno-inline $$a; done
 
 $(BUILD)/zstd/zstdlib.elf: zstd/payload zstd/zstdmain.c | $(BUILD)/zstd/
-	aarch64-linux-gnu-gcc -mgeneral-regs-only -static -DPAYLOAD_SIZE=$$(wc -c < zstd/payload) -Wl,--script=zstd/zstd.lds -O3 -Wall -fno-exceptions -fpie -mstrict-align -o $@ -ffreestanding -fno-builtin-functions -nostdlib -fno-inline zstd/zstdmain.c zstd/zstd/lib/decompress/*.c zstd/zstd/lib/common/*.c zstd/chickens.S # -flto -fomit-frame-pointer -Os -fPIC 
+	$(WITH_CROSS_PATH) aarch64-linux-gnu-gcc -mgeneral-regs-only -static -DPAYLOAD_SIZE=$$(wc -c < zstd/payload) -Wl,--script=zstd/zstd.lds -O3 -Wall -fno-exceptions -fpie -mstrict-align -o $@ -ffreestanding -fno-builtin-functions -nostdlib -fno-inline zstd/zstdmain.c zstd/zstd/lib/decompress/*.c zstd/zstd/lib/common/*.c zstd/chickens.S # -flto -fomit-frame-pointer -Os -fPIC 
 #zstd/zstdlib.elf: zstd/zstd.c
 #	aarch64-linux-gnu-gcc -DPAYLOAD_SIZE=$$(wc -c < zstd/payload) -Wl,--script=zstd/zstd.lds -fPIC -Os -flto  -Wall -fno-exceptions -mstrict-align -o $@ -ffreestanding -fno-builtin-functions -nostdlib -fno-inline zstd/zstd.c # -flto -fomit-frame-pointer -Os -fPIC 
 
 $(BUILD)/zstd/zstdlib: $(BUILD)/zstd/zstdlib.elf
-	aarch64-linux-gnu-objcopy -Obinary $< $@
+	$(WITH_CROSS_PATH) aarch64-linux-gnu-objcopy -Obinary $< $@
 
 $(BUILD)/zstd/zstdout: $(BUILD)/zstd/zstdlib zstd/payload
 	cat $^ > $@
@@ -28,10 +28,10 @@ $(BUILD)/zstd/zstdout: $(BUILD)/zstd/zstdlib zstd/payload
 	wc -c < $< > $@
 
 %.macho.zstlib.elf: %.macho.zst.size %.macho.size %.macho.real.size zstd/zstdmain.c | $(BUILD)/zstd/
-	aarch64-linux-gnu-gcc -mgeneral-regs-only -static -DTOTAL_UNCOMPRESSED_SIZE=$$(cat $*.macho.size) -DUNCOMPRESSED_SIZE=$$(cat $*.macho.real.size) -DCOMPRESSED_SIZE=$$(cat $<) -Wl,--script=zstd/zstd.lds -O3 -Wall -fno-exceptions -fpie -mstrict-align -o $@ -ffreestanding -fno-builtin-functions -nostdlib -fno-inline zstd/zstdmain.c zstd/zstd/lib/decompress/*.c zstd/zstd/lib/common/*.c zstd/chickens.S # -flto -fomit-frame-pointer -Os -fPIC
+	$(WITH_CROSS_PATH) aarch64-linux-gnu-gcc -mgeneral-regs-only -static -DTOTAL_UNCOMPRESSED_SIZE=$$(cat $*.macho.size) -DUNCOMPRESSED_SIZE=$$(cat $*.macho.real.size) -DCOMPRESSED_SIZE=$$(cat $<) -Wl,--script=zstd/zstd.lds -O3 -Wall -fno-exceptions -fpie -mstrict-align -o $@ -ffreestanding -fno-builtin-functions -nostdlib -fno-inline zstd/zstdmain.c zstd/zstd/lib/decompress/*.c zstd/zstd/lib/common/*.c zstd/chickens.S # -flto -fomit-frame-pointer -Os -fPIC
 
 %.macho.zstlib: %.macho.zstlib.elf
-	aarch64-linux-gnu-objcopy -Obinary $< $@
+	$(WITH_CROSS_PATH) aarch64-linux-gnu-objcopy -Obinary $< $@
 
 %.macho.zst.image: %.macho.zstlib %.macho.zst
 	cat $^ > $@
