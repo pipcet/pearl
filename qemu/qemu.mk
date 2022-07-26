817029bc2efe0fc680a9c879df16bab04ac6e7dc
Pip Cet <pipcet@gmail.com>
2022-07-26 18:13:18 +0000

    qemu/qemu.mk: new file

---
+++ qemu/qemu.mk
@@ -0,0 +1,15 @@
+$(call done,qemu,build): $(call done,qemu,configure)
+	$(MAKE) -C $(BUILD)/qemu/build
+	$(TIMESTAMP)
+
+$(call done,qemu,configure): $(call done,qemu,copy)
+	(cd $(BUILD)/qemu; ./configure --target-list=aarch64-softmmu)
+	$(TIMESTAMP)
+
+$(call done,qemu,copy): $(call done,qemu,checkout) | $(BUILD)/qemu/
+	$(COPY_SAUNA) $(PWD)/qemu/qemu/* $(BUILD)/qemu/
+	$(TIMESTAMP)
+
+$(call done,qemu,checkout): $(call done,qemu,)
+	$(MAKE) qemu/qemu{checkout}
+	$(TIMESTAMP)
