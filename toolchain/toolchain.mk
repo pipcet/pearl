a17e4ed89f9095c2fa8c9da45068a8f5ac643a64
Pip Cet <pipcet@gmail.com>
2022-07-27 19:15:18 +0000

    toolchain/toolchain.mk: fix syntax error

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -2,6 +2,6 @@ include toolchain/binutils-gdb/binutils-gdb.mk
 include toolchain/gcc/gcc.mk
 
 $(BUILD)/toolchain.tar: $(call done,toolchain/binutils-gdb,install) $(call done,toolchain/gcc,gcc/install)
-	tar -C . -cf $@ $(patsubst $(PWD)/%,%,$(BUILD)/pearl/toolchain $(BUILD)/pearl/install $(BUILD)/pearl/toolchain $(wildcard $(call done,toolchain/*,*))
+	tar -C . -cf $@ $(patsubst $(PWD)/%,%,$(BUILD)/pearl/toolchain) $(BUILD)/pearl/install $(BUILD)/pearl/toolchain $(wildcard $(call done,toolchain/*,*))
 
 SECTARGETS += $(BUILD)/toolchain.tar
