cec37c798b75d76b9beb3daef7c24e02edf05781
Pip Cet <pipcet@gmail.com>
2022-07-28 14:21:55 +0000

    bootloaders/bootloaders.mk: add missing dep

--- bootloaders/bootloaders.mk
+++ bootloaders/bootloaders.mk
@@ -3,7 +3,7 @@ include bootloaders/barebox/barebox.mk
 include bootloaders/u-boot/u-boot.mk
 include bootloaders/grub/grub.mk
 
-$(BUILD)/bootloaders.tar: $(call done,bootloaders/m1n1,install) $(call done,bootloaders/barebox,install) $(call done,bootloaders/u-boot,install) $(call done,bootloaders/grub,install)
+$(BUILD)/bootloaders.tar: $(call done,bootloaders/m1n1,install) $(call done,bootloaders/barebox,install) $(call done,bootloaders/u-boot,install) $(call done,bootloaders/grub,install) $(BOOTLOADER_FILES)
 	tar -C . -cf $@ $(BOOTLOADER_FILES)
 
 SECTARGETS += $(BUILD)/bootloaders.tar
