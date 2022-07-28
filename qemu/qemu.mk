fd6d52035c8826610a116106d86072180aba2cdf
Pip Cet <pipcet@gmail.com>
2022-07-28 20:47:34 +0000

    qemu/qemu.mk: %.image.mp4 recipe. Because I can.

--- qemu/qemu.mk
+++ qemu/qemu.mk
@@ -47,6 +47,60 @@ $(call done,qemu,checkout): $(call done,qemu,)
 	 echo end; \
 	 echo c) > $@
 
+%.image.gdb2: %.image
+	(echo file ./build/linux/pearl/build/vmlinux; \
+	 echo set disassemble-next-line on; \
+	 echo target remote localhost:1234; \
+	 echo si; \
+	 echo p '$$'x0 = 0; \
+	 echo si; \
+	 echo si; \
+	 echo si; \
+	 echo p '$$'x0 = 0; \
+	 echo p '$$'x1 = 0x900000000; \
+	 echo p '$$'x2 = 0x800000000; \
+	 echo p '*(unsigned long *)0x900000008 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000010 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000018 = 0x200000000'; \
+	 echo p '*(unsigned long *)0x900000020 = 0x900000000'; \
+	 echo p '*(unsigned long *)0x900000028 = 0xa00000000'; \
+	 echo p '*(unsigned long *)0x900000030 = 0'; \
+	 echo p '*(unsigned long *)0x900000038 = 4096'; \
+	 echo p '*(unsigned long *)0x900000040 = 1024'; \
+	 echo p '*(unsigned long *)0x900000048 = 1024'; \
+	 echo p '*(unsigned long *)0x900000050 = 32'; \
+	 echo p '*(unsigned long *)0x900000060 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000068 = 0'; \
+	 echo p '*(unsigned long *)0x9000002d8 = 0x200000000'; \
+	 echo b machine_kexec; \
+	 echo p '*'0x8008f0004 = 0xd503201f; \
+	 echo q) > $@
+
+%.image.gdb3: %.image
+	(echo file ./build/linux/pearl/build/vmlinux; \
+	 echo set disassemble-next-line on; \
+	 echo target remote localhost:1234; \
+	 echo si; \
+	 echo p '$$'x0 = 0; \
+	 echo si; \
+	 echo si; \
+	 echo si; \
+	 echo p '$$'x0 = 0x900000000; \
+	 echo p '*(unsigned long *)0x900000008 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000010 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000018 = 0x200000000'; \
+	 echo p '*(unsigned long *)0x900000020 = 0x900000000'; \
+	 echo p '*(unsigned long *)0x900000028 = 0xa00000000'; \
+	 echo p '*(unsigned long *)0x900000030 = 0'; \
+	 echo p '*(unsigned long *)0x900000038 = 4096'; \
+	 echo p '*(unsigned long *)0x900000040 = 1024'; \
+	 echo p '*(unsigned long *)0x900000048 = 1024'; \
+	 echo p '*(unsigned long *)0x900000050 = 32'; \
+	 echo p '*(unsigned long *)0x900000060 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000068 = 0'; \
+	 echo p '*(unsigned long *)0x9000002d8 = 0x200000000'; \
+	 echo q) > $@
+
 %.image.mpg: %.image %.image.gdb
 	timeout 300 ./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -S -s -device ramfb &
 	sleep 5
@@ -54,3 +108,26 @@ $(call done,qemu,checkout): $(call done,qemu,)
 	sleep 5
 	./build/toolchain/binutils-gdb/source/gdb/gdb --batch --command=$*.image.gdb
 	ffmpeg -i $*.image%05d.jpg -r 20 $@
+
+%.image.mp4: %.image %.image.gdb3
+	$(RM) -f $@ $*.image*.jpg $*.image*.jpg.ppm $*.image*.txt $*.image*.txt.pbm
+	timeout 700 ./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -S -s -device ramfb &
+	sleep 5
+	./build/toolchain/binutils-gdb/source/gdb/gdb --command=$*.image.gdb3 --batch
+	for i in $$(seq 1 600); do \
+	    sleep 1; \
+	    (echo "target remote localhost:1234"; \
+	    echo "shell vncsnapshot -allowblank :1 $*.image$$(printf %05d $$i).jpg"; \
+	    echo "pipe i reg | head -37 | tee $*.image$$(printf %05d $$i).txt 2>/dev/null"; \
+	    echo "pipe bt | head -37 | tee -a $*.image$$(printf %05d $$i).txt 2>/dev/null"; \
+	    echo "shell yes '' | head -100 | tee -a $*.image$$(printf %05d $$i).txt 2>/dev/null"; \
+	    echo "q") | ./build/toolchain/binutils-gdb/source/gdb/gdb || break; \
+        done
+	for a in $*.image*.txt; do (pbmtext -width 256 -builtin fixed | pamcut -height 1024) < $$a > $$a.pbm; done || true
+	for a in $*.image*.jpg; do jpegtopnm $$a > $$a.ppm; done || true
+	i=1; while [ -e $*.image$$(printf %05d $$i).jpg ]; do \
+	    pnmpad -white -right 256 $*.image$$(printf %05d $$i).jpg.ppm > $*.image$$(printf %05d $$i)-tmp.ppm; \
+	    pnmpaste -replace $*.image$$(printf %05d $$i).txt.pbm 1024 0 $*.image$$(printf %05d $$i)-tmp.ppm > $*.image$$(printf %05d $$i).ppm; \
+	    i=$$(($$i+1)); \
+	done
+	ffmpeg -r 1 -i $*.image%05d.ppm $@
