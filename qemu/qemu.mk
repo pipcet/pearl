c18dddc9fcd9a31998b354a45bf95edc57545c3e
Pip Cet <pipcet@gmail.com>
2022-07-28 18:35:02 +0000

    qemu/qemu.mk: rule to generate an mpeg of the emulator framebuffer

--- qemu/qemu.mk
+++ qemu/qemu.mk
@@ -13,3 +13,44 @@ $(call done,qemu,copy): $(call done,qemu,checkout) | $(BUILD)/qemu/
 $(call done,qemu,checkout): $(call done,qemu,)
 	$(MAKE) qemu/qemu{checkout}
 	$(TIMESTAMP)
+
+%.image.gdb: %.image
+	(echo file ./build/linux/pearl/build/vmlinux; \
+	 echo set disassemble-next-line on; \
+	 echo target remote localhost:1234; \
+	 echo si; \
+	 echo p '$$'x0 = 0; \
+	 echo si; \
+	 echo si; \
+	 echo si; \
+	 echo p '$$'x0 = 0; \
+	 echo p '$$'x1 = 0x900000000; \
+	 echo p '$$'x2 = 0x800000000; \
+	 echo p '*(unsigned long *)0x900000008 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000010 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000018 = 0x200000000'; \
+	 echo p '*(unsigned long *)0x900000020 = 0x900000000'; \
+	 echo p '*(unsigned long *)0x900000028 = 0xa00000000'; \
+	 echo p '*(unsigned long *)0x900000030 = 0'; \
+	 echo p '*(unsigned long *)0x900000038 = 8192'; \
+	 echo p '*(unsigned long *)0x900000040 = 2048'; \
+	 echo p '*(unsigned long *)0x900000048 = 2048'; \
+	 echo p '*(unsigned long *)0x900000050 = 32'; \
+	 echo p '*(unsigned long *)0x900000060 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000068 = 0'; \
+	 echo p '*(unsigned long *)0x9000002d8 = 0x200000000'; \
+	 echo b machine_kexec; \
+	 echo p '*'0x8008f0004 = 0xd503201f; \
+	 echo command 1; \
+	 echo k; \
+	 echo q; \
+	 echo end; \
+	 echo c) > $@
+
+%.image.mpg: %.image %.image.gdb
+	timeout 300 ./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -S -s -device ramfb &
+	sleep 5
+	vncsnapshot -fps 1 -count 200 :1  $*.image.jpg &
+	sleep 5
+	./build/toolchain/binutils-gdb/source/gdb/gdb --batch --command=$*.image.gdb
+	ffmpeg -i $*.image%05d.jpg -r 20 $@
