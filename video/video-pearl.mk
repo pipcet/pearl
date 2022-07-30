c159de8d79406c9c2f24d2a4487c225a01849037
Pip Cet <pipcet@gmail.com>
2022-07-30 10:49:34 +0000

    video/video-pearl.mk: convert to use unix socket

--- video/video-pearl.mk
+++ video/video-pearl.mk
@@ -1,3 +1,3 @@
 $(eval $(BUILD)/video/video-pearl/script.gdb: | $(BUILD)/video/video-pearl/ ; $(call video-gdb-bootargs))
 
-$(eval $(BUILD)/video/video-pearl/video.mp4: $(BUILD)/linux/pearl.image{qemu2} $(BUILD)/video/video-pearl/script.gdb | $(BUILD)/video/video-pearl/ ; $(call video-mp4,$(BUILD)/video/video-pearl/data,$(BUILD)/video/video-pearl/script.gdb))
+$(eval $(BUILD)/video/video-pearl/video.mp4: $(BUILD)/linux/pearl.image.qemu $(BUILD)/video/video-pearl/script.gdb | $(BUILD)/video/video-pearl/ ; $(call video-mp4,$(BUILD)/video/video-pearl/data,$(BUILD)/video/video-pearl/script.gdb,$$<))
