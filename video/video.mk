18696f55c4611af130cd13ee6d31e9af344404c1
Pip Cet <pipcet@gmail.com>
2022-07-30 10:02:08 +0000

    video/video.mk: move stuff here

--- video/video.mk
+++ video/video.mk
@@ -1,3 +1,29 @@
+define video-gdb-bootargs
+	(echo set disassemble-next-line on; \
+	 echo target remote localhost:1234; \
+	 echo si; \
+	 echo p '$$$$'x0 = 0; \
+	 echo si; \
+	 echo si; \
+	 echo si; \
+	 echo si; \
+	 echo p '$$$$x1 = 0x900000000'; \
+	 echo p '$$$$x2 = ($$$$x4 &~0x3fff)'; \
+	 echo p '*(unsigned long *)0x900000008 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000010 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000018 = 0x200000000'; \
+	 echo p '*(unsigned long *)0x900000020 = 0x820000000'; \
+	 echo p '*(unsigned long *)0x900000028 = 0xa00000000'; \
+	 echo p '*(unsigned long *)0x900000030 = 0'; \
+	 echo p '*(unsigned long *)0x900000038 = 4096'; \
+	 echo p '*(unsigned long *)0x900000040 = 1024'; \
+	 echo p '*(unsigned long *)0x900000048 = 1024'; \
+	 echo p '*(unsigned long *)0x900000050 = 32'; \
+	 echo p '*(unsigned long *)0x900000060 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000068 = 0'; \
+	 echo p '*(unsigned long *)0x9000002d8 = 0x200000000') > $$@
+endef
+
 define video-mp4
 	$$(RM) -f $$@ $(1).fifo $(1).image*.jpg $(1).image*.jpg.ppm $(1).image*.txt $(1).image*.txt.pbm
 	sleep 5
