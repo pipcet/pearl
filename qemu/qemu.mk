b92fa167f54fbfe8b119448c6cc4ed60c5080733
Pip Cet <pipcet@gmail.com>
2022-07-30 09:01:17 +0000

    qemu/qemu.mk: don't rely on code I haven't written yet

--- qemu/qemu.mk
+++ qemu/qemu.mk
@@ -110,6 +110,31 @@ $(call done,qemu,checkout): $(call done,qemu,)
 	 echo p '*(unsigned long *)0x9000002d8 = 0x200000000'; \
 	 echo q) > $@
 
+%/pearl.image.gdb3: %/pearl.image
+	(echo set disassemble-next-line on; \
+	 echo target remote localhost:1234; \
+	 echo si; \
+	 echo p '$$'x0 = 0; \
+	 echo si; \
+	 echo si; \
+	 echo si; \
+	 echo si; \
+	 echo p '$$x1 = 0x900000000'; \
+	 echo p '$$x2 = ($$x4 &~0x3fff)'; \
+	 echo p '*(unsigned long *)0x900000008 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000010 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000018 = 0x200000000'; \
+	 echo p '*(unsigned long *)0x900000020 = 0x900000000'; \
+	 echo p '*(unsigned long *)0x900000028 = 0xa00000000'; \
+	 echo p '*(unsigned long *)0x900000030 = 0'; \
+	 echo p '*(unsigned long *)0x900000038 = 4096'; \
+	 echo p '*(unsigned long *)0x900000040 = 1024'; \
+	 echo p '*(unsigned long *)0x900000048 = 1024'; \
+	 echo p '*(unsigned long *)0x900000050 = 32'; \
+	 echo p '*(unsigned long *)0x900000060 = 0x800000000'; \
+	 echo p '*(unsigned long *)0x900000068 = 0'; \
+	 echo p '*(unsigned long *)0x9000002d8 = 0x200000000') > $@
+
 %.image.mpg: %.image %.image.gdb
 	timeout 300 ./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -S -s -device ramfb &
 	sleep 5
