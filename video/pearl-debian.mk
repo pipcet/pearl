538804bb987a386c82f5166b95201a11e76e5194
Pip Cet <pipcet@gmail.com>
2022-07-30 13:34:19 +0000

    video/pearl-debian.mk: adjust frame count

--- video/pearl-debian.mk
+++ video/pearl-debian.mk
@@ -1,3 +1,3 @@
 $(eval $(BUILD)/video/pearl-debian/script.gdb: | $(BUILD)/video/pearl-debian/ ; $(call video-gdb-bootargs-x0,$(BUILD)/pearl-debian-uncompressed.macho.zst.image.gdb))
 
-$(eval $(BUILD)/video/pearl-debian/video.mp4: $(BUILD)/pearl-debian-uncompressed.macho.zst.image.qemu $(BUILD)/video/pearl-debian/script.gdb | $(BUILD)/video/pearl-debian/ ; $(call video-mp4,$(BUILD)/video/pearl-debian/data,$(BUILD)/video/pearl-debian/script.gdb,$$<,8000))
+$(eval $(BUILD)/video/pearl-debian/video.mp4: $(BUILD)/pearl-debian-uncompressed.macho.zst.image.qemu $(BUILD)/video/pearl-debian/script.gdb | $(BUILD)/video/pearl-debian/ ; $(call video-mp4,$(BUILD)/video/pearl-debian/data,$(BUILD)/video/pearl-debian/script.gdb,$$<,2000))
