67a2b2d525f9c63af0edd36191e48dee73122c00
Pip Cet <pipcet@gmail.com>
2022-07-30 13:34:32 +0000

    video/pearl.mk: add frame count

--- video/pearl.mk
+++ video/pearl.mk
@@ -1,3 +1,3 @@
 $(eval $(BUILD)/video/pearl/script.gdb: | $(BUILD)/video/pearl/ ; $(call video-gdb-bootargs,$(BUILD)/linux/pearl.image.gdb))
 
-$(eval $(BUILD)/video/pearl/video.mp4: $(BUILD)/linux/pearl.image.qemu $(BUILD)/video/pearl/script.gdb | $(BUILD)/video/pearl/ ; $(call video-mp4,$(BUILD)/video/pearl/data,$(BUILD)/video/pearl/script.gdb,$$<))
+$(eval $(BUILD)/video/pearl/video.mp4: $(BUILD)/linux/pearl.image.qemu $(BUILD)/video/pearl/script.gdb | $(BUILD)/video/pearl/ ; $(call video-mp4,$(BUILD)/video/pearl/data,$(BUILD)/video/pearl/script.gdb,$$<,1000))
