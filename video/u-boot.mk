5c547a63882d61aab2c6033be92a1c25eccce26f
Pip Cet <pipcet@gmail.com>
2022-08-01 20:59:03 +0000

    video/u-boot.mk: this appears to work well enough for the logo...

--- video/u-boot.mk
+++ video/u-boot.mk
@@ -1,3 +1,6 @@
-$(eval $(BUILD)/video/u-boot/script.gdb: | $(BUILD)/video/u-boot/ ; $(call video-gdb-bootargs,$(BUILD)/bootloaders/u-boot.image.gdb,$(BUILD)/bootloaders/u-boot.image))
+$(eval $(BUILD)/video/u-boot/script.gdb: | $(BUILD)/video/u-boot/ ; $(call video-gdb-no-bootargs,$(BUILD)/bootloaders/u-boot.image.gdb,$(BUILD)/bootloaders/u-boot.image))
 
 $(eval $(BUILD)/video/u-boot/video.mp4: $(BUILD)/bootloaders/u-boot.image.qemu $(BUILD)/video/u-boot/script.gdb | $(BUILD)/video/u-boot/ ; $(call video-mp4,$(BUILD)/video/u-boot/data,$(BUILD)/video/u-boot/script.gdb,$$<,1000))
+
+$(BUILD)/video/u-boot/video.mp4: $(call done,bootloaders/barebox,build)
+$(BUILD)/video/u-boot/video.mp4: $(call done,bootloaders/u-boot,build)
