5dfdd000439ca3c366160708599baf0e2c7cfba4
Pip Cet <pipcet@gmail.com>
2022-08-01 21:05:03 +0000

    qemu/qemu.mk: consolidate bootloader support code

--- qemu/qemu.mk
+++ qemu/qemu.mk
@@ -14,92 +14,14 @@ $(call done,qemu,checkout): | $(call done,qemu,)
 	$(MAKE) qemu/qemu{checkout}
 	$(TIMESTAMP)
 
-%/barebox.image.gdb3: %/barebox.image
-	(echo set disassemble-next-line on; \
-	 echo target remote localhost:1234) > $@
-
-%/u-boot.image.gdb3: %/u-boot.image
-	(echo set disassemble-next-line on; \
-	 echo target remote localhost:1234) > $@
-
-%.image.gdb3: %.image
-	(echo set disassemble-next-line on; \
-	 echo target remote localhost:1234; \
-	 echo si; \
-	 echo p '$$'x0 = 0; \
-	 echo si; \
-	 echo si; \
-	 echo si; \
-	 echo p '$$'x0 = 0x900000000; \
-	 echo p '*(unsigned long *)0x900000008 = 0x800000000'; \
-	 echo p '*(unsigned long *)0x900000010 = 0x800000000'; \
-	 echo p '*(unsigned long *)0x900000018 = 0x200000000'; \
-	 echo p '*(unsigned long *)0x900000020 = 0x900000000'; \
-	 echo p '*(unsigned long *)0x900000028 = 0xa00000000'; \
-	 echo p '*(unsigned long *)0x900000030 = 0'; \
-	 echo p '*(unsigned long *)0x900000038 = 4096'; \
-	 echo p '*(unsigned long *)0x900000040 = 1024'; \
-	 echo p '*(unsigned long *)0x900000048 = 1024'; \
-	 echo p '*(unsigned long *)0x900000050 = 32'; \
-	 echo p '*(unsigned long *)0x900000060 = 0x800000000'; \
-	 echo p '*(unsigned long *)0x900000068 = 0'; \
-	 echo p '*(unsigned long *)0x9000002d8 = 0x200000000') > $@
-
-%/pearl.image.gdb3: %/pearl.image
-	(echo set disassemble-next-line on; \
-	 echo target remote localhost:1234; \
-	 echo si; \
-	 echo p '$$'x0 = 0; \
-	 echo si; \
-	 echo si; \
-	 echo si; \
-	 echo si; \
-	 echo p '$$x1 = 0x900000000'; \
-	 echo p '$$x2 = ($$x4 &~0x3fff)'; \
-	 echo p '*(unsigned long *)0x900000008 = 0x800000000'; \
-	 echo p '*(unsigned long *)0x900000010 = 0x800000000'; \
-	 echo p '*(unsigned long *)0x900000018 = 0x200000000'; \
-	 echo p '*(unsigned long *)0x900000020 = 0x900000000'; \
-	 echo p '*(unsigned long *)0x900000028 = 0xa00000000'; \
-	 echo p '*(unsigned long *)0x900000030 = 0'; \
-	 echo p '*(unsigned long *)0x900000038 = 4096'; \
-	 echo p '*(unsigned long *)0x900000040 = 1024'; \
-	 echo p '*(unsigned long *)0x900000048 = 1024'; \
-	 echo p '*(unsigned long *)0x900000050 = 32'; \
-	 echo p '*(unsigned long *)0x900000060 = 0x800000000'; \
-	 echo p '*(unsigned long *)0x900000068 = 0'; \
-	 echo p '*(unsigned long *)0x9000002d8 = 0x200000000') > $@
-
-%.image.mp4.old: %.image %.image.gdb3
-	$(RM) -f $@ $*.image*.jpg $*.image*.jpg.ppm $*.image*.txt $*.image*.txt.pbm
-	timeout 600 ./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -S -s -d unimp -device ramfb &
-	sleep 5
-	./build/toolchain/binutils-gdb/source/gdb/gdb --data-directory=$(PWD)/build/toolchain/binutils-gdb/source/gdb/data-directory --command=$*.image.gdb3 --batch
-	(while true; do \
-	    (echo "target remote localhost:1234"; \
-	    echo "shell vncsnapshot -allowblank -quality 95 :0 $*.image.jpg"; \
-	    echo "pipe i reg | head -37 | tee $*.image.txt 2>/dev/null"; \
-	    echo "pipe x/32i \$$pc - 64 | head -37 | tee -a $*.image.txt 2>/dev/null"; \
-	    echo "pipe bt | head -37 | tee -a $*.image.txt 2>/dev/null"; \
-	    echo "shell yes '' | head -100 | tee -a $*.image.txt 2>/dev/null"; \
-	    echo "q") | ./build/toolchain/binutils-gdb/source/gdb/gdb || break; \
-	    sleep 1; \
-	    (cat $*.image.txt | pbmtext -builtin fixed | pnmpad -width 256 -height 1024 | pnmcut -width 256 -height 1024) > $*.image.pbm || break; \
-	    grep x27 $*.image.txt || break; \
-	    jpegtopnm $*.image.jpg > $*.image.ppm || true; \
-	    pnmpad -white -right 256 $*.image.ppm > $*.image.2.ppm; \
-	    pnmpaste -replace $*.image.pbm 1024 0 $*.image.2.ppm > /dev/fd/3; \
-        done) 3>&1 1>/dev/null 2>/dev/null | ffmpeg -r 1 -i pipe:0 $@
-
-%.image{gdb}: %.image
-	./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -S -s -d unimp -device ramfb &
-
 %.image.qemu: %.image
 	(./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -S -d unimp -device ramfb -monitor unix:$@,server,wait=off -chardev socket,path=$*.image.gdb,server=on,wait=on,id=gdb0 -gdb chardev:gdb0 -device usb-kbd -icount shift=0; rm -f $@ $*.image.gdb) &
 	sleep 5
 
-%/barebox.image.qemu: %/barebox.image %/barebox.dtb
-	QEMU_WITH_DTB=1 ./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -dtb $*/barebox.dtb -S -s -d unimp -device ramfb -device qemu-xhci -monitor unix:$*.socket,server &
+%/barebox.image.qemu %/barebox.image.gdb: %/barebox.image %/barebox.dtb
+	(QEMU_WITH_DTB=1 ./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -S -d unimp -device ramfb -monitor unix:$@,server,wait=off -chardev socket,path=$*/barebox.image.gdb,server=on,wait=on,id=gdb0 -gdb chardev:gdb0 -device usb-kbd -icount shift=0 -dtb $*/barebox.dtb; rm -f $@ $*/barebox.image.gdb) &
+	sleep 5
 
 %/u-boot.image.qemu: %/u-boot.image %/barebox.dtb
-	QEMU_WITH_DTB=1 ./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -dtb $*/barebox.dtb -S -s -d unimp -device ramfb -device qemu-xhci -monitor unix:$*.socket,server &
+	(QEMU_WITH_DTB=1 ./build/qemu/build/qemu-system-aarch64 -m 12g -cpu max -machine virt -kernel $< -S -d unimp -device ramfb -monitor unix:$@,server,wait=off -chardev socket,path=$*/u-boot.image.gdb,server=on,wait=on,id=gdb0 -gdb chardev:gdb0 -device usb-kbd -icount shift=0 -dtb $*/barebox.dtb; rm -f $@ $*/u-boot.image.gdb) &
+	sleep 5
