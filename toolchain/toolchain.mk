053c11b45ec865889d2bedef51c9a8d71fef1ba5
Pip Cet <pipcet@gmail.com>
2022-08-03 21:36:27 +0000

    toolchain/toolchain.mk: package all done/ stamps

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -2,6 +2,6 @@ include toolchain/binutils-gdb/binutils-gdb.mk
 include toolchain/gcc/gcc.mk
 
 $(BUILD)/toolchain.tar: $(call done,toolchain/binutils-gdb,install) $(call done,toolchain/gcc,gcc/install)
-	tar -C . -cf $@ $(patsubst $(PWD)/%,%,$(BUILD)/pearl/toolchain $(BUILD)/pearl/install $(wildcard $(call done,toolchain/*,*)) $(wildcard $(call done,linux/*,*)) $(wildcard $(call done,userspace/*,*)))
+	tar -C . -cf $@ $(patsubst $(PWD)/%,%,$(BUILD)/pearl/toolchain $(BUILD)/pearl/install done)
 
 SECTARGETS += $(BUILD)/toolchain.tar
