6997565c6ce4a7b76cc1c4a75101c7f0bc5a4847
Pip <pipcet@gmail.com>
2022-08-05 18:20:20 +0000

    toolchain/toolchain.mk: include libgcc build

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -1,7 +1,7 @@
 include toolchain/binutils-gdb/binutils-gdb.mk
 include toolchain/gcc/gcc.mk
 
-$(BUILD)/toolchain.tar: $(call done,toolchain/binutils-gdb,install) $(call done,toolchain/gcc,gcc/install)
+$(BUILD)/toolchain.tar: $(call done,toolchain/binutils-gdb,install) $(call done,toolchain/gcc,gcc/install) $(call done,toolchain/gcc,libgcc/install)
 	tar -C . -cf $@ $(patsubst $(PWD)/%,%,$(BUILD)/pearl/toolchain $(BUILD)/pearl/install done)
 
 SECTARGETS += $(BUILD)/toolchain.tar
