b36a98bc757dd31fd2fcc301bd5559d038fdee71
Pip Cet <pipcet@gmail.com>
2022-08-07 16:13:40 +0000

    local/memdump/memdump.mk: fix to use CROSS_CFLAGS

--- local/memdump/memdump.mk
+++ local/memdump/memdump.mk
@@ -1,6 +1,6 @@
 $(BUILD)/memdump/bin/macho-to-memdump: local/memdump/macho-to-memdump | $(BUILD)/memdump/bin/ ; $(COPY)
 $(BUILD)/memdump/bin/memdump-to-image: local/memdump/memdump-to-image.c | $(call deps,glibc gcc) $(BUILD)/memdump/bin/
-	$(WITH_CROSS_PATH) $(WITH_CROSS_CC) $(CROSS_CC) -static -Os -I$(BUILD)/local/memdump -o $@ $<
+	$(WITH_CROSS_PATH) $(WITH_CROSS_CC) $(CROSS_CC) $(CROSS_CFLAGS) -static -Os -I$(BUILD)/local/memdump -o $@ $<
 
 $(BUILD)/local/memdump/boot.c: local/memdump/boot.c
 	$(COPY)
