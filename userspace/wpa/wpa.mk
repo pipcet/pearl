74d90c3be473597a64712b0f8eec138700709389
Pip Cet <pipcet@gmail.com>
2022-08-07 08:41:23 +0000

    userspace/wpa/wpa.mk: don't use $< now the dep order changed

--- userspace/wpa/wpa.mk
+++ userspace/wpa/wpa.mk
@@ -8,7 +8,7 @@ $(call done,userspace/wpa_supplicant,build): $(call done,userspace/wpa_supplican
 	$(TIMESTAMP)
 
 $(call done,userspace/wpa_supplicant,configure): $(call done,userspace/wpa_supplicant,copy) | userspace/wpa/wpa_supplicant.config $(call done,userspace/libnl,install) $(call done,userspace/openssl,install)
-	cp $< $(BUILD)/userspace/wpa_supplicant/build/wpa_supplicant/.config
+	cp userspace/wpa/wpa_supplicant.config $(BUILD)/userspace/wpa_supplicant/build/wpa_supplicant/.config
 	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/userspace/wpa_supplicant/build/wpa_supplicant $(WITH_CROSS_CC) EXTRA_CFLAGS="$(CROSS_CFLAGS) -I$(BUILD)/pearl/install/include/libnl3" PKG_CONFIG=/bin/false defconfig
 	$(TIMESTAMP)
 
