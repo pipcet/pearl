9d315fe934be6d9eea2c442a2520a167788c8a3b
Pip Cet <pipcet@gmail.com>
2022-08-07 11:18:41 +0000

    userspace/wpa/wpa.mk: add LDFLAGS

--- userspace/wpa/wpa.mk
+++ userspace/wpa/wpa.mk
@@ -1,10 +1,10 @@
 $(call done,userspace/wpa_supplicant,install): $(call done,userspace/wpa_supplicant,build)
-	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/userspace/wpa_supplicant/build/wpa_supplicant $(WITH_CROSS_CC) PKG_CONFIG=/bin/false DESTDIR=$(call install,userspace/wpa_supplicant) install
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/userspace/wpa_supplicant/build/wpa_supplicant $(WITH_CROSS_CC) LDFLAGS="$(CROSS_CFLAGS)" PKG_CONFIG=/bin/false DESTDIR=$(call install,userspace/wpa_supplicant) install
 	$(INSTALL_LIBS) userspace/wpa_supplicant
 	$(TIMESTAMP)
 
 $(call done,userspace/wpa_supplicant,build): $(call done,userspace/wpa_supplicant,configure)
-	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/userspace/wpa_supplicant/build/wpa_supplicant $(WITH_CROSS_CC) EXTRA_CFLAGS="$(CROSS_CFLAGS) -I$(BUILD)/pearl/install/include/libnl3" PKG_CONFIG=/bin/false
+	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/userspace/wpa_supplicant/build/wpa_supplicant $(WITH_CROSS_CC) CFLAGS="$(CROSS_CFLAGS) -I$(BUILD)/pearl/install/include/libnl3" EXTRA_LDFLAGS="$(CROSS_CFLAGS)" PKG_CONFIG=/bin/false
 	$(TIMESTAMP)
 
 $(call done,userspace/wpa_supplicant,configure): $(call done,userspace/wpa_supplicant,copy) | userspace/wpa/wpa_supplicant.config $(call done,userspace/libnl,install) $(call done,userspace/openssl,install)
