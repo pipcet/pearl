c466fa66dbaef1a5f52967acf3830a6cb8dda314
Pip Cet <pipcet@gmail.com>
2022-08-07 18:53:56 +0000

    toolchain/toolchain.mk: new libgcc.tar target

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -4,4 +4,7 @@ include toolchain/gcc/gcc.mk
 $(BUILD)/toolchain.tar: $(call done,toolchain/binutils-gdb,install) $(call done,toolchain/gcc,gcc/install)
 	tar -C . -cf $@ $(patsubst $(PWD)/%,%,$(BUILD)/pearl/toolchain $(BUILD)/pearl/install done)
 
+$(BUILD)/libgcc.tar: $(call done,toolchain/gcc,libgcc/install)
+	tar -C . -cf $@ $(patsubst $(PWD)/%,%,$(BUILD)/pearl/toolchain $(BUILD)/pearl/install done)
+
 SECTARGETS += $(BUILD)/toolchain.tar
