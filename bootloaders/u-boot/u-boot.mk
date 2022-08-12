0d99c4ee730000dfe5156f7e5372da9aedff06a0
Pip <pipcet@gmail.com>
2022-08-12 12:13:54 +0000

    fix order-only dependencies

--- bootloaders/u-boot/u-boot.mk
+++ bootloaders/u-boot/u-boot.mk
@@ -54,7 +54,7 @@ $(call done,bootloaders/u-boot,build): $(call done,bootloaders/u-boot,configure)
 	$(TIMESTAMP)
 
 $(call done,bootloaders/u-boot,configure): $(call done,bootloaders/u-boot,copy) | bootloaders/u-boot/u-boot.config $(call done,toolchain/gcc,gcc/install)
-	$(CP) $< $(BUILD)/bootloaders/u-boot/build/.config
+	$(CP) bootloaders/u-boot/u-boot.config $(BUILD)/bootloaders/u-boot/build/.config
 	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/bootloaders/u-boot/build ARCH=arm CROSS_COMPILE=$(CROSS_COMPILE) oldconfig
 	$(TIMESTAMP)
 
