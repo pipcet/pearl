66c7ec91fafc2c810f47a26b6993d578b0e9a434
Pip <pipcet@gmail.com>
2022-08-13 14:47:46 +0000

    toolchain/toolchain.mk: make toolchain build conditional

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -1,8 +1,13 @@
 include toolchain/binutils-gdb/binutils-gdb.mk
 include toolchain/gcc/gcc.mk
 
+ifeq ($(filter toolchain,$(RELEASED_ARTIFACTS)),)
 $(BUILD)/toolchain.tar: $(call done,toolchain/binutils-gdb,install) $(call done,toolchain/gcc,gcc/install)
 	tar -C . -cf $@ $(patsubst $(PWD)/%,%,$(BUILD)/pearl/toolchain $(BUILD)/pearl/install done)
+else
+$(BUILD)/toolchain.tar: $(BUILD)/released/pipcet/pearl-toolchain/toolchain.tar.zstd{}
+	zstd -d < $(BUILD)/released/pipcet/pearl-toolchain/toolchain.tar.zstd > $@
+endif
 
 $(BUILD)/libgcc.tar: $(call done,toolchain/gcc,libgcc/install)
 	tar -C . -cf $@ $(patsubst $(PWD)/%,%,$(BUILD)/pearl/toolchain $(BUILD)/pearl/install done)
