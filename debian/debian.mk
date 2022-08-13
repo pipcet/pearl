180b025ef086ea8bd3d73396841d4baeb26981ac
Pip <pipcet@gmail.com>
2022-08-13 17:31:55 +0000

    debian/debian.mk: include installer.mk

--- debian/debian.mk
+++ debian/debian.mk
@@ -1,5 +1,6 @@
 include debian/deb/deb.mk
 include debian/debootstrap/debootstrap.mk
 include debian/debian-rootfs/debian-rootfs.mk
+include debian/installer/installer.mk
 
 $(call pearl-static,$(wildcard $(PWD)/debian/pearl/bin/*),$(PWD)/debian/pearl)
