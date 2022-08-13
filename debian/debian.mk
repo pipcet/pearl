871c7d4c8fe6072385534a3ccc3287f4ec8c08cd
Pip <pipcet@gmail.com>
2022-08-13 13:20:45 +0000

    debian/debian.mk: include debian-rootfs.mk

--- debian/debian.mk
+++ debian/debian.mk
@@ -1,4 +1,5 @@
 include debian/deb/deb.mk
 include debian/debootstrap/debootstrap.mk
+include debian/debian-rootfs/debian-rootfs.mk
 
 $(call pearl-static,$(wildcard $(PWD)/debian/pearl/bin/*),$(PWD)/debian/pearl)
