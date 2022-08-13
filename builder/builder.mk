b7236a1f17433ac9e20dd10c1371d8d748530e38
Pip <pipcet@gmail.com>
2022-08-13 17:21:53 +0000

    builder/builder.mk: run apt-get update

--- builder/builder.mk
+++ builder/builder.mk
@@ -2,8 +2,12 @@ ifeq ($(BUILDER),)
 $(PWD)/builder/packages/%{}:
 	$(TIMESTAMP)
 else
-$(PWD)/builder/packages/%{}: $(PWD)/builder/packages/
+$(PWD)/builder/packages/%{}: | $(PWD)/builder/packages/ $(PWD)/builder/update
 	$(PWD)/g/bin/locked --lockfile $(PWD)/builder.lock sudo apt-get -y install $*
+	$(TIMESTAMP)
+
+$(PWD)/builder/update:
+	$(PWD)/g/bin/locked --lockfile $(PWD)/builder.lock sudo apt-get -y update
 	$(TIMESTAMP)
 endif
 
