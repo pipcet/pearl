4f9fc12273f4a0615691f54f19d4083bee741cf0
Pip <pipcet@gmail.com>
2022-08-14 20:08:11 +0000

    video/*.mk: depend on binutils-gdb

--- video/pearl-debian.mk
+++ video/pearl-debian.mk
@@ -1,3 +1,3 @@
 $(eval $(BUILD)/video/pearl-debian/script.gdb: | $(BUILD)/video/pearl-debian/ ; $(call video-gdb-bootargs-x0,$(BUILD)/pearl-debian.image.gdb,$(BUILD)/pearl-debian.image.qemu,10000))
 
-$(eval $(BUILD)/video/pearl-debian/video.mp4: $(BUILD)/pearl-debian.image.qemu $(BUILD)/video/pearl-debian/script.gdb | $(BUILD)/video/pearl-debian/ builder/packages/ffmpeg{} builder/packages/netpbm{} builder/packages/socat{} builder/sysctl/overcommit_memory{} ; $(call video-mp4,$(BUILD)/video/pearl-debian/data,$(BUILD)/video/pearl-debian/script.gdb,$$<,2000))
+$(eval $(BUILD)/video/pearl-debian/video.mp4: $(BUILD)/pearl-debian.image.qemu $(BUILD)/video/pearl-debian/script.gdb | $(BUILD)/video/pearl-debian/ $(call done,toolchain/binutils-gdb,install) builder/packages/ffmpeg{} builder/packages/netpbm{} builder/packages/socat{} builder/sysctl/overcommit_memory{} ; $(call video-mp4,$(BUILD)/video/pearl-debian/data,$(BUILD)/video/pearl-debian/script.gdb,$$<,2000))
--- video/u-boot.mk
+++ video/u-boot.mk
@@ -1,6 +1,6 @@
 $(eval $(BUILD)/video/u-boot/script.gdb: | $(BUILD)/video/u-boot/ ; $(call video-gdb-no-bootargs,$(BUILD)/bootloaders/u-boot.image.gdb,$(BUILD)/bootloaders/u-boot.image))
 
-$(eval $(BUILD)/video/u-boot/video.mp4: $(BUILD)/bootloaders/u-boot.image.qemu $(BUILD)/video/u-boot/script.gdb | $(BUILD)/video/u-boot/ builder/packages/ffmpeg{} builder/packages/netpbm{} builder/packages/socat{} builder/sysctl/overcommit_memory{} ; $(call video-mp4,$(BUILD)/video/u-boot/data,$(BUILD)/video/u-boot/script.gdb,$$<,1000))
+$(eval $(BUILD)/video/u-boot/video.mp4: $(BUILD)/bootloaders/u-boot.image.qemu $(BUILD)/video/u-boot/script.gdb | $(BUILD)/video/u-boot/ $(call done,toolchain/binutils-gdb,install) builder/packages/ffmpeg{} builder/packages/netpbm{} builder/packages/socat{} builder/sysctl/overcommit_memory{} ; $(call video-mp4,$(BUILD)/video/u-boot/data,$(BUILD)/video/u-boot/script.gdb,$$<,1000))
 
 $(BUILD)/video/u-boot/video.mp4: $(call done,bootloaders/barebox,build)
 $(BUILD)/video/u-boot/video.mp4: $(call done,bootloaders/u-boot,build)
