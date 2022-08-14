c11b6abc1edd23749bfa2f0b50263af5ed1e58db
Pip <pipcet@gmail.com>
2022-08-14 13:32:08 +0000

    video/*.mk: add builder package deps

--- video/barebox.mk
+++ video/barebox.mk
@@ -1,5 +1,5 @@
 $(eval $(BUILD)/video/barebox/script.gdb: | $(BUILD)/video/barebox/ ; $(call video-gdb-no-bootargs,$(BUILD)/bootloaders/barebox.image.gdb,$(BUILD)/bootloaders/barebox.image))
 
-$(eval $(BUILD)/video/barebox/video.mp4: $(BUILD)/bootloaders/barebox.image.qemu $(BUILD)/video/barebox/script.gdb | $(BUILD)/video/barebox/ ; $(call video-mp4,$(BUILD)/video/barebox/data,$(BUILD)/video/barebox/script.gdb,$$<,1000))
+$(eval $(BUILD)/video/barebox/video.mp4: $(BUILD)/bootloaders/barebox.image.qemu $(BUILD)/video/barebox/script.gdb | $(BUILD)/video/barebox/ builder/packages/ffmpeg{} builder/packages/netpbm{} builder/packages/socat{} ; $(call video-mp4,$(BUILD)/video/barebox/data,$(BUILD)/video/barebox/script.gdb,$$<,1000))
 
 $(BUILD)/video/barebox/video.mp4: $(call done,bootloaders/barebox,build)
--- video/pearl.mk
+++ video/pearl.mk
@@ -1,3 +1,3 @@
 $(eval $(BUILD)/video/pearl/script.gdb: | $(BUILD)/video/pearl/ ; $(call video-gdb-bootargs,$(BUILD)/linux/pearl.image.gdb,$(BUILD)/linux/pearl.image))
 
-$(eval $(BUILD)/video/pearl/video.mp4: $(BUILD)/linux/pearl.image.qemu $(BUILD)/video/pearl/script.gdb | $(BUILD)/video/pearl/ ; $(call video-mp4,$(BUILD)/video/pearl/data,$(BUILD)/video/pearl/script.gdb,$$<,1000))
+$(eval $(BUILD)/video/pearl/video.mp4: $(BUILD)/linux/pearl.image.qemu $(BUILD)/video/pearl/script.gdb | $(BUILD)/video/pearl/ builder/packages/ffmpeg{} builder/packages/netpbm{} builder/packages/socat{} ; $(call video-mp4,$(BUILD)/video/pearl/data,$(BUILD)/video/pearl/script.gdb,$$<,1000))
