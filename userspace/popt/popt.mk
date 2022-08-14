7bbdaf67fedcc055b2761173e7c06f1473021018
Pip <pipcet@gmail.com>
2022-08-14 15:44:42 +0000

    userspace/popt/popt.mk: add dependencies

--- userspace/popt/popt.mk
+++ userspace/popt/popt.mk
@@ -10,7 +10,7 @@ $(call done,userspace/popt,build): $(call done,userspace/popt,configure)
 	$(WITH_CROSS_PATH) $(MAKE) -C $(BUILD)/userspace/popt/build
 	$(TIMESTAMP)
 
-$(call done,userspace/popt,configure): $(call done,userspace/popt,copy) | $(call deps,glibc gcc)
+$(call done,userspace/popt,configure): $(call done,userspace/popt,copy) | $(call deps,glibc gcc) builder/packages/qemu-user{} builder/packages/qemu-user-static{} builder/packages/binfmt-support{} builder/packages/autopoint{} builder/packages/gettext{} builder/packages/libtool-bin{}
 	(cd $(BUILD)/userspace/popt/build; $(WITH_CROSS_PATH) sh autogen.sh)
 	(cd $(BUILD)/userspace/popt/build; $(WITH_CROSS_PATH) ./configure --target=aarch64-linux-gnu --host=aarch64-linux-gnu --prefix=/ CFLAGS="$(CROSS_CFLAGS)")
 	$(TIMESTAMP)
