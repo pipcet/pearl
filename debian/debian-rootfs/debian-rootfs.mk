8f9af965dce97934d9f214b16e7667231ad417a8
Pip <pipcet@gmail.com>
2022-08-14 13:49:37 +0000

    .github/workflows/split1.yml: remove the last overcommit_memory

--- debian/debian-rootfs/debian-rootfs.mk
+++ debian/debian-rootfs/debian-rootfs.mk
@@ -52,7 +52,7 @@ $(BUILD)/debian/debian-rootfs/root0.cpio: | $(BUILD)/debian/debian-rootfs/ $(cal
 	sudo chmod u+x $(BUILD)/debian/debian-rootfs/di-debootstrap/init $(BUILD)/debian/debian-rootfs/di-debootstrap/init2
 	(cd $(BUILD)/debian/debian-rootfs/di-debootstrap; sudo chown root.root .; sudo find . | sudo cpio -H newc -o) > $@
 
-$(BUILD)/debian/debian-rootfs/root1.cpio: $(BUILD)/qemu-kernel $(BUILD)/debian/debian-rootfs/root0.cpio | $(BUILD)/ builder/packages/qemu-system-aarch64{} builder/packages/sharutils{}
+$(BUILD)/debian/debian-rootfs/root1.cpio: $(BUILD)/qemu-kernel $(BUILD)/debian/debian-rootfs/root0.cpio | $(BUILD)/ builder/packages/qemu-system-aarch64{} builder/packages/sharutils{} builder/sysctl/overcommit_memory{}
 	dd if=/dev/zero of=tmp bs=1G count=3
 	qemu-system-aarch64 -drive if=virtio,index=0,media=disk,driver=raw,file=tmp -machine virt -cpu max -kernel $(BUILD)/qemu-kernel -m 7g -serial stdio -initrd $(BUILD)/debian/debian-rootfs/root0.cpio -nic user,model=virtio -monitor none -smp 8 -nographic
 	uudecode -o $@ < tmp
@@ -74,7 +74,7 @@ $(BUILD)/debian/debian-rootfs/root2-script.bash: | $(BUILD)/debian/debian-rootfs
 	echo "cd /; find / -xdev | cpio -H newc -o | uuencode root2.cpio > /dev/vda") > $@
 
 ifeq ($(filter rootfs,$(RELEASED_ARTIFACTS)),)
-$(BUILD)/debian/debian-rootfs/root2.cpio: $(BUILD)/qemu-kernel $(BUILD)/debian/debian-rootfs/root1.cpio.gz $(BUILD)/debian/debian-rootfs/root2-script.bash | $(BUILD)/ builder/packages/qemu-system-aarch64{} builder/packages/sharutils
+$(BUILD)/debian/debian-rootfs/root2.cpio: $(BUILD)/qemu-kernel $(BUILD)/debian/debian-rootfs/root1.cpio.gz $(BUILD)/debian/debian-rootfs/root2-script.bash | $(BUILD)/ builder/packages/qemu-system-aarch64{} builder/packages/sharutils builder/sysctl/overcommit_memory{}
 	dd if=/dev/zero of=tmp bs=1G count=3
 	uuencode script.bash < $(BUILD)/debian/debian-rootfs/root2-script.bash | dd of=tmp conv=notrunc
 	qemu-system-aarch64 -drive if=virtio,index=0,media=disk,driver=raw,file=tmp -machine virt -cpu max -kernel $(BUILD)/qemu-kernel -m 7g -serial stdio -initrd $(BUILD)/debian/debian-rootfs/root1.cpio.gz -nic user,model=virtio -monitor none -nographic
