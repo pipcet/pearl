9b1f134cf78c78d601335486f696d1126ed42c48
Pip <pipcet@gmail.com>
2022-08-14 13:39:34 +0000

    builder/builder.mk: add sysctl support

--- builder/builder.mk
+++ builder/builder.mk
@@ -1,6 +1,9 @@
 ifeq ($(BUILDER),)
 $(PWD)/builder/packages/%{}:
 	$(TIMESTAMP)
+
+$(PWD)/builder/sysctl/%{}:
+	$(TIMESTAMP)
 else
 $(PWD)/builder/packages/%{}: | $(PWD)/builder/packages/ $(PWD)/builder/update
 	$(PWD)/g/bin/locked --lockfile $(PWD)/builder.lock sudo apt-get -y install $*
@@ -9,7 +12,13 @@ $(PWD)/builder/packages/%{}: | $(PWD)/builder/packages/ $(PWD)/builder/update
 $(PWD)/builder/update:
 	$(PWD)/g/bin/locked --lockfile $(PWD)/builder.lock sudo apt-get -y update
 	$(TIMESTAMP)
+
+$(PWD)/builder/sysctl/overcommit_memory{}:
+	echo 1 | sudo tee /proc/sys/vm/overcommit_memory
+	$(TIMESTAMP)
 endif
 
 builder/packages/%{}: $(PWD)/builder/packages/%{}
 	$(TIMESTAMP)
+
+builder/sysctl/overcommit_memory{}: $(PWD)/builder/sysctl/overcommit_memory{}
