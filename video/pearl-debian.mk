5bccf7fe2ebba6cc0eb41c651243f2d7090a7b5b
Pip <pipcet@gmail.com>
2022-08-14 11:51:49 +0000

    video/pearl-debian.mk: adjust to new path

--- video/pearl-debian.mk
+++ video/pearl-debian.mk
@@ -1,3 +1,3 @@
-$(eval $(BUILD)/video/pearl-debian/script.gdb: | $(BUILD)/video/pearl-debian/ ; $(call video-gdb-bootargs-x0,$(BUILD)/pearl-debian-uncompressed.macho.zst.image.gdb,$(BUILD)/pearl-debian-uncompressed.macho.zst.image.qemu,10000))
+$(eval $(BUILD)/video/pearl-debian/script.gdb: | $(BUILD)/video/pearl-debian/ ; $(call video-gdb-bootargs-x0,$(BUILD)/pearl-debian.image.gdb,$(BUILD)/pearl-debian.image.qemu,10000))
 
-$(eval $(BUILD)/video/pearl-debian/video.mp4: $(BUILD)/pearl-debian-uncompressed.macho.zst.image.qemu $(BUILD)/video/pearl-debian/script.gdb | $(BUILD)/video/pearl-debian/ ; $(call video-mp4,$(BUILD)/video/pearl-debian/data,$(BUILD)/video/pearl-debian/script.gdb,$$<,2000))
+$(eval $(BUILD)/video/pearl-debian/video.mp4: $(BUILD)/pearl-debian.image.qemu $(BUILD)/video/pearl-debian/script.gdb | $(BUILD)/video/pearl-debian/ ; $(call video-mp4,$(BUILD)/video/pearl-debian/data,$(BUILD)/video/pearl-debian/script.gdb,$$<,2000))
