945d4023f3295117af62b1371cc21d7bbe2dae55
Pip <pipcet@gmail.com>
2022-08-14 18:50:20 +0000

    local/dtc-relocs/dtc-relocs.mk: add dependencies

--- local/dtc-relocs/dtc-relocs.mk
+++ local/dtc-relocs/dtc-relocs.mk
@@ -1,2 +1,2 @@
-%.dts.h: %.dts local/dtc-relocs/dtc-relocs
+%.dts.h: %.dts local/dtc-relocs/dtc-relocs | builder/packages/libipc-run-perl{} builder/packages/libfile-slurp-perl{}
 	gcc -E -x assembler-with-cpp -nostdinc $< | local/dtc-relocs/dtc-relocs > $@
