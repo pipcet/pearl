558df9082c71a7d5ec9ebd245384c7106e76ec37
Pip <pipcet@gmail.com>
2022-08-15 15:59:45 +0000

    toolchain/toolchain.mk: oops, include glibc

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -2,7 +2,7 @@ include toolchain/binutils-gdb/binutils-gdb.mk
 include toolchain/gcc/gcc.mk
 
 ifeq ($(filter toolchain,$(RELEASED_ARTIFACTS)),)
-$(BUILD)/toolchain.tar: $(call done,toolchain/binutils-gdb,install) $(call done,toolchain/gcc,gcc/install)
+$(BUILD)/toolchain.tar: $(call done,toolchain/binutils-gdb,install) $(call done,toolchain/gcc,gcc/install) $(call done,userspace/glibc,glibc/install)
 	tar -C . -cf $@ $(patsubst $(PWD)/%,%,$(BUILD)/pearl/toolchain $(BUILD)/pearl/install done)
 else
 $(BUILD)/toolchain.tar: $(BUILD)/released/pipcet/pearl-toolchain/toolchain.tar.zstd{}
